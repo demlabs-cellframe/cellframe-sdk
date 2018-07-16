faf22e3227985bfe1745bbb4479ab3e56d2abf12
armatusmiles <akurotych@gmail.com>
2018-07-16 16:22:47 +0300

    Delete Qt framework from testing module

---
+++ test/core/main.c
@@ -0,0 +1,8 @@
+#include "test_headers.h"
+#include "dap_common.h"
+
+int main(void) {
+    // switch off debug info from library
+    set_log_level(L_CRITICAL);
+    dap_config_tests_run();
+}
---
+++ test/core/test_headers.h
@@ -0,0 +1,3 @@
+#pragma once
+
+#include "dap_config_test.h"
