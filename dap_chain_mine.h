2181efba45c7a7401e5ac6953942bc16e3ade238
Dmitriy A. Gerasimov <dmitriy.gerasimov@demlabs.net>
2018-09-08 19:27:22 +0700

    [-] Moved files to other reps

--- dap_chain_mine.h
+++
@@ -1,7 +0,0 @@
-
-#pragma once
-
-#include "dap_chain_block_cache.h"
-
-
-int dap_chain_mine_block(dap_chain_block_cache_t * a_block_cache, bool a_mine_gold_only, uint32_t a_threads);
--- dap_chain_srv_pkt.c
+++
@@ -1,3 +0,0 @@
-#include "dap_chain_srv_pkt.h"
-
-#define LOG_TAG "srv_pkt"
