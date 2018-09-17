a57b16a937dac77f6ac5ea0b06224a5a75eb68ce
armatusmiles <akurotych@gmail.com>
2018-09-17 16:40:15 +0300

    [+] test_cpu_get_stats

--- test/core/dap_cpu_monitor_test.c
+++ test/core/dap_cpu_monitor_test.c
@@ -1,7 +1,7 @@
 #include "dap_cpu_monitor_test.h"
 #include "dap_cpu_monitor.h"
-
 #include "unistd.h"
+
 static void init_test_case()
 {
     dap_assert(dap_cpu_monitor_init() == 0, "Cpu module init");
@@ -12,20 +12,26 @@ static void deinit_test_case()
     dap_cpu_monitor_deinit();
 }
 
+static void test_cpu_get_stats()
+{
+    dap_cpu_stats_t stat = dap_cpu_get_stats();
+    dap_assert(stat.cpu_cores_count > 0, "Check cpu count");
+    dap_assert(stat.cpu_summary.total_time > 0, "Check cpu summary total_time");
+    dap_assert(stat.cpu_summary.total_time > 0, "Check cpu summary idle_time");
+    dap_assert(stat.cpu_cores_count > 0, "Check cpu count");
+    for(unsigned i = 0; i < stat.cpu_cores_count; i++) {
+        dap_assert_PIF(stat.cpus[i].ncpu == i, "Check ncpu and index in array");
+        dap_assert_PIF(stat.cpus[i].idle_time > 0, "Check cpu idle_time");
+        dap_assert_PIF(stat.cpus[i].total_time > 0, "Check cpu total_time");
+    }
+}
+
 void dap_cpu_monitor_test_run()
 {
     dap_print_module_name("dap_cpu_monitor");
     init_test_case();
-//    while (1) {
-//        dap_cpu_stats_t s = dap_cpu_get_stats();
-//        printf("Summary average load %f\n", s.cpu_summary.load);
-
-//        for(int i = 0; i < s.cpu_cores_count; i++) {
-//            printf("Core %d load %f \n", i + 1, s.cpus[i].load);
-//        }
-//        fflush(stdout);
-//        sleep(1);
-//    }
+    usleep(1000); // wait for new cpu parameters
+    test_cpu_get_stats();
 
     dap_cpu_get_stats();
     deinit_test_case();
