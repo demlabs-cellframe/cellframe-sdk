93e279534456e5de6a9ef457b54dde810e4db732
Aleksandr Lysikov <lysikov@inbox.ru>
2019-01-15 22:16:17 +0500

    add libdap-chain-mempool

--- dap_chain_mempool.c
+++ dap_chain_mempool.c
@@ -1,16 +1,45 @@
+#include <stdio.h>
 #include "dap_common.h"
 #include "dap_http_client.h"
 #include "dap_http_simple.h"
+//#include "dap_enc_http.h"
+#include "dap_enc_http.h"
+//#include "dap_http.h"
+#include "http_status_code.h"
+#include "dap_chain_datum.h"
+#include "dap_chain_global_db.h"
 #include "dap_chain_mempool.h"
 
+#define FILE_MEMPOOL_DB "1.db" // TODO get from settings
 /**
  * @brief
  * @param cl_st HTTP server instance
- * @param arg Not used
+ * @param arg for return code
  */
 void chain_mempool_proc(struct dap_http_simple *cl_st, void * arg)
 {
-
+    http_status_code_t * return_code = (http_status_code_t*) arg;
+    enc_http_delegate_t *dg = enc_http_request_decode(cl_st);
+    if(dg) {
+        char *url = dg->url_path;
+        char *request_str = dg->request_str;
+        int request_size = dg->request_size;
+        printf("!!***!!! chain_mempool_proc arg=%d url=%s str=%s len=%d\n", arg, url, request_str, request_size);
+        if(request_str && request_size > 0) {
+            dap_datum_mempool_t *mempool = (dap_datum_mempool_t*)request_str;
+            const char *a_key = "";//TODO hash(mempool)
+            const char *a_value = "";// TODO mempool;
+            if(dap_chain_global_db_set(a_key, a_value))
+                *return_code = Http_Status_OK;
+            else
+                *return_code = Http_Status_InternalServerError;
+        }
+        else
+            *return_code = Http_Status_BadRequest;
+    }
+    else {
+        *return_code = Http_Status_Unauthorized;
+    }
 }
 
 /**
@@ -20,6 +49,6 @@ void chain_mempool_proc(struct dap_http_simple *cl_st, void * arg)
  */
 void dap_chain_mempool_add_proc(struct dap_http * sh, const char * url)
 {
-    /*void dap_http_simple_proc_add(dap_http_t *sh, const char * url_path, size_t reply_size_max, dap_http_simple_callback_t cb); // Add simple processor*/
+    dap_chain_global_db_init(FILE_MEMPOOL_DB);
     dap_http_simple_proc_add(sh, url, 4096, chain_mempool_proc);
 }
--- dap_chain_mempool.h
+++ dap_chain_mempool.h
@@ -1,3 +1,14 @@
 #pragma once
 
+// datum mempool structure
+typedef struct dap_datum_mempool {
+    int16_t version;               // structure version
+    uint16_t datum_count;          // datums count
+    struct {
+        int32_t datum_size;
+        dap_chain_datum_t *datum;
+    }DAP_ALIGN_PACKED data[];      // mass of datums
+}DAP_ALIGN_PACKED dap_datum_mempool_t;
+
+
 void dap_chain_mempool_add_proc(struct dap_http * sh, const char * url);
