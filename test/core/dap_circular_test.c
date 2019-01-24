381bbf9a7b97f4b2f5d947c74108a03ebc6ecd41
armatusmiles <akurotych@gmail.com>
2019-01-24 18:08:59 +0700

    [+] dap_circular_buffer

---
+++ test/core/dap_circular_test.c
@@ -0,0 +1,8 @@
+#include "dap_circular_test.h"
+#include "dap_circular_buffer.h"
+
+
+void dap_circular_test_run()
+{
+    dap_print_module_name("dap_circular");
+}
--- test/core/main.c
+++ test/core/main.c
@@ -1,12 +1,14 @@
 #include "dap_config_test.h"
 #include "dap_common_test.h"
 #include "dap_network_monitor_test.h"
+#include "dap_circular_test.h"
 #include "dap_common.h"
 
 
 int main(void) {
     // switch off debug info from library
     set_log_level(L_CRITICAL);
+    dap_circular_test_run();
     dap_config_tests_run();
     dap_common_test_run();
 #ifdef __linux__
