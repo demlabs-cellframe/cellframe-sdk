b5966a5d1b9d38aa124514284fd096783856ac10
armatusmiles <akurotych@gmail.com>
2019-02-09 02:36:54 +0700

    [+] dap_http_simple_test skeleton

---
+++ test/http_server/dap_http_simple_test.c
@@ -0,0 +1,6 @@
+#include "dap_http_simple_test.h"
+
+void dap_http_http_simple_test_run()
+{
+    dap_print_module_name("dap_http_http_simple_test");
+}
---
+++ test/http_server/dap_http_simple_test.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "dap_test.h"
+#include "dap_http_simple.h"
+
+void dap_http_http_simple_test_run(void);
