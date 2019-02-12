9c9b3aad76d90952073dda34adad1f5bdf2d5043
Aleksandr Lysikov <lysikov@inbox.ru>
2019-02-12 15:19:38 +0500

    add struct dap_global_db_obj_t modify dap_chain_global_db_load()

--- dap_chain_global_db.c
+++ dap_chain_global_db.c
@@ -10,6 +10,32 @@
 // for access from several streams
 static pthread_mutex_t ldb_mutex = PTHREAD_MUTEX_INITIALIZER;
 
+/**
+ * Clean struct dap_global_db_obj_t
+ */
+void dap_chain_global_db_obj_clean(dap_global_db_obj_t *obj)
+{
+    DAP_DELETE(obj->key);
+    DAP_DELETE(obj->value);
+    obj->key = NULL;
+    obj->value = NULL;
+}
+
+/**
+ * Delete mass of struct dap_global_db_obj_t
+ */
+void dap_chain_global_db_objs_delete(dap_global_db_obj_t **objs)
+{
+    int i = 0;
+    while(objs) {
+        if(!(objs[i]))
+            break;
+        dap_chain_global_db_obj_clean(objs[i]);
+        i++;
+    }
+    DAP_DELETE(objs);
+}
+
 int dap_chain_global_db_init(dap_config_t * g_config)
 {
     const char *a_storage_path = dap_config_get_item_str(g_config, "resources", "dap_global_db_path");
@@ -40,14 +66,17 @@ char * dap_chain_global_db_gr_get(const char *a_key, const char *a_group)
     int count = 0;
     if(!a_key)
         return NULL ;
-    char query[32 + strlen(a_key)];
-    sprintf(query, "(cn=%s)(objectClass=addr_leased)", a_key);
+    ssize_t query_len = snprintf(NULL, 0, "(cn=%s)(objectClass=%s)", a_key, a_group);
+    //char query[32 + strlen(a_key)];
+    char *query = DAP_NEW_Z_SIZE(char, query_len + 1);
+    snprintf(query, query_len + 1, "(cn=%s)(objectClass=%s)", a_key, a_group);
     pthread_mutex_lock(&ldb_mutex);
     pdap_store_obj_t store_data = dap_db_read_data(query, &count, a_group);
     pthread_mutex_unlock(&ldb_mutex);
     if(count == 1 && store_data && !strcmp(store_data->key, a_key))
         str = (store_data->value) ? strdup(store_data->value) : NULL;
     dab_db_free_pdap_store_obj_t(store_data, count);
+    DAP_DELETE(query);
     return str;
 }
 char * dap_chain_global_db_get(const char *a_key)
@@ -104,29 +133,48 @@ bool dap_chain_global_db_del(const char *a_key)
  * @param data_size[out] size of output array
  * @return array (note:not Null-terminated string) on NULL in case of an error
  */
-uint8_t* dap_chain_global_db_gr_load(size_t *data_size, const char *a_group)
+dap_global_db_obj_t** dap_chain_global_db_gr_load(size_t *data_size, const char *a_group)
 {
-    const char *query = "(objectClass=addr_leased)";
+    ssize_t query_len = snprintf(NULL, 0, "(objectClass=%s)", a_group);
+    char *query = DAP_NEW_Z_SIZE(char, query_len + 1);
+    //const char *query = "(objectClass=addr_leased)";
+    snprintf(query, query_len + 1, "(objectClass=%s)", a_group);
     int count = 0;
     // Read data
     pthread_mutex_lock(&ldb_mutex);
     pdap_store_obj_t store_obj = dap_db_read_data(query, &count, a_group);
     pthread_mutex_unlock(&ldb_mutex);
+    DAP_DELETE(query);
     // Serialization data
     dap_store_obj_pkt_t *pkt = dap_store_packet_multiple(store_obj, count);
     dab_db_free_pdap_store_obj_t(store_obj, count);
     if(pkt)
     {
-        uint8_t *data = DAP_NEW_SIZE(uint8_t, pkt->data_size);
-        memcpy(data, pkt->data, pkt->data_size);
-        *data_size = pkt->data_size;
+        int count_new = 0;
+        pdap_store_obj_t store_data = dap_store_unpacket(pkt, &count_new);
+        assert(count_new == count);
+        //char **data = DAP_NEW_SIZE(char*, (count_new + 1) * sizeof(char*));
+        dap_global_db_obj_t **data = DAP_NEW_Z_SIZE(dap_global_db_obj_t*,
+                (count_new + 1) * sizeof(dap_global_db_obj_t*));// last item in mass must be zero
+        for(int i = 0; i < count_new; i++) {
+            pdap_store_obj_t store_data_cur = store_data + i;
+            assert(store_data_cur);
+            data[i] = DAP_NEW(dap_global_db_obj_t);
+            data[i]->key = strdup(store_data_cur->key);
+            data[i]->value = strdup(store_data_cur->value);
+        }
+        DAP_DELETE(store_data);
         DAP_DELETE(pkt);
+        if(data_size)
+            *data_size = (size_t) count_new;
         return data;
     }
+    if(data_size)
+        *data_size = 0;
     return NULL ;
 }
 
-uint8_t* dap_chain_global_db_load(size_t *data_size)
+dap_global_db_obj_t** dap_chain_global_db_load(size_t *data_size)
 {
     return dap_chain_global_db_gr_load(data_size, GROUP_NAME_DEFAULT);
 }
@@ -142,7 +190,7 @@ bool dap_chain_global_db_gr_save(uint8_t* data, size_t data_size, const char *a_
     int a = sizeof(dap_store_obj_pkt_t);
     int count = 0;
     dap_store_obj_pkt_t *pkt = DAP_NEW_Z_SIZE(dap_store_obj_pkt_t, sizeof(dap_store_obj_pkt_t) + data_size);
-    //pkt->count = 0;
+//pkt->count = 0;
     pkt->data_size = data_size;
     memcpy(pkt->data, data, data_size);
     pdap_store_obj_t store_data = dap_store_unpacket(pkt, &count);
@@ -171,6 +219,8 @@ bool dap_chain_global_db_save(uint8_t* data, size_t data_size)
  */
 char* dap_chain_global_db_hash(const uint8_t *data, size_t data_size)
 {
+    if(!data || data_size <= 0)
+        return NULL ;
     dap_chain_hash_t a_hash;
     dap_hash((char*) data, data_size, a_hash.raw, sizeof(a_hash.raw), DAP_HASH_TYPE_SLOW_0);
     size_t a_str_max = (sizeof(a_hash.raw) + 1) * 2 + 2; /* heading 0x */
--- dap_chain_global_db.h
+++ dap_chain_global_db.h
@@ -2,6 +2,8 @@
 
 #include <stdint.h>
 #include <stdbool.h>
+
+#include "dap_common.h"
 #include "dap_config.h"
 
 #define GROUP_NODE "addrs_leased"
@@ -9,6 +11,21 @@
 #define GROUP_DATUM "datums"
 
 #define GROUP_NAME_DEFAULT GROUP_DATUM
+
+typedef struct dap_global_db_obj {
+    char *key;
+    char *value;
+}DAP_ALIGN_PACKED dap_global_db_obj_t, *pdap_global_db_obj_t;
+
+/**
+ * Clean struct dap_global_db_obj_t
+ */
+void dap_chain_global_db_obj_clean(dap_global_db_obj_t *obj);
+
+/**
+ * Delete mass of struct dap_global_db_obj_t
+ */
+void dap_chain_global_db_objs_delete(dap_global_db_obj_t **objs);
 
 int dap_chain_global_db_init(dap_config_t * a_config);
 
@@ -38,8 +55,8 @@ bool dap_chain_global_db_del(const char *a_key);
  * @param data_size[out] size of output array
  * @return array (note:not Null-terminated string) on NULL in case of an error
  */
-uint8_t* dap_chain_global_db_gr_load(size_t *data_size, const char *a_group);
-uint8_t* dap_chain_global_db_load(size_t *data_size);
+dap_global_db_obj_t** dap_chain_global_db_gr_load(size_t *data_size, const char *a_group);
+dap_global_db_obj_t** dap_chain_global_db_load(size_t *data_size);
 
 /**
  * Write to the database from an array of data_size bytes
