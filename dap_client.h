d33fdc54a5fc56a1825270a2fd203612867dbb4f
Aleksandr Lysikov <lysikov@inbox.ru>
2019-03-10 01:03:50 +0500

    fixed define "stream_ctl"

--- dap_client.h
+++ dap_client.h
@@ -76,7 +76,7 @@ typedef void (*dap_client_callback_data_size_t) (dap_client_t *, void *, size_t)
 
 #define DAP_UPLINK_PATH_ENC_INIT         "enc_init" //"1901248124123459"
 #define DAP_UPLINK_PATH_DB               "01094787531354"
-#define DAP_UPLINK_PATH_STREAM_CTL       "091348758013553"
+#define DAP_UPLINK_PATH_STREAM_CTL       "stream_ctl" //"091348758013553"
 #define DAP_UPLINK_PATH_STREAM           "874751843144"
 #define DAP_UPLINK_PATH_LICENSE          "license"
 #define DAP_UPLINK_PATH_SERVER_LIST      "slist"
