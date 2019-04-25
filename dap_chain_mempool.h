4d156a2fd416f2975c65ad7c025e09ad9fa7e093
Dmtiriy Gerasimov <naeper@demlabs.net>
2019-04-25 10:29:56 +0700

    [*] Refactoring, renames, added authors

--- dap_chain_mempool.h
+++ dap_chain_mempool.h
@@ -15,7 +15,6 @@
  */
 
 #define DAP_DATUM_MEMPOOL_VERSION "01"
-#define DAP_CHAIN_GDB_GROUP_DATUM_POOL "datum_pool"
 
 // action
 enum {
@@ -29,6 +28,10 @@ typedef struct dap_datum_mempool {
     dap_chain_datum_t **data;// mass of datums
 }DAP_ALIGN_PACKED dap_datum_mempool_t;
 
+int dap_datum_mempool_init();
+
+extern const char* c_dap_datum_mempool_gdb_group;
+
 uint8_t* dap_datum_mempool_serialize(dap_datum_mempool_t *datum_mempool, size_t *size);
 dap_datum_mempool_t * dap_datum_mempool_deserialize(uint8_t *datum_mempool_str, size_t size);
 
@@ -37,3 +40,9 @@ void dap_datum_mempool_free(dap_datum_mempool_t *datum);
 
 void dap_chain_mempool_add_proc(struct dap_http * sh, const char * url);
 
+int dap_chain_mempool_tx_create(dap_enc_key_t *a_key_from,
+        const dap_chain_addr_t* a_addr_from, const dap_chain_addr_t* a_addr_to, const dap_chain_addr_t* a_addr_fee,
+                                const char a_token_ticker[10],
+        uint64_t a_value, uint64_t a_value_fee);
+
+int dap_chain_mempool_datum_add(dap_chain_datum_t * a_datum);
