cc4daa32b13324990863e08110bb18c3ceebb9e3
Dmitriy Gerasimov <naeper@demlabs.net>
2019-05-17 19:53:17 +0700

    [*] Lot of reworks related with node-node connections

--- dap_chain_node_client.c
+++ dap_chain_node_client.c
@@ -49,7 +49,10 @@
 
 static int listen_port_tcp = 8079;
 static void s_stage_end_callback(dap_client_t *a_client, void *a_arg);
-static void s_ch_chain_callback_notify_packet(dap_stream_ch_chain_t*, uint8_t a_pkt_type,
+static void s_ch_chain_callback_notify_packet_out(dap_stream_ch_chain_t*, uint8_t a_pkt_type,
+                                                      dap_stream_ch_chain_pkt_t *a_pkt, size_t a_pkt_data_size,
+                                                      void * a_arg);
+static void s_ch_chain_callback_notify_packet_in(dap_stream_ch_chain_t* a_ch_chain, uint8_t a_pkt_type,
                                                       dap_stream_ch_chain_pkt_t *a_pkt, size_t a_pkt_data_size,
                                                       void * a_arg);
 
@@ -124,19 +127,25 @@ static void s_stage_end_callback(dap_client_t *a_client, void *a_arg)
     dap_chain_node_client_t *l_node_client = a_client->_inheritor;
     assert(l_node_client);
     if(l_node_client) {
+        log_it(L_NOTICE,"Stream connection with node 0x%016X established", l_node_client->remote_node_addr.uint64);
         pthread_mutex_lock(&l_node_client->wait_mutex);
         l_node_client->state = NODE_CLIENT_STATE_CONNECTED;
 
         dap_stream_ch_t * l_ch = dap_client_get_stream_ch( a_client , dap_stream_ch_chain_get_id() );
         if (l_ch){
             dap_stream_ch_chain_t * l_ch_chain = DAP_STREAM_CH_CHAIN(l_ch);
-            l_ch_chain->notify_callback = s_ch_chain_callback_notify_packet;
-            l_ch_chain->notify_callback_arg = l_node_client;
+            l_ch_chain->callback_notify_packet_out = s_ch_chain_callback_notify_packet_out;
+            l_ch_chain->callback_notify_packet_in = s_ch_chain_callback_notify_packet_in;
+            l_ch_chain->callback_notify_arg = l_node_client;
+        }else {
+            log_it(L_WARNING,"No ch_chain channel, can't init notify callback for pkt type CH_CHAIN");
         }
+
         pthread_mutex_unlock(&l_node_client->wait_mutex);
         if ( l_node_client->callback_connected )
             l_node_client->callback_connected(l_node_client,a_arg);
         l_node_client->keep_connection = true;
+        log_it(L_DEBUG,"Wakeup all who waits");
         pthread_cond_signal(&l_node_client->wait_cond);
     }
 }
@@ -148,10 +157,73 @@ static void s_stage_end_callback(dap_client_t *a_client, void *a_arg)
  * @param a_pkt_data_size
  * @param a_arg
  */
-static void s_ch_chain_callback_notify_packet(dap_stream_ch_chain_t* a_ch_chain, uint8_t a_pkt_type,
+static void s_ch_chain_callback_notify_packet_in(dap_stream_ch_chain_t* a_ch_chain, uint8_t a_pkt_type,
                                                       dap_stream_ch_chain_pkt_t *a_pkt, size_t a_pkt_data_size,
                                                       void * a_arg)
 {
+    dap_chain_node_client_t * l_node_client = (dap_chain_node_client_t *) a_arg;
+    switch (a_pkt_type) {
+        case DAP_STREAM_CH_CHAIN_PKT_TYPE_SYNCED_ALL:
+        case DAP_STREAM_CH_CHAIN_PKT_TYPE_SYNCED_GLOBAL_DB:
+        case DAP_STREAM_CH_CHAIN_PKT_TYPE_SYNCED_CHAINS:{
+            dap_stream_ch_chain_sync_request_t * l_request = NULL;
+            //if ( a_pkt_data_size == sizeof ( *l_request))
+            //     l_request = (dap_stream_ch_chain_sync_request_t* ) a_pkt->data;
+
+            if ( l_request ){
+                if ( l_request->ts_start < (uint64_t) dap_db_log_get_last_timestamp() ){
+                    log_it(L_INFO, "Remote is synced all but we have updates for it");
+                    // Get log diff
+                    a_ch_chain->request_last_ts = dap_db_log_get_last_timestamp();
+                    dap_list_t *l_list = dap_db_log_get_list((time_t) l_request->ts_start);
+
+                    if ( l_list ) {
+                        // Add it to outgoing list
+                        l_list->next = a_ch_chain->request_global_db_trs;
+                        a_ch_chain->request_global_db_trs = l_list;
+                        a_ch_chain->request_net_id.uint64 = a_pkt->hdr.net_id.uint64;
+                        a_ch_chain->request_cell_id.uint64 = a_pkt->hdr.cell_id.uint64;
+                        a_ch_chain->request_chain_id.uint64 = a_pkt->hdr.chain_id.uint64;
+                        a_ch_chain->state = CHAIN_STATE_SYNC_GLOBAL_DB ;
+
+                        log_it(L_INFO, "Sync for remote tr_count=%d",dap_list_length(l_list));
+                        dap_stream_ch_set_ready_to_write(a_ch_chain->ch, true);
+                    }
+                }else {
+                    log_it(L_INFO, "Remote node has lastes ts for us");
+                    pthread_mutex_lock(&l_node_client->wait_mutex);
+                    l_node_client->state = NODE_CLIENT_STATE_SYNCED;
+                    pthread_mutex_unlock(&l_node_client->wait_mutex);
+                    pthread_cond_signal(&l_node_client->wait_cond);
+                }
+            }else {
+                log_it(L_INFO, "Sync notify without request to sync back, stay in SYNCED state");
+                pthread_mutex_lock(&l_node_client->wait_mutex);
+                l_node_client->state = NODE_CLIENT_STATE_SYNCED;
+                pthread_mutex_unlock(&l_node_client->wait_mutex);
+                pthread_cond_signal(&l_node_client->wait_cond);
+            }
+
+        }
+        default:{}
+    }
+}
+
+/**
+ * @brief s_ch_chain_callback_notify_packet_in
+ * @param a_ch_chain
+ * @param a_pkt_type
+ * @param a_pkt
+ * @param a_pkt_data_size
+ * @param a_arg
+ */
+static void s_ch_chain_callback_notify_packet_out(dap_stream_ch_chain_t* a_ch_chain, uint8_t a_pkt_type,
+                                                      dap_stream_ch_chain_pkt_t *a_pkt, size_t a_pkt_data_size,
+                                                      void * a_arg)
+{
+    (void) a_pkt;
+    (void) a_pkt_data_size;
+    (void) a_ch_chain;
     dap_chain_node_client_t * l_node_client = (dap_chain_node_client_t *) a_arg;
     switch (a_pkt_type) {
         case DAP_STREAM_CH_CHAIN_PKT_TYPE_SYNCED_ALL:
@@ -161,11 +233,10 @@ static void s_ch_chain_callback_notify_packet(dap_stream_ch_chain_t* a_ch_chain,
             l_node_client->state = NODE_CLIENT_STATE_SYNCED;
             pthread_mutex_unlock(&l_node_client->wait_mutex);
             pthread_cond_signal(&l_node_client->wait_cond);
-        }
+        }break;
         default:{}
     }
 }
-
 
 /**
  * Create connection to server
@@ -186,7 +257,7 @@ dap_chain_node_client_t* dap_chain_node_client_connect(dap_chain_node_info_t *no
     l_node_client->events = NULL; //dap_events_new();
     l_node_client->client = dap_client_new(l_node_client->events, stage_status_callback, s_stage_status_error_callback);
     l_node_client->client->_inheritor = l_node_client;
-    dap_client_set_active_channels(l_node_client->client,"NC");
+    dap_client_set_active_channels(l_node_client->client,"CN");
 
     int hostlen = 128;
     char host[hostlen];
@@ -207,11 +278,11 @@ dap_chain_node_client_t* dap_chain_node_client_connect(dap_chain_node_info_t *no
     }
     dap_client_set_uplink(l_node_client->client, strdup(host), listen_port_tcp);
 //    dap_client_stage_t a_stage_target = STAGE_ENC_INIT;
-    dap_client_stage_t a_stage_target = STAGE_STREAM_STREAMING;
+    dap_client_stage_t l_stage_target = STAGE_STREAM_STREAMING;
 
     l_node_client->state = NODE_CLIENT_STATE_CONNECT;
     // Handshake & connect
-    dap_client_go_stage(l_node_client->client, a_stage_target, s_stage_end_callback);
+    dap_client_go_stage(l_node_client->client, l_stage_target, s_stage_end_callback);
     return l_node_client;
 }
 
@@ -284,6 +355,7 @@ int dap_chain_node_client_wait(dap_chain_node_client_t *a_client, int a_waited_s
         to.tv_nsec = (long) nsec_new;
     // signal waiting
     do {
+
         int wait = pthread_cond_timedwait(&a_client->wait_cond, &a_client->wait_mutex, &to);
         if(wait == 0 && a_client->state == a_waited_state) {
             ret = 1;
