4024a0a068a92238d25aac3bd6acd4c765ea99b3
Dmitriy Gerasimov <naeper@demlabs.net>
2019-05-22 17:04:54 +0700

    [-] Removed all global groups. Now they all are tracking with stream_ch_chain [+] callback_arg setup for all notify callback calls [*] Smaller fixes

--- dap_chain_global_db_hist.c
+++ dap_chain_global_db_hist.c
@@ -121,7 +121,7 @@ bool dap_db_history_add(char a_type, pdap_store_obj_t a_store_obj, size_t a_dap_
     l_store_data.key = dap_db_history_timestamp();
     l_store_data.value = (uint8_t*) strdup(l_str) ;
     l_store_data.value_len = l_str_len+1;
-    l_store_data.group = GROUP_GLOBAL_HISTORY;
+    l_store_data.group = GROUP_LOCAL_HISTORY;
     l_store_data.timestamp = time(NULL);
     int l_res = dap_db_add(&l_store_data, 1);
     if(l_rec.keys_count > 1)
@@ -148,7 +148,7 @@ time_t dap_db_log_get_last_timestamp(void)
 {
     char *last_key = NULL;
     size_t l_data_size_out = 0;
-    dap_global_db_obj_t **l_objs = dap_chain_global_db_gr_load(GROUP_GLOBAL_HISTORY, &l_data_size_out);
+    dap_global_db_obj_t **l_objs = dap_chain_global_db_gr_load(GROUP_LOCAL_HISTORY, &l_data_size_out);
     if(l_data_size_out > 0)
         last_key = l_objs[0]->key;
     for(size_t i = 1; i < l_data_size_out; i++) {
@@ -181,7 +181,7 @@ dap_list_t* dap_db_log_get_list(time_t first_timestamp)
     size_t l_list_count = 0;
     char *l_first_key_str = dap_strdup_printf("%lld", (int64_t) first_timestamp);
     size_t l_data_size_out = 0;
-    dap_global_db_obj_t **l_objs = dap_chain_global_db_gr_load(GROUP_GLOBAL_HISTORY, &l_data_size_out);
+    dap_global_db_obj_t **l_objs = dap_chain_global_db_gr_load(GROUP_LOCAL_HISTORY, &l_data_size_out);
     for(size_t i = 0; i < l_data_size_out; i++) {
         dap_global_db_obj_t *l_obj_cur = l_objs[i];
 //        log_it(L_DEBUG,"%lld and %lld tr",strtoll(l_obj_cur->key,NULL,10), first_timestamp );
--- dap_chain_global_db_pvt.c
+++ dap_chain_global_db_pvt.c
@@ -114,14 +114,10 @@ int dap_db_init(const char *path)
             talloc_free(msg);
 
             // level 2: groups
-            dap_db_group_create( GROUP_GLOBAL_ADDRS_LEASED);
-            dap_db_group_create( GROUP_GLOBAL_ALIAS);
-            dap_db_group_create( GROUP_GLOBAL_DATUM);
-            dap_db_group_create( GROUP_GLOBAL_HISTORY);
-            dap_db_group_create( GROUP_GLOBAL_ADDRS_LEASED );
+            dap_db_group_create( GROUP_LOCAL_HISTORY);
+            dap_db_group_create( GROUP_LOCAL_GENERAL );
+            dap_db_group_create( GROUP_LOCAL_NODE_LAST_TS);
 
-            talloc_free(msg->dn);
-            talloc_free(msg);
         }
         talloc_free(data_message);
         DAP_DELETE(l_tdb_path);
