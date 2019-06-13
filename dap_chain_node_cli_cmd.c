6fbbc9479c5ea39e454f43e026c3c7e8e9397f80
p-const <p.const@bk.ru>
2019-06-14 00:50:54 +0300

    balance calc fixed

--- dap_chain_node_cli_cmd.c
+++ dap_chain_node_cli_cmd.c
@@ -1267,7 +1267,9 @@ int com_tx_wallet(int argc,  char ** argv, char **str_reply)
 
             size_t l_addr_tokens_size = 0;
             char **l_addr_tokens = NULL;
-            dap_chain_ledger_addr_get_token_ticker_all(l_ledger, l_addr, &l_addr_tokens, &l_addr_tokens_size);
+            //dap_chain_ledger_addr_get_token_ticker_all(l_ledger, l_addr, &l_addr_tokens, &l_addr_tokens_size);
+            // seriously?...
+            dap_chain_ledger_addr_get_token_ticker_all_fast(l_ledger, l_addr, &l_addr_tokens, &l_addr_tokens_size);
             if(l_addr_tokens_size > 0)
                 dap_string_append_printf(l_string_ret, "balance:\n");
             else
