08987d5c6be9090e0ba10a46912f10cd29556364
ANTA <arcticshine999@gmail.com>
2019-07-15 19:13:51 +0400

    1.1-11-Win

--- dap_chain_gdb.c
+++ dap_chain_gdb.c
@@ -26,6 +26,19 @@
 #include <stdbool.h>
 #include <pthread.h>
 
+#ifdef _WIN32
+#undef _WIN32_WINNT
+#define _WIN32_WINNT 0x0600
+#include <winsock2.h>
+#include <windows.h>
+#include <mswsock.h>
+#include <ws2tcpip.h>
+#include <io.h>
+#include <time.h>
+#include <wepoll.h>
+#include <pthread.h>
+#endif
+
 #include "utlist.h"
 
 #include "dap_common.h"
