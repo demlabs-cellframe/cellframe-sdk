d2f249c4e87e98e189db5137f29ea700bc37dbab
Dmitriy A. Gerasimov <dmitriy.gerasimov@demlabs.net>
2019-11-14 17:15:03 +0700

    [*] License updated

--- dap_chain_net_srv_order.h
+++ dap_chain_net_srv_order.h
@@ -1,25 +1,25 @@
 /*
 * Authors:
-* Dmitrii Gerasimov <naeper@demlabs.net>
+* Dmitriy Gerasimov <naeper@demlabs.net>
+* Cellframe       https://cellframe.net
 * DeM Labs Inc.   https://demlabs.net
-* Cellframe https://cellframe.net
 * Copyright  (c) 2017-2019
 * All rights reserved.
 
-This file is part of DAP (Deus Applications Prototypes) the open source project
+This file is part of CellFrame SDK the open source project
 
-DAP (Deus Applicaions Prototypes) is free software: you can redistribute it and/or modify
+CellFrame SDK is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-DAP is distributed in the hope that it will be useful,
+CellFrame SDK is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with any DAP based project.  If not, see <http://www.gnu.org/licenses/>.
+along with any CellFrame SDK based project.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #pragma once
