d7401c1493dbbd86e843c639182469c09331d077
Constantin Papizh <p.const@bk.ru>
2019-11-28 22:21:07 +0300

    wepoll issues

--- enc_server/dap_enc_http.c
+++ enc_server/dap_enc_http.c
@@ -25,15 +25,12 @@
 #include "dap_common.h"
 
 #ifdef _WIN32
-#undef _WIN32_WINNT
-#define _WIN32_WINNT 0x0600
 #include <winsock2.h>
 #include <windows.h>
 #include <mswsock.h>
 #include <ws2tcpip.h>
 #include <io.h>
 #include <time.h>
-#include <wepoll.h>
 #endif
 
 #include <pthread.h>
