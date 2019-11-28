8de30467285db82cdeef34e95eae60efecec95d3
Constantin Papizh <p.const@bk.ru>
2019-11-28 22:19:02 +0300

    wepoll issues

--- dap_chain_node_cli.c
+++ dap_chain_node_cli.c
@@ -46,14 +46,11 @@ typedef int SOCKET;
 #define INVALID_SOCKET  -1  // for win32 =  (SOCKET)(~0)
 // for Windows
 #else
-#undef _WIN32_WINNT
-#define _WIN32_WINNT 0x0600
 #include <winsock2.h>
 #include <windows.h>
 #include <mswsock.h>
 #include <ws2tcpip.h>
 #include <io.h>
-#include <wepoll.h>
 #endif
 
 #include <pthread.h>
