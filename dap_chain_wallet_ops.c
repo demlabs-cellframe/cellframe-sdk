3fcaedb1f33a3db1bde6050f824fdabf07e9ad34
Constantin Papizh <p.const@bk.ru>
2019-11-28 21:47:48 +0300

    wepoll issues

--- dap_chain_wallet_ops.c
+++ dap_chain_wallet_ops.c
@@ -9,14 +9,11 @@
 #include <errno.h>
 
 #ifdef WIN32
-#undef _WIN32_WINNT
-#define _WIN32_WINNT 0x0600
 #include <winsock2.h>
 #include <windows.h>
 #include <mswsock.h>
 #include <ws2tcpip.h>
 #include <io.h>
-#include <wepoll.h>
 #endif
 
 #include <pthread.h>
