87d5ba7e651bbfd7f8a835c7b5ddb171ee0f8d7e
Alеxаnder Lysikоv <alexander.lysikov@demlabs.net>
2019-12-27 23:05:13 +0500

    removed unnecessary variables

--- dap_chain_net.c
+++ dap_chain_net.c
@@ -113,7 +113,6 @@ typedef struct dap_chain_net_pvt{
     dap_chain_node_info_t * node_info; // Current node's info
 
     dap_chain_node_client_t * links;
-    size_t link_curdel;
     size_t links_count;
 
     dap_chain_node_addr_t *links_addrs;
