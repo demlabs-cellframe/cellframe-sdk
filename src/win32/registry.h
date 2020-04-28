2945a6b50667d3b79158736fb3e17d7eee440e49
danil.martynenko <danil.martynenko@demlabs.net>
2020-04-28 16:23:30 +0300

    bugs-3405 | Added restart service

--- src/win32/registry.h
+++ src/win32/registry.h
@@ -13,6 +13,7 @@ wchar_t* readRegKey(HKEY hKey, LPCWSTR regSubKey, LPCWSTR val);
 wchar_t* getTapGUID();
 wchar_t* getTapName();
 wchar_t* getUserSID(LPCWSTR homePath);
+wchar_t* shGetUsrPath();
 
 wchar_t*    regWGetUsrPath();
 char*       regGetUsrPath();
