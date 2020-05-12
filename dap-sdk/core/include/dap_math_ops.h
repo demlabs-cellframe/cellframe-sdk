1742af05c8a9f4da6cc73e1d685d2da5a858b08f
Dmitriy A. Gerasimov <dmitriy.gerasimov@demlabs.net>
2020-05-12 21:48:06 +0700

    [+] uint128_t and int128_t for platforms without native 128 bit integer support

--- dap-sdk/core/include/dap_math_ops.h
+++ dap-sdk/core/include/dap_math_ops.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <stdint.h>
+#include "dap_common.h"
 //#include "common/int-util.h"
 
 #if defined(__GNUC__) ||defined (__clang__)
@@ -11,9 +12,13 @@ typedef __int128 _dap_int128_t;
 
 #if !defined (int128_t)
 typedef __int128 int128_t;
+#else
+typedef struct int128{ int64_t i64[2]; } DAP_ALIGIN_PACKED int128_t;
 #endif
 #if !defined (uint128_t)
 typedef unsigned __int128 uint128_t;
+#else
+typedef struct uint128{ uint64_t ui64[2]; } DAP_ALIGIN_PACKED uint128_t;
 #else
 typedef unsigned uint64_t[2] uint128_t;
 #endif
