f51b463d3804ecd5628ce6168dc66d4eecb11041
Dmitriy A. Gerasimov <dmitriy.gerasimov@demlabs.net>
2020-05-13 13:06:09 +0700

    [*] Fixed 128 bits

--- dap-sdk/core/include/dap_math_ops.h
+++ dap-sdk/core/include/dap_math_ops.h
@@ -13,15 +13,16 @@ typedef __int128 _dap_int128_t;
 #if !defined (int128_t)
 typedef __int128 int128_t;
 #else
-typedef struct int128{ int64_t i64[2]; } DAP_ALIGIN_PACKED int128_t;
+typedef unsigned int64_t[2] int128_t;
 #endif
+
 #if !defined (uint128_t)
 typedef unsigned __int128 uint128_t;
 #else
-typedef struct uint128{ uint64_t ui64[2]; } DAP_ALIGIN_PACKED uint128_t;
-#else
 typedef unsigned uint64_t[2] uint128_t;
 #endif
+
+
 #endif
 #endif
 
