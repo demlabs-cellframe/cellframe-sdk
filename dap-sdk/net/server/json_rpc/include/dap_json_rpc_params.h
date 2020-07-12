8fed6079a173b9052bfe359fadbee72141348567
alexey.stratulat <alexey.stratulat@demlabs.net>
2020-07-12 15:57:17 +0700

    [+] Added GNU license handler.

--- dap-sdk/net/server/json_rpc/include/dap_json_rpc_params.h
+++ dap-sdk/net/server/json_rpc/include/dap_json_rpc_params.h
@@ -1,7 +1,35 @@
+/*
+ * Authors:
+ * Alexey V. Stratulat <alexey.stratulat@demlabs.net>
+ * DeM Labs Inc.   https://demlabs.net
+ * DeM Labs Open source community https://gitlab.demlabs.net/cellframe/cellframe-sdk
+ * Copyright  (c) 2017-2020
+ * All rights reserved.
+
+ This file is part of DAP (Deus Applications Prototypes) the open source project
+
+    DAP (Deus Applicaions Prototypes) is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    DAP is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with any DAP based project.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #pragma once
 #include "dap_common.h"
 #include "dap_strfuncs.h"
 #include "json-c/json.h"
+
+#ifdef __cplusplus
+extern "C"{
+#endif
 
 typedef enum dap_json_rpc_type_param{
     TYPE_PARAM_NULL,
@@ -45,5 +73,6 @@ void dap_json_rpc_param_remove(dap_json_rpc_param_t *param);
 dap_json_rpc_params_t * dap_json_rpc_params_create_from_array_list(json_object *a_array_list);
 char *dap_json_rpc_params_get_string_json(dap_json_rpc_params_t * a_params);
 
-
-
+#ifdef __cplusplus
+}
+#endif
