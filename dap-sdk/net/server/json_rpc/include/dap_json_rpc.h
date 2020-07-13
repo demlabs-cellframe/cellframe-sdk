12157bfb57d6cf8adb45b4c37770b2a005df1fcc
alexey.stratulat <alexey.stratulat@demlabs.net>
2020-07-13 17:25:35 +0700

    [+] Added functions for reading and writing headers HTTP.

--- dap-sdk/net/server/json_rpc/include/dap_json_rpc.h
+++ dap-sdk/net/server/json_rpc/include/dap_json_rpc.h
@@ -1,12 +1,13 @@
 #pragma once
-#include "dap_common.h"
-#include "dap_config.h"
-#include "utlist.h"
+#include "dap_http.h"
+#include "dap_strfuncs.h"
 
-#undef LOG_TAG
-#define LOG_TAG "dap_json_rpc"
+typedef enum dap_json_rpc_version{
+    RPC_VERSION_1
+}dap_json_rpc_version_t;
 
 const char* v_specification = "1.0";
 
 int dap_json_rpc_init();
 void dap_json_rpc_deinit();
+void dap_json_rpc_add_proc_http(struct dap_http *sh, const char *URL);
--- dap-sdk/net/server/json_rpc/src/dap_json_rpc.c
+++ dap-sdk/net/server/json_rpc/src/dap_json_rpc.c
@@ -2,6 +2,21 @@
 
 static bool init_module = false;
 
+//void _dap_json_rpc_http_new_callback(dap_http_t *a_http, void *s_args);
+//void _dap_json_rpc_http_delete_callback(dap_http_t *a_http, void *s_args);
+void _dap_json_rpc_http_headers_read_callback(dap_http_client_t *a_http_client, void *a_args);
+void _dap_json_rpc_http_headers_write_callback(dap_http_client_t *a_http_client, void *a_args);
+void _dap_json_rpc_http_data_read_callback(dap_http_client_t *a_http_client, void *a_args);
+void _dap_json_rpc_http_data_write_callback(dap_http_client_t *a_http_client, void *a_args);
+//void _dap_json_rpc_http_error_callback(dap_http_t *a_http, void *s_args);
+//dap_http_client_callback_t new_callback
+//                      ,dap_http_client_callback_t delete_callback
+//                      ,dap_http_client_callback_t headers_read_callback
+//                      ,dap_http_client_callback_t headers_write_callback
+//                      ,dap_http_client_callback_t data_read_callback
+//                      ,dap_http_client_callback_t data_write_callback
+//                      ,dap_http_client_callback_t error_callback
+
 int dap_json_rpc_init(){
     init_module = true;
     return 0;
@@ -9,4 +24,44 @@ int dap_json_rpc_init(){
 
 void dap_json_rpc_deinit(){
     //
+}
+
+void _dap_json_rpc_http_headers_read_callback(dap_http_client_t *a_http_client, void *a_args){
+    (void) a_args;
+    if (dap_strcmp(a_http_client->action, "POST") == 0){
+        if(dap_strcmp(a_http_client->in_content_type, "application/json") == 0){
+            a_http_client->reply_status_code = 200;
+        } else {
+            a_http_client->reply_status_code = 404;
+            strcpy(a_http_client->reply_reason_phrase, "The request must be have content type: applicaton/json.");
+        }
+    } else {
+        a_http_client->reply_status_code = 404;
+        strcpy(a_http_client->reply_reason_phrase, "The request must be executed using the POST method.");
+    }
+}
+void _dap_json_rpc_http_headers_write_callback(dap_http_client_t *a_http_client, void *a_args){
+    if (a_http_client->reply_status_code == 200){
+        dap_http_out_header_add(a_http_client,"Content-Type","application/json");
+        dap_http_out_header_add(a_http_client,"Connnection","keep-alive");
+        dap_http_out_header_add(a_http_client,"Cache-Control","no-cache");
+        a_http_client->state_read=DAP_HTTP_CLIENT_STATE_DATA;
+        dap_client_remote_ready_to_read(a_http_client->client,true);
+    }
+}
+void _dap_json_rpc_http_data_read_callback(dap_http_client_t *a_http_client, void *a_args){}
+void _dap_json_rpc_http_data_write_callback(dap_http_client_t *a_http_client, void *a_args){
+
+}
+
+void dap_json_rpc_add_proc_http(struct dap_http *sh, const char *URL){
+    dap_http_add_proc(sh, URL, NULL,
+                      NULL,
+                      NULL,
+                      _dap_json_rpc_http_headers_read_callback,
+                      _dap_json_rpc_http_headers_write_callback,
+                      _dap_json_rpc_http_data_read_callback,
+                      _dap_json_rpc_http_headers_write_callback,
+                      NULL);
+    //dap_http_add_proc(sh, URL, )
 }
