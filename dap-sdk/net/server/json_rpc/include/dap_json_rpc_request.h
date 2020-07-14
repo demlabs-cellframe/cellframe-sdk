b927a0f8384e327c580f8c094bf7258d0614510c
alexey.stratulat <alexey.stratulat@demlabs.net>
2020-07-14 16:03:25 +0700

    [+] Added GNU license handler.

--- dap-sdk/net/server/json_rpc/include/dap_json_rpc_request.h
+++ dap-sdk/net/server/json_rpc/include/dap_json_rpc_request.h
@@ -1,8 +1,36 @@
+/*
+ * Authors:
+ * Alexey V. Stratulat <alexey.stratulat@demlabs.net>
+ * DeM Labs Inc.   https://demlabs.net
+ * DeM Labs Open source community https://gitlab.demlabs.net/cellframe/cellframe-sdk
+ * Copyright  (c) 2017-2020
+ * All rights reserved.
+
+ This file is part of DAP (Deus Applications Prototypes) the open source project
+
+    DAP (Deus Applicaions Prototypes) is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    DAP is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with any DAP based project.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #pragma once
 #include "dap_common.h"
 #include "dap_json_rpc_params.h"
 #include "json-c/json.h"
 #include "dap_string.h"
+
+#ifdef __cplusplus
+extern "C"{
+#endif
 
 typedef struct dap_json_rpc_request{
     char* method;
@@ -14,3 +42,7 @@ dap_json_rpc_request_t *dap_json_rpc_request_creation(const char *a_method, dap_
 
 dap_json_rpc_request_t *dap_json_rpc_request_from_json(const char *a_data);
 char *dap_json_rpc_request_to_json(const dap_json_rpc_request_t *a_request);
+
+#ifdef __cplusplus
+}
+#endif
