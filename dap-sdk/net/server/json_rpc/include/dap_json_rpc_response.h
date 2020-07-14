3c2702e803b50a1bf8be9608e4fd1a9fb9b08b22
alexey.stratulat <alexey.stratulat@demlabs.net>
2020-07-14 22:21:56 +0700

    [+] Added function dap_json_rpc_response_set and announced other functions. Added enum dap_json_rpc_response_type_result_t.

--- dap-sdk/net/server/json_rpc/include/dap_json_rpc_response.h
+++ dap-sdk/net/server/json_rpc/include/dap_json_rpc_response.h
@@ -1,9 +1,60 @@
+/*
+ * Authors:
+ * Alexey V. Stratulat <alexey.stratulat@demlabs.net>
+ * DeM Labs Inc.   https://demlabs.net
+ * DeM Labs Open source community https://gitlab.demlabs.net/cellframe/cellframe-sdk
+ * Copyright  (c) 2017-2020
+ * All rights reserved.
+
+ This file is part of DAP (Deus Applications Prototypes) the open source project
+
+    DAP (Deus Applicaions Prototypes) is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    DAP is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with any DAP based project.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #pragma once
 
 #include "dap_json_rpc_errors.h"
+#include "dap_http.h"
+#include "json-c/json.h"
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+typedef enum dap_json_rpc_response_type_result{
+//    TYPE_RESPONSE_NULL,
+    TYPE_RESPONSE_STRING,
+    TYPE_RESPONSE_INTEGER,
+    TYPE_RESPONSE_DOUBLE,
+    TYPE_RESPONSE_BOOLEAN
+}dap_json_rpc_response_type_result_t;
 
 typedef struct dap_json_rpc_response{
-    void* result;
+    dap_json_rpc_response_type_result_t type_result;
+    char* result_string;
+    int64_t result_int;
+    double result_double;
+    bool result_boolean;
     dap_json_rpc_error_t* error;
-    void* id;
+    int id;
 }dap_json_rpc_response_t;
+
+//dap_json_rpc_response_t *dap_json_rpc_response_create(void *a_result);
+void dap_json_rpc_response_free(dap_json_rpc_response_t *a_response);
+
+void dap_json_rpc_response_send(dap_json_rpc_response_t *a_response, dap_client_remote_t *a_client_remote);
+
+#ifdef __cplusplus
+}
+#endif
---
+++ dap-sdk/net/server/json_rpc/src/dap_json_rpc_response.c
@@ -0,0 +1,37 @@
+#include "dap_json_rpc_response.h"
+
+
+void dap_json_rpc_response_send(dap_json_rpc_response_t *a_response, dap_client_remote_t *a_client_remote){
+    char *str_response = NULL;
+    json_object *l_jobj = json_object_new_object();
+    json_object *l_jobj_error = json_object_new_object();
+    json_object *l_jobj_result;
+    if (a_response->error != NULL){
+        switch (a_response->type_result) {
+        case TYPE_RESPONSE_STRING:
+            l_jobj_result = json_object_new_string(a_response->result_string);
+            break;
+        case TYPE_RESPONSE_DOUBLE:
+            l_jobj_result = json_object_new_double(a_response->result_double);
+            break;
+        case TYPE_RESPONSE_BOOLEAN:
+            l_jobj_result = json_object_new_boolean(a_response->result_boolean);
+            break;
+        case TYPE_RESPONSE_INTEGER:
+            l_jobj_result = json_object_new_int64(a_response->result_int);
+            break;
+        }
+    }else{
+        l_jobj_result = json_object_new_object();
+        json_object *l_jobj_error_code = json_object_new_int(a_response->error->code_error);
+        json_object *l_jobj_error_msg = json_object_new_string(a_response->error->msg);
+        json_object_object_add(l_jobj_error, "code", l_jobj_error_code);
+        json_object_object_add(l_jobj_error, "message", l_jobj_error_msg);
+    }
+    json_object_object_add(l_jobj, "result", l_jobj_result);
+    json_object_object_add(l_jobj, "error", l_jobj_error);
+    json_object_object_add(l_jobj, "id", json_object_new_int(a_response->id));
+    str_response = strdup(json_object_get_string(l_jobj));
+    a_client_remote->buf_out_size = strlen(str_response);
+    memcpy(a_client_remote->buf_out, str_response, a_client_remote->buf_out_size);
+}
