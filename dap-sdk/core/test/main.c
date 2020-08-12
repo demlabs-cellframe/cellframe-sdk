8bdff24a2f2025042f887727e99f9537a60a46ef
alexey.stratulat <alexey.stratulat@demlabs.net>
2020-08-12 17:34:36 +0700

    [*] Fixed warrning and errors compilation.

--- dap-sdk/core/test/main.c
+++ dap-sdk/core/test/main.c
@@ -7,6 +7,14 @@
 #include "dap_common.h"
 
 
+#ifdef __unix__
+#include "dap_process_mem_test.h"
+#include "dap_cpu_monitor_test.h"
+#include "dap_network_monitor.h"
+#include "dap_circular_test.h"
+#endif
+
+
 int main(void) {
     // switch off debug info from library
     dap_log_level_set(L_CRITICAL);
@@ -14,10 +22,6 @@ int main(void) {
     dap_config_tests_run();
     dap_common_test_run();
 #ifdef __unix__
-#include "dap_process_mem_test.h"
-#include "dap_cpu_monitor_test.h"
-#include "dap_network_monitor.h"
-#include "dap_circular_test.h"
     dap_circular_test_run();
     dap_process_mem_test_run();
     dap_cpu_monitor_test_run();
