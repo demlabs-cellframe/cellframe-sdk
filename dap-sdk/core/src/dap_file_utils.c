800c25380fb91bee3b573c6687d1b34ec952d464
Danil Martynenko <danil.martynenko@demlabs.net>
2020-10-27 14:29:26 +0000

    bugs-4270-3 [*] fixed re-creation of the log when deleting

--- dap-sdk/core/src/dap_file_utils.c
+++ dap-sdk/core/src/dap_file_utils.c
@@ -156,7 +156,8 @@ int dap_mkdir_with_parents(const char *a_dir_path)
 #ifdef _WIN32
             int result = mkdir(path);
 #else
-            int result = mkdir(path, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+            int result = mkdir(path, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH | S_IWOTH);
+                         chmod(path, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH | S_IWOTH);
 #endif
             if(result == -1) {
                 errno = ENOTDIR;
