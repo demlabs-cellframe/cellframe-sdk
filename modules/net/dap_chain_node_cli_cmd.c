f0ad7c5f6fba5369b976cf8e13b77bea09f037f3
Dmitriy A. Gerasimov <dmitriy.gerasimov@demlabs.net>
2020-10-28 19:18:58 +0700

    [+] Added 'version' command [!] Now full version is present in log files

--- modules/net/dap_chain_node_cli_cmd.c
+++ modules/net/dap_chain_node_cli_cmd.c
@@ -1476,6 +1476,25 @@ int com_ping(int argc, char** argv, void *arg_func, char **str_reply)
 }
 
 /**
+ * @brief com_version
+ * @param argc
+ * @param argv
+ * @param arg_func
+ * @param str_reply
+ * @return
+ */
+int com_version(int argc, char ** argv, void *arg_func, char **str_reply)
+{
+    (void) argc;
+    (void) argv;
+    (void) arg_func;
+    dap_chain_node_cli_set_reply_text(str_reply,
+            "%s version %s\n", dap_get_appname(), DAP_VERSION );
+    return 0;
+}
+
+
+/**
  * Help command
  */
 int com_help(int argc, char ** argv, void *arg_func, char **str_reply)
