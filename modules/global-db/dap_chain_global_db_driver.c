1a33857e3dc7a9e5143a40e88c1843c0138bd332
Constantin Papizh <p.const@bk.ru>
2021-01-26 16:22:58 +0300

    Paths refined

--- modules/global-db/dap_chain_global_db_driver.c
+++ modules/global-db/dap_chain_global_db_driver.c
@@ -81,16 +81,18 @@ int dap_db_driver_init(const char *a_driver_name, const char *a_filename_db)
     if(s_used_driver)
         dap_db_driver_deinit();
     s_used_driver = dap_strdup(a_driver_name);
+    char l_db_path_ext[strlen(a_driver_name) + strlen(a_filename_db) + 3];
+    dap_snprintf(l_db_path_ext, sizeof(l_db_path_ext), "/%s.%s", a_filename_db, a_driver_name);
     memset(&s_drv_callback, 0, sizeof(dap_db_driver_callbacks_t));
     if(!dap_strcmp(s_used_driver, "ldb"))
         l_ret = -1;
     else if(!dap_strcmp(s_used_driver, "sqlite"))
-        l_ret = dap_db_driver_sqlite_init(a_filename_db, &s_drv_callback);
+        l_ret = dap_db_driver_sqlite_init(l_db_path_ext, &s_drv_callback);
     else if(!dap_strcmp(s_used_driver, "cdb"))
-        l_ret = dap_db_driver_cdb_init(a_filename_db, &s_drv_callback);
+        l_ret = dap_db_driver_cdb_init(l_db_path_ext, &s_drv_callback);
 #ifdef DAP_CHAIN_GDB_ENGINE_MDBX
     else if(!dap_strcmp(s_used_driver, "mdbx"))
-        l_ret = dap_db_driver_mdbx_init(a_filename_db, &s_drv_callback);
+        l_ret = dap_db_driver_mdbx_init(l_db_path_ext, &s_drv_callback);
 #endif
     else
         log_it(L_ERROR, "Unknown global_db driver \"%s\"", a_driver_name);
--- modules/net/include/dap_chain_node_cli_cmd.h
+++ modules/net/include/dap_chain_node_cli_cmd.h
@@ -133,8 +133,6 @@ int com_print_log(int argc, char ** argv, void *arg_func, char **str_reply);
 int com_stats(int argc, char ** argv, void *arg_func, char **str_reply);
 
 int com_exit(int argc, char ** argv, void *arg_func, char **str_reply);
-int cmd_gdb_import(int argc, char ** argv, void *arg_func, char ** a_str_reply);
-int cmd_gdb_export(int argc, char ** argv, void *arg_func, char ** a_str_reply);
 
 int com_mempool_delete(int argc, char ** argv, void *arg_func, char ** a_str_reply);
 int com_mempool_list(int argc, char ** argv, void *arg_func, char ** a_str_reply);
