9a05930f8609dff3cc1014bf7f155468c5830eae
Dmitriy Gerasimov <naeper@demlabs.net>
2021-05-21 17:10:28 +0700

    [*] Modified error string output when can't create or open log file

--- dap-sdk/core/src/dap_common.c
+++ dap-sdk/core/src/dap_common.c
@@ -235,7 +235,7 @@ int dap_common_init( const char *a_console_title, const char *a_log_file_path, c
         if( s_log_file == NULL)
             s_log_file = fopen( a_log_file_path , "w" );
         if ( s_log_file == NULL ) {
-            dap_fprintf( stderr, "Can't open log file %s to append\n", a_log_file_path );
+            dap_fprintf( stderr, "Can't open log file %s \n", a_log_file_path );
             return -1;   //switch off show log in cosole if file not open
         }
         dap_stpcpy(s_log_dir_path,  a_log_dirpath);
