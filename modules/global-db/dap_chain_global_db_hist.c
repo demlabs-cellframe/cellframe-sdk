752cb1613e54b9dfec7c349e1d8bb582f3a20290
Dmitriy Naidolinskiy <dmitriy.naidolinskiy@demlabs.net>
2022-01-03 16:58:38 +0000

    [+] add white groups

--- modules/global-db/dap_chain_global_db_hist.c
+++ modules/global-db/dap_chain_global_db_hist.c
@@ -28,8 +28,12 @@ typedef struct dap_tx_data{
 
 #define LOG_TAG "dap_chain_global_db_hist"
 
+#if 0
 static char **s_ban_list = NULL;
 static uint16_t s_size_ban_list = 0;
+static char **s_white_list = NULL;
+static uint16_t s_size_white_list = 0;
+#endif
 
 /**
  * @brief Packs members of a_rec structure into a single string.
@@ -279,15 +283,40 @@ dap_db_log_list_t* dap_db_log_list_start(dap_chain_node_addr_t a_addr, int a_fla
 
     static uint16_t s_size_ban_list = 0;
     static char **s_ban_list = NULL;
+
+	static uint16_t s_size_white_list = 0;
+	static char **s_white_list = NULL;
+
     static bool l_try_read_ban_list = false;
+	static bool l_try_read_white_list = false;
 
     if (!l_try_read_ban_list) {
             s_ban_list = dap_config_get_array_str(g_config, "stream_ch_chain", "ban_list_sync_groups", &s_size_ban_list);
             l_try_read_ban_list = true;
     }
+	if (!l_try_read_white_list) {
+			s_white_list = dap_config_get_array_str(g_config, "stream_ch_chain", "white_list_sync_groups", &s_size_white_list);
+			l_try_read_white_list = true;
+	}
 
-    /* delete groups from ban list */
-    if (s_size_ban_list > 0) {
+	/* delete if not condition */
+	if (s_size_white_list > 0) {
+        for (dap_list_t *l_groups = l_dap_db_log_list->groups; l_groups; ) {
+            bool l_found = false;
+            for (int i = 0; i < s_size_white_list; i++) {
+                if (!dap_fnmatch(s_white_list[i], l_groups->data, FNM_NOESCAPE)) {
+                    l_found = true;
+                    break;
+                }
+            }
+            if (!l_found) {
+                    dap_list_t *l_tmp = l_groups->next;
+                    l_dap_db_log_list->groups = dap_list_delete_link(l_dap_db_log_list->groups, l_groups);
+                    l_groups = l_tmp;
+			}
+            l_groups = dap_list_next(l_groups);
+        }
+	} else if (s_size_ban_list > 0) {
         for (dap_list_t *l_groups = l_dap_db_log_list->groups; l_groups; ) {
             bool l_found = false;
             for (int i = 0; i < s_size_ban_list; i++) {
