5955581574a27785a4f948daddf5a6e6d940394d
cellframe.docs <cellframe.docs@gmail.com>
2022-02-11 09:11:18 +0300

    Add patch for ticket 5443

--- modules/net/dap_chain_node_cli_cmd.c
+++ modules/net/dap_chain_node_cli_cmd.c
@@ -3851,6 +3851,17 @@ int com_tx_create(int argc, char ** argv, char **str_reply)
         return -1;
     }
 
+    //
+    // Check, if network ID is same as ID in destination wallet address. If not - operation is cancelled.
+    //
+
+    if (addr_to->net_id.uint64 != l_net->pub.id.uint64)
+    {
+        dap_chain_node_cli_set_reply_text(str_reply, "destination wallet network ID=0x%llx and network ID=0x%llx is not equal. Please, change network name or wallet address", 
+                                            addr_to->net_id.uint64, l_net->pub.id.uint64);
+        return -1;
+    }
+
     /*    if(!l_chain_name) {
      dap_chain_node_cli_set_reply_text(str_reply, "tx_create requires parameter '-chain'");
      return -1;
