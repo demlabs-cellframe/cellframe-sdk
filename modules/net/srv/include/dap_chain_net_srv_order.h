357c478716106f8ff3f6339c76a1ec2cb83146ec
Roman Khlopkov <roman.khlopkov@demlabs.net>
2022-02-14 17:16:45 +0300

    [*] Porting hotfix from master

--- modules/net/srv/include/dap_chain_net_srv_order.h
+++ modules/net/srv/include/dap_chain_net_srv_order.h
@@ -126,7 +126,24 @@ char *dap_chain_net_srv_order_create(dap_chain_net_t * a_net,
         dap_enc_key_t *a_key
         );
 
-int dap_chain_net_srv_order_save(dap_chain_net_t * a_net, dap_chain_net_srv_order_t *a_order);
+dap_chain_net_srv_order_t *dap_chain_net_srv_order_compose(
+        dap_chain_net_t *a_net,
+        dap_chain_net_srv_order_direction_t a_direction,
+        dap_chain_net_srv_uid_t a_srv_uid, // Service UID
+        dap_chain_node_addr_t a_node_addr, // Node address that servs the order (if present)
+        dap_chain_hash_fast_t a_tx_cond_hash, // Hash index of conditioned transaction attached with order
+        uint64_t a_price, //  service price in datoshi, for SERV_CLASS_ONCE ONCE for the whole service, for SERV_CLASS_PERMANENT  for one unit.
+        dap_chain_net_srv_price_unit_uid_t a_price_unit, // Unit of service (seconds, megabytes, etc.) Only for SERV_CLASS_PERMANENT
+        const char a_price_ticker[DAP_CHAIN_TICKER_SIZE_MAX],
+        dap_chain_time_t a_expires, // TS when the service expires
+        const uint8_t *a_ext,
+        uint32_t a_ext_size,
+        const char *a_region,
+        int8_t a_continent_num,
+        dap_enc_key_t *a_key
+        );
+
+char *dap_chain_net_srv_order_save(dap_chain_net_t *a_net, dap_chain_net_srv_order_t *a_order);
 void dap_chain_net_srv_order_dump_to_string(dap_chain_net_srv_order_t *a_order,dap_string_t * a_str_out, const char *a_hash_out_type);
 void dap_chain_net_srv_order_add_notify_callback(dap_chain_net_t *a_net, dap_global_db_obj_callback_notify_t a_callback);
 /**
