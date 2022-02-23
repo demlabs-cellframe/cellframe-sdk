ddbc56d1ea59c6821428d0ef50f29d5e66a2a2f1
Roman Khlopkov <roman.khlopkov@demlabs.net>
2022-02-23 21:29:53 +0300

    [*] Custom data service client test

--- modules/net/srv/dap_chain_net_srv_client.c
+++ modules/net/srv/dap_chain_net_srv_client.c
@@ -76,15 +76,17 @@ static void s_srv_client_callback_connected(dap_chain_node_client_t *a_node_clie
     l_srv_client->net_client = a_node_client->client;
     if (l_srv_client->callbacks.connected)
         l_srv_client->callbacks.connected(l_srv_client, l_srv_client->callbacks_arg);
-    /* Test code for get reply from server
-    const int l_test_size = 64;
-    size_t l_request_size = l_test_size + sizeof(dap_stream_ch_chain_net_srv_pkt_test_t);
-    dap_stream_ch_chain_net_srv_pkt_test_t *l_request = DAP_NEW_Z_SIZE(dap_stream_ch_chain_net_srv_pkt_test_t, l_request_size);
-    l_request->data_size = l_test_size;
-    randombytes(l_request->data, l_test_size);
-    dap_hash_fast(l_request->data, l_request->data_size, &l_request->data_hash);
-    dap_chain_net_srv_client_write(l_srv_client, DAP_STREAM_CH_CHAIN_NET_SRV_PKT_TYPE_CHECK_REQUEST, l_request, l_request_size);
-    DAP_DELETE(l_request); */
+    const char *l_test_data = "Sevice test custom data example";
+    int l_test_size = strlen(l_test_data) + 1;
+    size_t l_request_size = l_test_size + sizeof(dap_stream_ch_chain_net_srv_pkt_data_t);
+    dap_stream_ch_chain_net_srv_pkt_data_t *l_request = DAP_NEW_Z_SIZE(dap_stream_ch_chain_net_srv_pkt_data_t, l_request_size);
+    l_request->hdr.version = 1;
+    l_request->hdr.data_size = l_test_size;
+    l_request->hdr.srv_uid.uint64 = 156;
+    memcpy(l_request->data, l_test_data, l_test_size);
+    // dap_hash_fast(l_request->data, l_request->data_size, &l_request->data_hash);
+    dap_chain_net_srv_client_write(l_srv_client, DAP_STREAM_CH_CHAIN_NET_SRV_PKT_TYPE_DATA, l_request, l_request_size);
+    DAP_DELETE(l_request);
 }
 
 static void s_srv_client_callback_disconnected(dap_chain_node_client_t *a_node_client, void *a_arg)
@@ -112,7 +114,7 @@ static void s_srv_client_pkt_in(dap_stream_ch_chain_net_srv_t *a_ch_chain, uint8
     //dap_chain_node_client_t *l_node_client = (dap_chain_node_client_t *)a_arg;
     switch (a_pkt_type) {
     case DAP_STREAM_CH_CHAIN_NET_SRV_PKT_TYPE_CHECK_RESPONSE: {
-            dap_stream_ch_chain_net_srv_pkt_test_t *l_request = (dap_stream_ch_chain_net_srv_pkt_test_t *) a_pkt->data;
+            dap_stream_ch_chain_net_srv_pkt_test_t *l_request = (dap_stream_ch_chain_net_srv_pkt_test_t *)a_pkt->data;
             size_t l_request_size = l_request->data_size + sizeof(dap_stream_ch_chain_net_srv_pkt_test_t);
             if(a_pkt->hdr.size != l_request_size) {
                 log_it(L_WARNING, "Wrong request size, less or more than required");
@@ -124,6 +126,10 @@ static void s_srv_client_pkt_in(dap_stream_ch_chain_net_srv_t *a_ch_chain, uint8
     case DAP_STREAM_CH_CHAIN_NET_SRV_PKT_TYPE_RESPONSE_SUCCESS:
     case DAP_STREAM_CH_CHAIN_NET_SRV_PKT_TYPE_RESPONSE_ERROR:
         break;
+    case DAP_STREAM_CH_CHAIN_NET_SRV_PKT_TYPE_DATA: {
+        dap_stream_ch_chain_net_srv_pkt_data_t *l_response = (dap_stream_ch_chain_net_srv_pkt_data_t *)a_pkt->data;
+        log_it(L_INFO, "Service client respose with data '%s'", l_response->data);
+    }
     default:
         break;
     }
