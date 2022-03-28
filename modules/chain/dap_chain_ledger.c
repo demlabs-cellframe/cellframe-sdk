1fae0fc21ffb9b8a0c5e6213597f97889fb2f5ca
cellframe.docs <artur.khudiaev@gitlab.demlabs.net>
2022-03-28 08:56:12 +0300

    Small refactoring

--- modules/chain/dap_chain_ledger.c
+++ modules/chain/dap_chain_ledger.c
@@ -1375,8 +1375,7 @@ bool s_chain_ledger_token_tsd_check(dap_ledger_t *a_ledger, dap_chain_ledger_tok
     size_t l_tsd_size=0;
     size_t l_tsd_total_size = l_token_item->datum_token->header_private_decl.tsd_total_size;
 
-    for( size_t l_offset=0; l_offset < l_tsd_total_size;  l_offset += l_tsd_size )
-    {
+    for( size_t l_offset=0; l_offset < l_tsd_total_size;  l_offset += l_tsd_size ){
         l_tsd = (dap_tsd_t *) (((byte_t*)l_tsd ) +l_offset);
         l_tsd_size = l_tsd? dap_tsd_size(l_tsd): 0;
         if(l_tsd_size==0){
@@ -1427,8 +1426,7 @@ int dap_chain_ledger_token_emission_add(dap_ledger_t *a_ledger, byte_t *a_token_
         return DAP_CHAIN_CS_VERIFY_CODE_TX_NO_TOKEN;
 
     //additional check for private tokens
-    if ((l_token_item->type == DAP_CHAIN_DATUM_TOKEN_TYPE_PRIVATE_DECL) | (l_token_item->type == DAP_CHAIN_DATUM_TOKEN_TYPE_PRIVATE_UPDATE))
-    {
+    if ((l_token_item->type == DAP_CHAIN_DATUM_TOKEN_TYPE_PRIVATE_DECL) | (l_token_item->type == DAP_CHAIN_DATUM_TOKEN_TYPE_PRIVATE_UPDATE)){
         if (!s_chain_ledger_token_tsd_check(a_ledger, l_token_item, (dap_chain_datum_token_emission_t *)a_token_emission))
             return DAP_CHAIN_CS_VERIFY_CODE_TSD_FAILED;
     }
@@ -1460,8 +1458,7 @@ int dap_chain_ledger_token_emission_add(dap_ledger_t *a_ledger, byte_t *a_token_
                    return DAP_CHAIN_CS_VERIFY_CODE_TX_NO_EMISSION;
             }
               // if cache is disabled, we need calc tokens manually
-            if (PVT(a_ledger)->load_mode)
-            {
+            if (PVT(a_ledger)->load_mode){
                 if (!dap_config_get_item_bool_default(g_config, "ledger", "cached", true)) {
 
                         if (l_token_item->current_supply >= l_token_emission_item->datum_token_emission->hdr.value){
