3a1ba15c79db417ad0c91e66f36a13fb4fd13c39
cellframe.docs <artur.khudiaev@gitlab.demlabs.net>
2022-04-02 09:48:51 +0300

    Fix datum hex output

--- modules/common/dap_chain_datum_token.c
+++ modules/common/dap_chain_datum_token.c
@@ -292,7 +292,7 @@ dap_chain_datum_token_emission_t *dap_chain_datum_emission_read(byte_t *a_emissi
 {
     assert(a_emission_serial);
     assert(a_emission_size);
-    dap_chain_datum_token_emission_t *l_emission;
+    dap_chain_datum_token_emission_t *l_emission = NULL;
     if (((dap_chain_datum_token_emission_t *)a_emission_serial)->hdr.version == 0) {
         size_t l_emission_size = *a_emission_size;
         size_t l_old_hdr_size = sizeof(struct dap_chain_emission_header_v0);
@@ -309,6 +309,8 @@ dap_chain_datum_token_emission_t *dap_chain_datum_emission_read(byte_t *a_emissi
         (*a_emission_size) = l_emission_size;
     } else {
         l_emission = DAP_DUP_SIZE(a_emission_serial, *a_emission_size);
+        //l_emission = DAP_MALLOC(*a_emission_size - sizeof(l_emission->hdr));
+        //memcpy(a_emission_serial,l_emission,a_emission_size - sizeof(l_emission->hdr));
         if (((dap_chain_datum_token_emission_t *)a_emission_serial)->hdr.version == 1)
             l_emission->hdr.value_256 = dap_chain_uint256_from(
                         ((dap_chain_datum_token_emission_t *)a_emission_serial)->hdr.value);
