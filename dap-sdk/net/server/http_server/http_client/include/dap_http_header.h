1ddefead7508135834bd0bd93a8c825b85ba1591
Ruslan (The BadAss SysMan) Laishev <ruslan.laishev@demlabs.net>
2022-05-21 14:24:38 +0300

    [*] #6099 Save work changes

--- dap-sdk/net/server/http_server/http_client/include/dap_http_header.h
+++ dap-sdk/net/server/http_server/http_client/include/dap_http_header.h
@@ -24,10 +24,11 @@
 #pragma once
 //Structure for holding HTTP header in the bidirectional list
 typedef struct dap_http_header{
-    char *name;
-    char *value;
-    struct dap_http_header *next;
-    struct dap_http_header *prev;
+    char    *name, *value;                                                  /* Area to keep HT field name and value */
+    size_t  namesz, valuesz;                                                /* Dimension of corresponding field */
+    int     htfld;                                                          /* HTTP Field numeric Id */
+
+    struct dap_http_header *next, *prev;                                    /* List's element links */
 } dap_http_header_t;
 
 struct dap_http_client;
@@ -35,19 +36,37 @@ struct dap_http_client;
 int dap_http_header_init(); // Init module
 void dap_http_header_deinit(); // Deinit module
 
-int dap_http_header_parse(struct dap_http_client * cl_ht, const char * str);
+int dap_http_header_parse(struct dap_http_client * cl_ht, const char * ht_line, size_t ht_line_len);
 
 dap_http_header_t *dap_http_header_add(dap_http_header_t **a_top, const char *a_name, const char *a_value);
 
 dap_http_header_t * dap_http_out_header_add(struct dap_http_client * ht, const char*name, const char * value);
 dap_http_header_t * dap_http_out_header_add_f(struct dap_http_client * ht, const char*name, const char * value,...);
 
-dap_http_header_t * dap_http_header_find(dap_http_header_t * top, const char*name);
+dap_http_header_t *dap_http_header_find(dap_http_header_t * ht, const char*name);
 
 dap_http_header_t * dap_http_headers_dup(dap_http_header_t * a_top);
 
 void dap_http_header_remove(dap_http_header_t **a_top,dap_http_header_t *a_hdr);
 
 // For debug output
-void print_dap_http_headers(dap_http_header_t * top);
+void print_dap_http_headers(dap_http_header_t * a_ht);
 
+/*
+ * https://en.wikipedia.org/wiki/List_of_HTTP_header_fields
+ * Don't change order of field until u undrstand what u do
+ */
+#define HTTP$SZ_METHOD      16                                              /* POST, GET, HEAD ... */
+#define HTTP_FLD$SZ_NAME    64                                              /* Maximum HTTP Field name */
+#define HTTP_FLD$SZ_VALUE   1024                                            /* -- // -- field length */
+
+enum    {
+
+    HTTP_FLD$K_CONNECTION = 0,                                              /* Connection: Keep-Alive */
+    HTTP_FLD$K_CONTENT_TYPE,                                                /* Content-Type: application/x-www-form-urlencoded */
+    HTTP_FLD$K_CONTENT_LEN,                                                 /* Content-Length: 348 */
+    HTTP_FLD$K_COOKIE,                                                      /* Cookie: $Version=1; Skin=new; */
+
+
+    HTTP_FLD$K_EOL                                                          /* End-Of-List marker, mast be last element here */
+};
