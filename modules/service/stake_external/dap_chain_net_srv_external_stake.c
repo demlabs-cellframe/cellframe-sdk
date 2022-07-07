0275c8e9a464180f33203047d5ed04eb708999d0
davlet.sibgatullin <davlet.sibgatullin@demlabs.net>
2022-07-07 18:54:26 +0300

    [*] hendler receipt

--- modules/service/stake_external/dap_chain_net_srv_external_stake.c
+++ modules/service/stake_external/dap_chain_net_srv_external_stake.c
@@ -28,6 +28,18 @@
 
 bool dap_chain_net_srv_stake_lock_verificator(dap_chain_tx_out_cond_t *a_cond, dap_chain_datum_tx_t *a_tx, bool a_owner)
 {
+	/*if (!a_owner)
+		return false;*/
+	dap_chain_datum_tx_receipt_t *l_receipt = (dap_chain_datum_tx_receipt_t *)
+			dap_chain_datum_tx_item_get(a_tx, NULL, TX_ITEM_TYPE_RECEIPT, NULL);
+	if (!l_receipt)
+		return false;
+	dap_sign_t *l_sign = dap_chain_datum_tx_receipt_sign_get(l_receipt, l_receipt->size, 1);
+	if (!l_sign)
+		return false;
+	dap_hash_fast_t l_pkey_hash;
+	if (!dap_sign_get_pkey_hash(l_sign, &l_pkey_hash))
+		return false;
+	return dap_hash_fast_compare(&l_pkey_hash, &a_cond->subtype.srv_pay.pkey_hash);
 
-	return false;
 }
