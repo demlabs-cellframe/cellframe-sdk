527844826f439b6121a6190d073a0c10af17fcdb
alexey.stratulat <alexey.stratulat@demlabs.net>
2022-07-29 23:03:36 +0700

    [*] Fixed the work of the preprocessor and removed the include files from the function main.

--- dap-sdk/core/test/main.c
+++ dap-sdk/core/test/main.c
@@ -1,8 +1,14 @@
 #include "dap_config_test.h"
 #include "dap_common_test.h"
 #ifndef _WIN32
+#ifdef  DAP_OS_UNIX
+#include "dap_circular_test.h"
+#include "dap_process_mem_test.h"
+#include "dap_cpu_monitor_test.h"
 #ifndef DDAP_NETWORK_MONITOR_TEST_OFF
+#include "dap_network_monitor.h"
 #include "dap_network_monitor_test.h"
+#endif
 #endif
 #endif
 #include "dap_strfuncs_test.h"
@@ -15,13 +21,7 @@ int main(void) {
     dap_strfuncs_tests_run();
     dap_config_tests_run();
     dap_common_test_run();
-#ifdef __unix__
-#include "dap_process_mem_test.h"
-#include "dap_cpu_monitor_test.h"
-#ifndef DDAP_NETWORK_MONITOR_TEST_OFF
-#include "dap_network_monitor.h"
-#endif
-#include "dap_circular_test.h"
+#ifdef DAP_OS_UNIX
     dap_circular_test_run();
     dap_process_mem_test_run();
     dap_cpu_monitor_test_run();
