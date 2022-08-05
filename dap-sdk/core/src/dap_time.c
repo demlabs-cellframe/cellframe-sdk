047ceb53949acf5d0eaf82876986db1bd7c936e4
davlet.sibgatullin <davlet.sibgatullin@demlabs.net>
2022-08-05 22:40:39 +0300

    [*] DEBUG test time for win

--- dap-sdk/core/src/dap_time.c
+++ dap-sdk/core/src/dap_time.c
@@ -1,5 +1,6 @@
 #ifdef _WIN32
 #include <windows.h>
+#include <sys/time.h>
 #endif
 #include <errno.h>
 #include <string.h>
@@ -201,84 +202,40 @@ static void tmp_strptime(const char *buff, struct tm *tm)
     dap_sscanf(time, "%u.%u.%u", &tm->tm_year, &tm->tm_mon, &tm->tm_mday);
     tm->tm_year += 100;
 }
-#endif
-/*
-void setCustomTimeAndDate(char *dateBuf, char *timeBuf)
+
+static void tmp2_strptime(const char *buff, struct tm *tm)
 {
-	_u8 year;
-	_u8 mon;
-	_u8 day;
-	_u8 hour;
-	_u8 min;
-	_u8 sec;
+	char tbuff[50];
+	uint8_t year;
+	uint8_t mon;
+	uint8_t day;
 
-	struct timeval now;
-	mingw_gettimeofday(&now, NULL);
+	memcpy(tbuff, buff, dap_strlen(buff));
 
-	time_t gst = now.tv_sec;
-	
-	global_start_time = GetTick(0);
+	day = atoi(&tbuff[4]);
+	tbuff[4] = '\0';
+	if (day == 0)
+		day = 1;
 
-	struct tm* timeinfo = localtime(&gst);
+	mon = atoi(&tbuff[2]);
+	if (mon > 0)
+		mon--;
+	tbuff[2] = '\0';
 
-	if (setDate)
-	{
-		day = atoi(&dateBuf[4]);
-		dateBuf[4] = '\0';
-		if (day == 0)
-			day = 1;
+	year = atoi(tbuff);
+	if (year == 0)
+		year = 100;
+	else if (year < 69)
+		year += 100;
 
-		mon = atoi(&dateBuf[2]);
-		if (mon > 0)
-			mon--;
-		dateBuf[2] = '\0';
-
-		year = atoi(dateBuf);
-		if (year == 0)
-			year = 100;
-		else if (year < 70)
-			year += 100;
-	}
-	else
-	{
-		year = timeinfo->tm_year;
-		mon = timeinfo->tm_mon;
-		day = timeinfo->tm_mday;
-	}
-
-	if (setTime)
-	{
-		sec = atoi(&timeBuf[4]);
-		timeBuf[4] = '\0';
-		min = atoi(&timeBuf[2]);
-		timeBuf[2] = '\0';
-		hour = atoi(timeBuf);
-	}
-	else
-	{
-		hour = timeinfo->tm_hour;
-		min = timeinfo->tm_min;
-		sec = timeinfo->tm_sec;
-	}
-
-	if (year == 70
-	&&	mon == 0
-	&&	hour <= 3)
-	{
-		hour = 4;
-		min = 0;
-		sec = 0;
-	}
-
-	timeinfo->tm_year = year;
-	timeinfo->tm_mon = mon;
-	timeinfo->tm_mday = day;
-	timeinfo->tm_hour = hour;
-	timeinfo->tm_min = min;
-	timeinfo->tm_sec = sec;
-
-	global_initial_time = mktime(timeinfo);
-}*/
+	tm->tm_year = year;
+	tm->tm_mon = mon;
+	tm->tm_mday = day;
+	tm->tm_hour = 0;
+	tm->tm_min = 0;
+	tm->tm_sec = 0;
+}
+#endif
 
 /**
  * @brief Get time_t from string simplified formatted [%y%m%d = 220610 = 10 june 2022 00:00]
@@ -303,7 +260,7 @@ dap_time_t dap_time_from_str_simplified(const char *a_time_str)
 	strptime(a_time_str, "%y%m%d", &l_tm);
 #else
 	log_it(L_INFO, "WIN_VER\n");
-	tmp_strptime(a_time_str, &l_tm);
+	tmp2_strptime(a_time_str, &l_tm);
 #endif
 
     time_t tmp = mktime(&l_tm);
