5b1118d0556510ec5caeda80cf4f939ab3f74212
alexey.stratulat <alexey.stratulat@demlabs.net>
2022-08-06 16:33:57 +0700

    Moved completely from the wizard setting values DAP_UINT64_FORMAT_X, DAP_UINT64_FORMAT_x, DAP_UINT64_FORMAT_u.

--- dap-sdk/core/include/dap_common.h
+++ dap-sdk/core/include/dap_common.h
@@ -188,7 +188,7 @@ DAP_STATIC_INLINE void _dap_aligned_free( void *ptr )
 
 #define DAP_CLIENT_PROTOCOL_VERSION   24
 
-#if __SIZEOF_LONG__==8 && !defined(DAP_OS_DARWIN)
+#if __SIZEOF_LONG__==8
 #define DAP_UINT64_FORMAT_X  "llX"
 #define DAP_UINT64_FORMAT_x  "llx"
 #define DAP_UINT64_FORMAT_U  "llu"
@@ -198,17 +198,19 @@ DAP_STATIC_INLINE void _dap_aligned_free( void *ptr )
 #undef DAP_UINT64_FORMAT_x
 #undef DAP_UINT64_FORMAT_U
 
+
 #define DAP_UINT64_FORMAT_X  "lX"
 #define DAP_UINT64_FORMAT_x  "lx"
 #define DAP_UINT64_FORMAT_U  "lu"
 #endif
-#if __SIZEOF_LONG__==4 || defined (DAP_OS_DARWIN)
+#elif __SIZEOF_LONG__==4
 #define DAP_UINT64_FORMAT_X  "llX"
 #define DAP_UINT64_FORMAT_x  "llx"
 #define DAP_UINT64_FORMAT_U  "llu"
 #else
 #error "DAP_UINT64_FORMAT_* are undefined for your platform"
 #endif
+
 
 #ifdef DAP_OS_WINDOWS
 #ifdef _WIN64
