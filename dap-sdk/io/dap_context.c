b76063e5de0f36e31950099fd3615b95543db4ad
alexey.stratulat <alexey.stratulat@demlabs.net>
2022-08-18 00:15:47 +0700

    [*] Moved changes from dap_proc_thread.c and dap_server.c master branch.

--- dap-sdk/io/dap_context.c
+++ dap-sdk/io/dap_context.c
@@ -379,7 +379,7 @@ static int s_thread_loop(dap_context_t * a_context)
 
     do {
 #ifdef DAP_EVENTS_CAPS_EPOLL
-        struct epoll_event *l_epoll_events = l_context->epoll_events;
+        struct epoll_event *l_epoll_events = a_context->epoll_events;
         l_selected_sockets = epoll_wait(a_context->epoll_fd, l_epoll_events, DAP_EVENTS_SOCKET_MAX, -1);
         l_sockets_max = l_selected_sockets;
 #elif defined(DAP_EVENTS_CAPS_POLL)
@@ -696,7 +696,8 @@ static int s_thread_loop(dap_context_t * a_context)
                     case DESCRIPTOR_TYPE_QUEUE:
                         dap_events_socket_queue_proc_input_unsafe(l_cur);
                         dap_events_socket_set_writable_unsafe(l_cur, false);
-                    break;
+//                    break;
+                        continue;
                     case DESCRIPTOR_TYPE_EVENT:
                         dap_events_socket_event_proc_input_unsafe(l_cur);
                     break;
