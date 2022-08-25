420427f6eec13d7ffa016f51cef619879ef6691e
alexey.stratulat <alexey.stratulat@demlabs.net>
2022-08-25 20:33:15 +0700

    [*] ...

--- dap-sdk/core/test/dap_circular_test.c
+++ dap-sdk/core/test/dap_circular_test.c
@@ -45,18 +45,18 @@ void dap_circular_test_double_write()
     const int buf_size = 8;
     const char* expected_string = "0123456701";
     int expected_string_len = strlen(expected_string);
-    circular_buffer_t cb = circular_buffer_create(buf_size);
+    dap_cbuf_t cb = dap_cbuf_create(buf_size);
 
-    circular_buffer_push(cb, chars_string, buf_size);
+    dap_cbuf_push(cb, chars_string, buf_size);
 
     int fd[2];
     socketpair(PF_LOCAL, SOCK_STREAM, 0, fd);
 
-    int ret = circular_buffer_write_In_socket(cb, fd[0]);
+    int ret = dap_cbuf_write_in_socket(cb, fd[0]);
 
-    circular_buffer_push(cb, chars_string, 2);
+    dap_cbuf_push(cb, chars_string, 2);
 
-    ret = circular_buffer_write_In_socket(cb, fd[0]);
+    ret = dap_cbuf_write_in_socket(cb, fd[0]);
     dap_assert(ret == 2, "Check ret write in socket");
 
     char result_buff[MAX_RESULT_BUF_LEN] = {0};
@@ -79,11 +79,11 @@ void dap_circular_test_defrag_write()
     const int buf_size = 8;
     const char* expected_string = "56701201";
     int expected_string_len = strlen(expected_string);
-    circular_buffer_t cb = circular_buffer_create(buf_size);
+    dap_cbuf_t cb = dap_cbuf_create(buf_size);
 
-    circular_buffer_push(cb, chars_string, buf_size);
-    circular_buffer_pop(cb, 5, NULL);
-    circular_buffer_push(cb, chars_string, 3);
+    dap_cbuf_push(cb, chars_string, buf_size);
+    dap_cbuf_pop(cb, 5, NULL);
+    dap_cbuf_push(cb, chars_string, 3);
     // expected string here 567012
 
 
@@ -91,14 +91,14 @@ void dap_circular_test_defrag_write()
     socketpair(PF_LOCAL, SOCK_STREAM, 0, fd);
 
     // write 567012
-    int ret = circular_buffer_write_In_socket(cb, fd[0]);
+    int ret = dap_cbuf_write_in_socket(cb, fd[0]);
     dap_assert(ret == 6, "Check ret write in socket");
 
     // push 01
-    circular_buffer_push(cb, chars_string, 2);
+    dap_cbuf_push(cb, chars_string, 2);
 
     // write 01
-    ret = circular_buffer_write_In_socket(cb, fd[0]);
+    ret = dap_cbuf_write_in_socket(cb, fd[0]);
     dap_assert(ret == 2, "Check ret write in socket");
 
     char result_buff[MAX_RESULT_BUF_LEN] = {0};
@@ -119,9 +119,9 @@ void dap_circular_test_defrag_write()
 void dap_circular_test_write_bad_socket()
 {
     const int buf_size = 8;
-    circular_buffer_t cb = circular_buffer_create(buf_size);
+    dap_cbuf_t cb = dap_cbuf_create(buf_size);
 
-    circular_buffer_push(cb, chars_string, buf_size);
+    dap_cbuf_push(cb, chars_string, buf_size);
 
     int fd[2];
     socketpair(PF_LOCAL, SOCK_STREAM, 0, fd);
@@ -129,10 +129,10 @@ void dap_circular_test_write_bad_socket()
     socketpair(PF_LOCAL, SOCK_STREAM, 0, fd2);
 
     close(fd[0]);
-    int ret = circular_buffer_write_In_socket(cb, fd[0]);
+    int ret = dap_cbuf_write_in_socket(cb, fd[0]);
     dap_assert(ret == -1, "Check ret write in socket");
 
-    ret = circular_buffer_write_In_socket(cb, fd2[0]);
+    ret = dap_cbuf_write_in_socket(cb, fd2[0]);
     dap_assert(ret == 8, "Check ret write in socket");
     char result_buff[MAX_RESULT_BUF_LEN] = {0};
     ssize_t res = read(fd2[1], result_buff, MAX_RESULT_BUF_LEN);
@@ -142,7 +142,7 @@ void dap_circular_test_write_bad_socket()
     dap_assert(dap_strn_equals(result_buff, chars_string, buf_size),
                "Check result buf");
 
-    ret = circular_buffer_write_In_socket(cb, fd2[0]);
+    ret = dap_cbuf_write_in_socket(cb, fd2[0]);
     dap_assert(ret == 0, "Check zero write");
     dap_assert(circular_buffer_get_data_size(cb) == 0, "Check data size");
     close(fd[1]);
@@ -161,7 +161,7 @@ void dap_circular_load_test()
     const char *digits = "123456789";
 
     const int buf_size = strlen(digits);
-    circular_buffer_t cb = circular_buffer_create(buf_size);
+    dap_cbuf_t cb = dap_cbuf_create(buf_size);
 
     int fd[2];
     socketpair(PF_LOCAL, SOCK_STREAM, 0, fd);
@@ -169,9 +169,9 @@ void dap_circular_load_test()
     int count_writed_bytes = 0;
 
     // defrag buffer
-    circular_buffer_push(cb, (void*)digits, strlen(digits));
-    circular_buffer_pop(cb, strlen(digits) - 1, NULL);
-    circular_buffer_push(cb, (void*)digits, 3);
+    dap_cbuf_push(cb, (void*)digits, strlen(digits));
+    dap_cbuf_pop(cb, strlen(digits) - 1, NULL);
+    dap_cbuf_push(cb, (void*)digits, 3);
     count_writed_bytes = 4;
 
     char expectedBuffer[MAX_RESULT_BUF_LEN];
@@ -180,12 +180,12 @@ void dap_circular_load_test()
 
     int count_write_bytes = 4;
     do {
-        int r = circular_buffer_write_In_socket(cb, fd[0]);
+        int r = dap_cbuf_write_in_socket(cb, fd[0]);
         dap_assert_PIF(r == count_write_bytes, "Check write bytes");
         dap_assert_PIF(circular_buffer_get_data_size(cb) == 0, "buf size must be 0!");
 
         count_write_bytes = rand() % strlen(digits);
-        circular_buffer_push(cb, (void*)digits, count_write_bytes);
+        dap_cbuf_push(cb, (void*)digits, count_write_bytes);
         strncat(expectedBuffer, digits, count_write_bytes);
         count_writed_bytes += count_write_bytes;
     } while (--iterations);
