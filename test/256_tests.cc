3eab9605f64d56870ba47c77e82a9d7520fa8ff3
Evgeniy Kramsakov <sobiegrus@gmail.com>
2022-08-30 11:46:55 +0300

    [+] Add some boost-powered random tests

--- test/256_tests.cc
+++ test/256_tests.cc
@@ -3,6 +3,7 @@
 #include <boost/program_options/options_description.hpp>
 #include <boost/program_options/option.hpp>
 #include <boost/multiprecision/cpp_int.hpp>
+#include <boost/multiprecision/random.hpp>
 #include <boost/random.hpp>
 using namespace std;
 #include <iostream>
@@ -12,6 +13,29 @@ using namespace std;
 
 #include "gtest/gtest-spi.h"
 
+namespace mp = boost::multiprecision;
+
+
+class RandomInputTests : public ::testing::Test {
+protected:
+
+    void SetUp() override {
+        gen512.seed(clock());
+        gen256.seed(clock());
+        gen128.seed(clock());
+    }
+    typedef boost::random::independent_bits_engine<boost::random::mt19937, 512, mp::cpp_int> generator_type_512;
+    typedef boost::random::independent_bits_engine<boost::random::mt19937, 256, mp::cpp_int> generator_type_256;
+    typedef boost::random::independent_bits_engine<boost::random::mt19937, 128, mp::cpp_int> generator_type_128;
+
+    generator_type_512 gen512;
+    generator_type_256 gen256;
+    generator_type_128 gen128;
+};
+
+class RandomComparisonTests: public RandomInputTests {
+
+};
 
 
 TEST(InputTests, ZeroInputBase) {
@@ -84,7 +108,7 @@ TEST(InputTests, Min128FromString) {
     uint256_t min = dap_chain_balance_scan("18446744073709551616");
 #if defined(DAP_GLOBAL_IS_INT128)
     ASSERT_EQ(min.hi, 0);
-    ASSERT_EQ(min.lo, boost::multiprecision::uint128_t("18446744073709551616"));
+    ASSERT_EQ(min.lo, mp::uint128_t("18446744073709551616"));
 
 #else
     //todo: сreate test for non-native 128 bit
@@ -96,7 +120,7 @@ TEST(InputTests, Max128FromString) {
     uint256_t max = dap_chain_balance_scan("340282366920938463463374607431768211455");
 #if defined(DAP_GLOBAL_IS_INT128)
     ASSERT_EQ(max.hi, 0);
-    ASSERT_EQ(max.lo, boost::multiprecision::uint128_t("340282366920938463463374607431768211455"));
+    ASSERT_EQ(max.lo, mp::uint128_t("340282366920938463463374607431768211455"));
 #else
     //todo: сreate test for non-native 128 bit
     FAIL();
@@ -118,8 +142,8 @@ TEST(InputTests, Min256FromString) {
 TEST(InputTests, Max256FromString) {
     uint256_t max = dap_chain_balance_scan("115792089237316195423570985008687907853269984665640564039457584007913129639935");
 #if defined(DAP_GLOBAL_IS_INT128)
-    ASSERT_EQ(max.hi, boost::multiprecision::uint128_t("340282366920938463463374607431768211455"));
-    ASSERT_EQ(max.lo, boost::multiprecision::uint128_t("340282366920938463463374607431768211455"));
+    ASSERT_EQ(max.hi, mp::uint128_t("340282366920938463463374607431768211455"));
+    ASSERT_EQ(max.lo, mp::uint128_t("340282366920938463463374607431768211455"));
 #else
     //todo: сreate test for non-native 128 bit
     FAIL();
@@ -133,14 +157,12 @@ TEST(InputTests, Get256From128) {
     ASSERT_EQ(b.hi, 0);
 }
 
-TEST(InputTests, RandomInput) {
-//    using namespace boost::multiprecision;
-    using namespace boost::random;
-    typedef independent_bits_engine<mt19937, 256, boost::multiprecision::cpp_int> generator_type;
-    generator_type gen;
+TEST_F(RandomInputTests, Input256) {
+    mp::uint256_t boost_a(gen256());
 
-    boost::multiprecision::uint256_t boost_a(gen());
-    EXPECT_TRUE(false) << boost_a;
+    uint256_t a = dap_chain_balance_scan(to_string(boost_a).c_str());
+    ASSERT_EQ(a.hi, boost_a >> 128);
+    ASSERT_EQ(a.lo, boost_a & mp::uint256_t("0xffffffffffffffffffffffffffffffff"));
 }
 
 TEST(ComparisonTests, Equal128) {
@@ -157,11 +179,127 @@ TEST(ComparisonTests, Equal128) {
 }
 
 
+TEST(ComparisonTests, Equal256) {
+    uint256_t a, b;
+
+    a = GET_256_FROM_64(0);
+    b = GET_256_FROM_64(0);
+
+    ASSERT_TRUE(EQUAL_256(a, b));
+
+    a = GET_256_FROM_64(1);
+
+    ASSERT_FALSE(EQUAL_256(a, b));
+
+    a = dap_chain_balance_scan("340282366920938463463374607431768211456");
+
+    ASSERT_FALSE(EQUAL_256(a, b));
+
+    b = dap_chain_balance_scan("340282366920938463463374607431768211455");
+
+    ASSERT_FALSE(EQUAL_256(a, b));
+
+    b = dap_chain_balance_scan("340282366920938463463374607431768211456");
+
+    ASSERT_TRUE(EQUAL_256(a, b));
+
+    a = dap_chain_balance_scan("115792089237316195423570985008687907853269984665640564039457584007913129639935");
+
+    ASSERT_FALSE(EQUAL_256(a, b));
+
+    b = dap_chain_balance_scan("115792089237316195423570985008687907853269984665640564039457584007913129639934");
+
+    ASSERT_FALSE(EQUAL_256(a, b));
+
+    b = dap_chain_balance_scan("115792089237316195423570985008687907853269984665640564039457584007913129639935");
+
+    ASSERT_TRUE(EQUAL_256(a, b));
+}
+
+TEST_F(RandomComparisonTests, Equal256) {
+    mp::uint256_t boost_a(gen128());
+
+    uint256_t a = dap_chain_balance_scan(to_string(boost_a).c_str());
+    uint256_t b = dap_chain_balance_scan(to_string(boost_a).c_str());
+
+    ASSERT_TRUE(EQUAL_256(a, b));
+
+    ASSERT_TRUE(EQUAL_128(a.lo, b.lo));
+    ASSERT_TRUE(EQUAL_128(a.hi, b.hi));
+}
+
+TEST(ComparisonTests, IsZeroTest128) {
+    uint128_t a = uint128_0;
+
+    ASSERT_TRUE(IS_ZERO_128(a));
+
+    a = uint128_1;
+
+    ASSERT_FALSE(IS_ZERO_128(a));
+
+}
+
+TEST(ComparisonTests, IsZeroTest256) {
+    uint256_t a = uint256_0;
+
+    ASSERT_TRUE(IS_ZERO_256(a));
+
+    a = uint256_1;
+
+    ASSERT_FALSE(IS_ZERO_256(a));
+
+    a.lo = 0;
+    a.hi = 0;
+
+    ASSERT_TRUE(IS_ZERO_256(a));
+
+    a.lo = 1;
+
+    ASSERT_FALSE(IS_ZERO_256(a));
+
+    a.hi = 1;
+
+    ASSERT_FALSE(IS_ZERO_256(a));
+
+    a.lo = 0;
+
+    ASSERT_FALSE(IS_ZERO_256(a));
+
+    a = dap_chain_balance_scan("0");
+
+    ASSERT_TRUE(IS_ZERO_256(a));
+
+    a = dap_chain_balance_scan("340282366920938463463374607431768211455");
+
+    ASSERT_FALSE(IS_ZERO_256(a));
+
+    a = dap_chain_balance_scan("115792089237316195423570985008687907853269984665640564039457584007913129639935");
+
+    ASSERT_FALSE(IS_ZERO_256(a));
+
+}
+
+TEST_F(RandomComparisonTests, IsZeroTest) {
+    mp::uint256_t boost_a(gen128());
+
+    uint256_t a = dap_chain_balance_scan(to_string(boost_a).c_str());
+
+    if (boost_a == 0) {
+        ASSERT_TRUE(IS_ZERO_256(a));
+    }
+    else {
+        ASSERT_FALSE(IS_ZERO_256(a));
+    }
+}
+
+TEST()
+
+
 
 //TEST(BoostTest, BasicSum) {
-//    boost::multiprecision::uint256_t a {123};
-//    boost::multiprecision::uint256_t b {321};
-//    boost::multiprecision::uint256_t c {123+321};
+//    mp::uint256_t a {123};
+//    mp::uint256_t b {321};
+//    mp::uint256_t c {123+321};
 //
 //    uint256_t aa = GET_256_FROM_64(123);
 //    uint256_t bb = GET_256_FROM_64(321);
