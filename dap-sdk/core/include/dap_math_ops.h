166113a8bcf76188a1749118c7a1ba654cea0b30
Evgeniy Kramsakov <sobiegrus@gmail.com>
2022-09-20 13:52:32 +0300

    [+] Add ability to compile with non-native 128-bit under any target

--- dap-sdk/core/include/dap_math_ops.h
+++ dap-sdk/core/include/dap_math_ops.h
@@ -13,7 +13,7 @@
 
 #if defined(__GNUC__) || defined (__clang__)
 
-#if __SIZEOF_INT128__ == 16
+#if ((__SIZEOF_INT128__ == 16) && !DAP_DISABLE_INT128)
 
 #define DAP_GLOBAL_IS_INT128
 typedef __int128 _dap_int128_t;
