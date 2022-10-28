7a1fdc05cc527b69c6f1044785ab9923b12d4c00
Ruslan (The BadAss SysMan) Laishev <ruslan.laishev@demlabs.net>
2022-10-28 22:52:49 +0300

    [*] #6131 Deactivation fix

--- modules/wallet/dap_chain_wallet.c
+++ modules/wallet/dap_chain_wallet.c
@@ -330,10 +330,11 @@ dap_chain_wallet_n_pass_t   *l_prec;
     {
         if ( !l_prec->pass_len )                                        /* Password is zero - has been reset probably */
             log_it(L_WARNING, "The Wallet %.*s is not active", a_name_len, a_name);
-                                                                        /* Check that passwords is equivalent */
-        else if ( (l_prec->pass_len != a_pass_len)
+
+        else if ( (l_prec->pass_len != a_pass_len)                      /* Check that passwords is equivalent */
              || memcmp(l_prec->pass, a_pass, l_prec->pass_len) )
             l_rc = -EINVAL, log_it(L_ERROR, "Wallet's password does not match");
+
         else    l_rc = 0, memset(l_prec->pass, l_prec->pass_len = 0, sizeof(l_prec->pass));
     }
 
