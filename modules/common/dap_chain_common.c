9601ca9aa0957ca23af9d22e146b9c505a50ef44
alexey.stratulat <alexey.stratulat@demlabs.net>
2022-11-30 14:26:39 +0700

    [*] Fixed return "null" from dap_chain_addr_to_str

--- modules/common/dap_chain_common.c
+++ modules/common/dap_chain_common.c
@@ -84,7 +84,7 @@ char* dap_chain_addr_to_str(const dap_chain_addr_t *a_addr)
     if ( a_addr ==NULL)
         return  NULL;
 
-    if (dap_chain_addr_is_blank(a_addr)) return "null";
+    if (dap_chain_addr_is_blank(a_addr)) return dap_strdup("null");
 
     size_t l_ret_size = DAP_ENC_BASE58_ENCODE_SIZE(sizeof(dap_chain_addr_t));
     char * l_ret = DAP_NEW_SIZE(char, l_ret_size);
