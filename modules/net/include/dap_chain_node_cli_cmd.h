bac6a80a0fd7c9236a2b139a152e4ebfdc674716
alexey.stratulat <alexey.stratulat@demlabs.net>
2023-01-27 16:52:05 +0700

    [*] Made a backport of the changes from the features-7841 branch.

--- modules/net/include/dap_chain_node_cli_cmd.h
+++ modules/net/include/dap_chain_node_cli_cmd.h
@@ -141,6 +141,7 @@ int cmd_gdb_export(int a_argc, char **a_argv, char **a_str_reply);
 int com_mempool_delete(int a_argc, char **a_argv, char **a_str_reply);
 int com_mempool_list(int a_argc, char **a_argv, char **a_str_reply);
 int com_mempool_proc(int a_argc, char **a_argv, char **a_str_reply);
+int com_mempool_proc_all(int argc, char ** argv, char ** a_str_reply);
 int com_mempool_check(int a_argc, char **a_argv, char **a_str_reply);
 /**
  * Place public CA into the mempool
