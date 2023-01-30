07576a5bd83d6376d64464b8486a659e9101f706
papizh.konstantin <papizh.konstantin@demlabs.net>
2023-01-31 00:18:11 +0300

    ...

--- dap-sdk/net/core/dap_proc_thread.c
+++ dap-sdk/net/core/dap_proc_thread.c
@@ -758,7 +758,7 @@ static void * s_proc_thread_function(void * a_arg)
 #if defined(DAP_EVENTS_CAPS_QUEUE_PIPE2)
                                 l_bytes_sent = write(l_cur->socket, l_cur->buf_out, /*sizeof(void*)*/ l_cur->buf_out_size);
                                 debug_if(g_debug_reactor, L_NOTICE, "send %ld bytes to pipe", l_bytes_sent);
-                                l_errno = l_bytes_sent < l_cur->buf_out_size ? errno : 0;
+                                l_errno = l_bytes_sent < (ssize_t)l_cur->buf_out_size ? errno : 0;
                                 debug_if(l_errno, L_ERROR, "Writing to pipe %d bytes failed, sent %d only...", l_cur->buf_out_size, l_bytes_sent);
 #elif defined DAP_EVENTS_CAPS_MSMQ
                                 /* TODO: Windows-way message waiting and handling
