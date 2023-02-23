d3e5025f633789536caac6978639e7a0147b38fb
Constantin P <papizh.konstantin@demlabs.net>
2023-02-23 12:45:12 +0300

    ...

--- 3rdparty/json-c/json_inttypes.h
+++ 3rdparty/json-c/json_inttypes.h
@@ -12,12 +12,12 @@
 /* inttypes.h includes stdint.h */
 #include <inttypes.h>
 
-#else
-#include <stdint.h>
+//#else
+//#include <stdint.h>
 
-#define PRId64 "I64d"
-#define SCNd64 "I64d"
-#define PRIu64 "I64u"
+//#define PRId64 "I64d"
+//#define SCNd64 "I64d"
+//#define PRIu64 "I64u"
 
 //#endif
 
