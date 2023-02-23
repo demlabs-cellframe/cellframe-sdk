a11c255ec847363f2a3a85f923cc15ddf0cc9719
papizh.konstantin <papizh.konstantin@demlabs.net>
2023-02-23 17:16:42 +0300

    ...

--- 3rdparty/json-c/json_inttypes.h
+++ 3rdparty/json-c/json_inttypes.h
@@ -8,18 +8,18 @@
 
 #include "json_config.h"
 
-//#ifdef JSON_C_HAVE_INTTYPES_H
+#ifdef JSON_C_HAVE_INTTYPES_H
 /* inttypes.h includes stdint.h */
 #include <inttypes.h>
 
-//#else
-//#include <stdint.h>
+#else
+#include <stdint.h>
 
-//#define PRId64 "I64d"
-//#define SCNd64 "I64d"
-//#define PRIu64 "I64u"
+#define PRId64 "I64d"
+#define SCNd64 "I64d"
+#define PRIu64 "I64u"
 
-//#endif
+#endif
 
 #if defined(_MSC_VER)
 #include <BaseTsd.h>
