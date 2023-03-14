7bf6c6b67e313d0e11b148c1eba983db24bd66ae
Roman Khlopkov <roman.khlopkov@demlabs.net>
2023-03-14 20:41:28 +0300

    [*] Stake lock timing return

--- modules/service/stake_lock/dap_chain_net_srv_stake_lock.c
+++ modules/service/stake_lock/dap_chain_net_srv_stake_lock.c
@@ -308,7 +308,7 @@ static enum error_code s_cli_hold(int a_argc, char **a_argv, int a_arg_index, da
     dap_time_t l_time_now = dap_time_now();
     if (l_time_staking < l_time_now)
         return TIME_ERROR;
-    l_time_staking = 0;// -= l_time_now;
+    l_time_staking -= l_time_now;
 
 	if (dap_chain_node_cli_find_option_val(a_argv, a_arg_index, a_argc, "-reinvest", &l_reinvest_percent_str)
 	&& NULL != l_reinvest_percent_str) {
