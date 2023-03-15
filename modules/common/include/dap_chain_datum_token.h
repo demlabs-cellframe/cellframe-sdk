d8f61dcbf268095cda743bf7c48b5dd5e7b327cf
alexey.stratulat <alexey.stratulat@demlabs.net>
2023-03-15 14:16:07 +0700

    [*] Edit dap_chain_datum_token_t.

--- modules/common/include/dap_chain_datum_token.h
+++ modules/common/include/dap_chain_datum_token.h
@@ -86,6 +86,7 @@ typedef struct dap_chain_datum_token{
     uint16_t subtype;
     char ticker[DAP_CHAIN_TICKER_SIZE_MAX];
     uint16_t signs_valid; // Emission auth signs
+    uint16_t signs_total; // Emission auth signs
     uint256_t total_supply;
     union {
         // Simple token declaration. Useful for 100% premined emission without any plays with token and owners after that
@@ -124,7 +125,6 @@ typedef struct dap_chain_datum_token{
         } DAP_ALIGN_PACKED header_public;
         byte_t header[256]; // For future changes
     };
-    uint16_t signs_total; // Emission auth signs
     byte_t data_n_tsd[]; // Signs and/or types-size-data sections
 } DAP_ALIGN_PACKED dap_chain_datum_token_t;
 
