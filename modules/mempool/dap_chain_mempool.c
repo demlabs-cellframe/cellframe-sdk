f7d6c262d873af45be623d17e3c83852569d0571
roman.padenkov <roman.padenkov@demlabs.net>
2023-03-20 13:30:08 +0000

    feature-8265

--- modules/mempool/dap_chain_mempool.c
+++ modules/mempool/dap_chain_mempool.c
@@ -276,7 +276,7 @@ char *dap_chain_mempool_tx_create(dap_chain_t * a_chain, dap_enc_key_t *a_key_fr
  *
  * return hash_tx Ok, , NULL other Error
  */
-char *dap_chain_mempool_tx_coll_fee_create(dap_enc_key_t *a_key_from,const dap_chain_addr_t* a_addr_to,dap_chain_block_cache_t *a_block_cache,
+char *dap_chain_mempool_tx_coll_fee_create(dap_enc_key_t *a_key_from,const dap_chain_addr_t* a_addr_to,dap_list_t *a_block_list,
                                            uint256_t a_value_fee, const char *a_hash_out_type)
 {
     uint256_t                   l_value_to_items = {};
@@ -286,7 +286,7 @@ char *dap_chain_mempool_tx_coll_fee_create(dap_enc_key_t *a_key_from,const dap_c
     dap_chain_addr_t            l_addr_fee = {};
     dap_chain_t                 *l_chain = NULL;
 
-    l_chain = DAP_CHAIN_CS_BLOCKS(a_block_cache)->chain;
+    l_chain = DAP_CHAIN_CS_BLOCKS((dap_chain_block_cache_t *)a_block_list->data)->chain;
     bool l_net_fee_used = dap_chain_net_tx_get_fee(l_chain->net_id, &l_net_fee, &l_addr_fee);
     //add tx
     if (NULL == (l_tx = dap_chain_datum_tx_create())) {
@@ -294,18 +294,20 @@ char *dap_chain_mempool_tx_coll_fee_create(dap_enc_key_t *a_key_from,const dap_c
         return NULL;
     }
 
-    dap_list_t *l_list_used_out = dap_chain_block_get_list_tx_cond_outs_with_val(l_chain->ledger,a_block_cache,&l_value_out);
-    if (!l_list_used_out) {
-        log_it(L_WARNING, "Not enough funds to transfer");
-        dap_chain_datum_tx_delete(l_tx);
-        return NULL;
-    }
-
-    //add 'in' items
+    for(dap_list_t *bl = a_block_list; bl; bl = bl->next)
     {
-        l_value_to_items = dap_chain_datum_tx_add_in_cond_item_list(&l_tx, l_list_used_out);
-        assert(EQUAL_256(l_value_to_items, l_value_out));
-        dap_list_free_full(l_list_used_out, NULL);
+        uint256_t l_value_out_block = {};
+        dap_chain_block_cache_t *l_block_cache = (dap_chain_block_cache_t *)bl->data;
+        dap_list_t *l_list_used_out = dap_chain_block_get_list_tx_cond_outs_with_val(l_chain->ledger,l_block_cache,&l_value_out_block);
+        if (!l_list_used_out) continue;
+
+        //add 'in' items
+        {
+            l_value_to_items = dap_chain_datum_tx_add_in_cond_item_list(&l_tx, l_list_used_out);
+            assert(EQUAL_256(l_value_to_items, l_value_out_block));
+            dap_list_free_full(l_list_used_out, NULL);
+        }
+        SUM_256_256(l_value_out, l_value_out_block, &l_value_out);
     }
     //add 'fee' items
     {
@@ -328,7 +330,14 @@ char *dap_chain_mempool_tx_coll_fee_create(dap_enc_key_t *a_key_from,const dap_c
                 return NULL;
             }
         }
-        SUBTRACT_256_256(l_value_out,l_value_pack,&l_value_out);
+        if(compare256(l_value_out,l_value_pack)!=-1)
+            SUBTRACT_256_256(l_value_out,l_value_pack,&l_value_out);
+        else
+        {
+            log_it(L_WARNING, "The transaction fee is greater than the sum of the block fees");
+            dap_chain_datum_tx_delete(l_tx);
+            return NULL;
+        }
     }
 
     //add 'out' items
@@ -363,7 +372,7 @@ int dap_chain_mempool_tx_create_massive( dap_chain_t * a_chain, dap_enc_key_t *a
         const char a_token_ticker[10], uint256_t a_value, uint256_t a_value_fee,
         size_t a_tx_num)
 {
-    // check valid param
+    // check valid paramdap_chain_mempool_tx_coll_fee_create
     if(!a_chain | !a_key_from || !a_addr_from || !a_key_from->priv_key_data || !a_key_from->priv_key_data_size ||
             !dap_chain_addr_check_sum(a_addr_from) || !dap_chain_addr_check_sum(a_addr_to) ||
             IS_ZERO_256(a_value) || !a_tx_num){
