17fba73554b33f79918ab238db9bafcd63397a11
alexey.stratulat <alexey.stratulat@demlabs.net>
2023-04-24 12:08:13 +0700

    [*] ...

--- modules/common/include/dap_chain_common.h
+++ modules/common/include/dap_chain_common.h
@@ -107,7 +107,6 @@ DAP_STATIC_INLINE bool dap_chain_node_addr_str_check(const char *a_addr_str) {
 
 DAP_STATIC_INLINE int dap_chain_node_addr_from_str(dap_chain_node_addr_t *a_addr, const char *a_addr_str)
 {
-    dap_chain_node_addr_str_check(a_addr_str);
     if (sscanf(a_addr_str, NODE_ADDR_FP_STR, NODE_ADDR_FPS_ARGS(a_addr)) == 4)
         return 0;
     if (sscanf(a_addr_str, "0x%016"DAP_UINT64_FORMAT_x, &a_addr->uint64) == 1)
