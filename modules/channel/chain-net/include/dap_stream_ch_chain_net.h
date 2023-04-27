ba7f452f12bf07d0abbfe77569742b4125698ae4
roman.padenkov <roman.padenkov@demlabs.net>
2023-04-27 17:40:07 +0700

    change out valuet for validator check func

--- modules/channel/chain-net/include/dap_stream_ch_chain_net.h
+++ modules/channel/chain-net/include/dap_stream_ch_chain_net.h
@@ -49,6 +49,8 @@ typedef struct dap_stream_ch_chain_rnd{
         uint8_t flags;//0 bit -autoproc; 1 bit - find order; 2 bit - auto online; 3 bit - auto update; 6 bit - data sign; 7 bit - find cert;
         uint32_t sign_size;
         //uint8_t data[10];
+        uint8_t sign_correct;
+        uint8_t overall_correct;
     }DAP_ALIGN_PACKED header;
     byte_t sign[];
 } DAP_ALIGN_PACKED dap_stream_ch_chain_rnd_t;
