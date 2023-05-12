23ca16d215e7789ccd7be9a5c3cc57c481be371a
Constantin <papizh.konstantin@demlabs.net>
2023-05-12 14:30:12 +0700

    ...

--- modules/chain/dap_chain_ledger.c
+++ modules/chain/dap_chain_ledger.c
@@ -1784,6 +1784,26 @@ dap_list_t *dap_chain_ledger_token_info(dap_ledger_t *a_ledger)
 }
 
 /**
+ * @brief Get all token declatations
+ * @param a_ledger
+ * @return
+ */
+dap_list_t* dap_chain_ledger_token_decl_all(dap_ledger_t *a_ledger)
+{
+    dap_list_t * l_ret = NULL;
+    dap_chain_ledger_token_item_t *l_token_item, *l_tmp_item;
+    pthread_rwlock_rdlock(&PVT(a_ledger)->tokens_rwlock);
+
+    HASH_ITER(hh, PVT(a_ledger)->tokens, l_token_item, l_tmp_item) {
+        dap_chain_datum_token_t *l_token = l_token_item->datum_token;
+        l_ret = dap_list_append(l_ret, l_token);
+    }
+    pthread_rwlock_unlock(&PVT(a_ledger)->tokens_rwlock);
+    return l_ret;
+}
+
+
+/**
  * @brief s_threshold_emissions_proc
  * @param a_ledger
  */
