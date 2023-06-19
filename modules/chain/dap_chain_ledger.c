308b9faba3eb9b5839168f0ae4a3c626592aa433
alexey.stratulat <alexey.stratulat@demlabs.net>
2023-06-19 10:00:13 +0000

    Hotfix 8866

--- modules/chain/dap_chain_ledger.c
+++ modules/chain/dap_chain_ledger.c
@@ -428,8 +428,7 @@ static bool s_ledger_token_update_check(dap_chain_ledger_token_item_t *a_cur_tok
 
 	l_signs_upd_token = dap_chain_datum_token_signs_parse(a_token_update, a_token_update_size,
 														  &auth_signs_total, &auth_signs_valid);
-	if (a_cur_token_item->auth_signs_total != auth_signs_total
-	||	a_cur_token_item->auth_signs_valid != auth_signs_valid) {
+    if (a_cur_token_item->auth_signs_valid > auth_signs_total) {
 		DAP_DEL_Z(l_signs_upd_token);
 		if(s_debug_more)
 			log_it(L_WARNING,"Can't update token with ticker '%s' because: "
@@ -441,17 +440,25 @@ static bool s_ledger_token_update_check(dap_chain_ledger_token_item_t *a_cur_tok
 		return false;
 	}
 	if(auth_signs_total) {
-		for(uint16_t i = 0; i < auth_signs_total; i++){
+        size_t l_valid_pkeys = 0;
+        for(uint16_t i = 0; i < auth_signs_total; i++){
             dap_pkey_t *l_pkey_upd_token = dap_sign_get_pkey_deserialization(l_signs_upd_token[i]);
-			if (!dap_pkey_match(a_cur_token_item->auth_pkeys[i], l_pkey_upd_token)) {
-				DAP_DEL_Z(l_signs_upd_token);
-                DAP_DELETE(l_pkey_upd_token);
-				if(s_debug_more)
-					log_it(L_WARNING, "Can't update token with ticker '%s' because: Signs not compare", a_token_update->ticker);
-				return false;
-			}
+            for (size_t j = 0; j < a_cur_token_item->auth_signs_total; j++) {
+                if (dap_pkey_match(a_cur_token_item->auth_pkeys[j], l_pkey_upd_token)) {
+                    l_valid_pkeys++;
+                    break;
+                }
+            }
             DAP_DELETE(l_pkey_upd_token);
-		}
+        }
+        if (a_cur_token_item->auth_signs_valid > l_valid_pkeys) {
+            DAP_DEL_Z(l_signs_upd_token);
+            if (s_debug_more)
+                log_it(L_WARNING, "Can't update token with ticker '%s' because: Insufficient number of valid signatures "
+                                  "for an token update. Verified %zu needs %zu.", a_token_update->ticker, l_valid_pkeys,
+                       a_cur_token_item->auth_signs_valid);
+            return false;
+        }
 	}
 	DAP_DEL_Z(l_signs_upd_token);
 	if (!IS_ZERO_256(a_token_update->total_supply)){
