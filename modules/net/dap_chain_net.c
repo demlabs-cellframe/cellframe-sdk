5907c7e204efee9fbf037e61da20c59d5eb978c8
roman.padenkov <roman.padenkov@demlabs.net>
2023-08-03 16:35:18 +0700

    ...

--- modules/net/dap_chain_net.c
+++ modules/net/dap_chain_net.c
@@ -1030,14 +1030,15 @@ static void s_net_balancer_link_prepare_success(dap_worker_t * a_worker, dap_cha
     dap_chain_node_info_t * l_node_info = (dap_chain_node_info_t *)a_link_full_node_list->nodes_info;
     int l_res = 0;
     size_t i = 0;
+    struct json_object *l_json = s_net_states_json_collect(l_net);
+    char l_err_str[128] = { };
     while(!l_res){
         if(i >= a_link_full_node_list->count_node)
             break;
         l_res = s_net_link_add(l_net, l_node_info + i);
         switch (l_res) {
         case 0:
-            struct json_object *l_json = s_net_states_json_collect(l_net);
-            char l_err_str[128] = { };
+
             snprintf(l_err_str, sizeof(l_err_str)
                          , "Link " NODE_ADDR_FP_STR " prepared"
                          , NODE_ADDR_FP_ARGS_S((l_node_info + i)->hdr.address));
