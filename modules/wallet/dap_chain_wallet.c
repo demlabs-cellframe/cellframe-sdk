75880d7d685c08571c65d61aa079427f506a8a2a
pavel.uhanov <pavel.uhanov@demlabs.net>
2023-08-15 17:30:06 +0700

    [*] small fixes

--- modules/wallet/dap_chain_wallet.c
+++ modules/wallet/dap_chain_wallet.c
@@ -603,8 +603,9 @@ if ( a_pass )
 #ifdef DAP_OS_WINDOWS
     l_fh = CreateFile(l_wallet_internal->file_name, GENERIC_WRITE, /*FILE_SHARE_READ | FILE_SHARE_WRITE */ 0, NULL, CREATE_ALWAYS,
                           /*FILE_FLAG_RANDOM_ACCESS | FILE_FLAG_OVERLAPPED | FILE_FLAG_NO_BUFFERING*/ 0, NULL);
-    SetEndOfFile(l_fh);
-    if (l_fh == INVALID_HANDLE_VALUE) {
+    if (l_fh != INVALID_HANDLE_VALUE) {
+        SetEndOfFile(l_fh);
+    } else {
         l_err = GetLastError();
 #else
     if ( 0 > (l_fh = open(l_wallet_internal->file_name , O_CREAT | O_WRONLY | O_TRUNC, s_fileprot)) ) {
