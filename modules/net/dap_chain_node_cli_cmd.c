19f55b88ed55117cce03c82b2b6cc920bd11bf79
pavel.uhanov <pavel.uhanov@demlabs.net>
2023-08-16 14:02:38 +0700

    [*] change check result hash converting

--- modules/net/dap_chain_node_cli_cmd.c
+++ modules/net/dap_chain_node_cli_cmd.c
@@ -5621,7 +5621,7 @@ int com_tx_history(int a_argc, char ** a_argv, char **a_str_reply)
     }
 
     dap_chain_hash_fast_t l_tx_hash;
-    if (l_tx_hash_str && dap_chain_hash_fast_from_str(l_tx_hash_str, &l_tx_hash) < 0) {
+    if (l_tx_hash_str && dap_chain_hash_fast_from_str(l_tx_hash_str, &l_tx_hash)) {
         dap_cli_server_cmd_set_reply_text(a_str_reply, "tx hash not recognized");
         return -3;
     }
