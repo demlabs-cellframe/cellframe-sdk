b3877fe3704015986ab34e57297d7433e6b85a7d
pavel.uhanov <pavel.uhanov@demlabs.net>
2023-08-18 15:17:57 +0700

    [*] apply allocation error style

--- modules/wallet/dap_chain_wallet.c
+++ modules/wallet/dap_chain_wallet.c
@@ -126,7 +126,7 @@ char *c_wallets_path;
     {
         l_prec  = DAP_NEW_Z(dap_chain_wallet_n_pass_t);                 /* Get memory for new record */
         if (!l_prec) {
-            log_it(L_CRITICAL, "Memory allocation error");
+            log_it(L_CRITICAL, "Memory allocation error in %s, line %d", __PRETTY_FUNCTION__, __LINE__);
             return -EINVAL;
         }
         *l_prec = l_rec;                                                /* Fill it by data */
@@ -397,12 +397,12 @@ dap_chain_wallet_internal_t *l_wallet_internal;
         return  log_it(L_ERROR, "Wallet's password is too long ( > %d)", DAP_WALLET$SZ_PASS), NULL;
 
     if ( !(l_wallet = DAP_NEW_Z(dap_chain_wallet_t)) ) {
-        log_it(L_CRITICAL, "Memory allocation error");
+        log_it(L_CRITICAL, "Memory allocation error in %s, line %d", __PRETTY_FUNCTION__, __LINE__);
         return NULL;
     }
 
     if ( !(l_wallet->_internal = l_wallet_internal = DAP_NEW_Z(dap_chain_wallet_internal_t)) ) {
-        log_it(L_CRITICAL, "Memory allocation error");
+        log_it(L_CRITICAL, "Memory allocation error in %s, line %d", __PRETTY_FUNCTION__, __LINE__);
         DAP_DELETE(l_wallet);
         return NULL;
     }
@@ -412,7 +412,7 @@ dap_chain_wallet_internal_t *l_wallet_internal;
     l_wallet_internal->certs = DAP_NEW_Z_SIZE(dap_cert_t *,l_wallet_internal->certs_count * sizeof(dap_cert_t *));
     assert(l_wallet_internal->certs);
     if (!l_wallet_internal->certs) {
-        log_it(L_CRITICAL, "Memory allocation error");
+        log_it(L_CRITICAL, "Memory allocation error in %s, line %d", __PRETTY_FUNCTION__, __LINE__);
         dap_chain_wallet_close(l_wallet);
         return NULL;
     }
@@ -502,9 +502,13 @@ dap_chain_addr_t* dap_chain_wallet_get_addr(dap_chain_wallet_t * a_wallet, dap_c
  * @param a_net_id
  * @return
  */
-dap_chain_addr_t * dap_cert_to_addr(dap_cert_t * a_cert, dap_chain_net_id_t a_net_id)
+dap_chain_addr_t *dap_cert_to_addr(dap_cert_t * a_cert, dap_chain_net_id_t a_net_id)
 {
-    dap_chain_addr_t * l_addr = DAP_NEW_Z(dap_chain_addr_t);
+    dap_chain_addr_t *l_addr = DAP_NEW_Z(dap_chain_addr_t);
+    if(!l_addr) {
+        log_it(L_CRITICAL, "Memory allocation error in %s, line %d", __PRETTY_FUNCTION__, __LINE__);
+        return NULL;
+    }
     dap_chain_addr_fill_from_key(l_addr, a_cert->enc_key, a_net_id);
     return l_addr;
 }
@@ -845,7 +849,7 @@ uint32_t    l_csum = CRC32C_INIT, l_csum2 = CRC32C_INIT;
     l_wallet = DAP_NEW_Z(dap_chain_wallet_t);
     assert(l_wallet);
     if (!l_wallet) {
-        log_it(L_CRITICAL, "Memory allocation error");
+        log_it(L_CRITICAL, "Memory allocation error in %s, line %d", __PRETTY_FUNCTION__, __LINE__);
         dap_fileclose(l_fh);
         return NULL;
     }
@@ -853,7 +857,7 @@ uint32_t    l_csum = CRC32C_INIT, l_csum2 = CRC32C_INIT;
     DAP_CHAIN_WALLET_INTERNAL_LOCAL_NEW(l_wallet);
     assert(l_wallet_internal);
     if (!l_wallet_internal) {
-        log_it(L_CRITICAL, "Memory allocation error");
+        log_it(L_CRITICAL, "Memory allocation error in %s, line %d", __PRETTY_FUNCTION__, __LINE__);
         DAP_DEL_Z(l_wallet);
         dap_fileclose(l_fh);
         return NULL;
@@ -874,7 +878,7 @@ uint32_t    l_csum = CRC32C_INIT, l_csum2 = CRC32C_INIT;
     l_wallet_internal->certs = DAP_NEW_Z_SIZE(dap_cert_t *, l_wallet_internal->certs_count * sizeof(dap_cert_t *));
     assert(l_wallet_internal->certs);
     if (!l_wallet_internal->certs) {
-        log_it(L_CRITICAL, "Memory allocation error");
+        log_it(L_CRITICAL, "Memory allocation error in %s, line %d", __PRETTY_FUNCTION__, __LINE__);
         DAP_DEL_Z(l_wallet);
         dap_fileclose(l_fh);
         return NULL;
