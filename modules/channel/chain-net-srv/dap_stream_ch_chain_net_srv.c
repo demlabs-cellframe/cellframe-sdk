f71cee8aee223228197f310cff2347ef46452575
daniil.frolov <daniil.frolov@demlabs.net>
2023-09-01 19:55:28 +0700

    ..

--- modules/channel/chain-net-srv/dap_stream_ch_chain_net_srv.c
+++ modules/channel/chain-net-srv/dap_stream_ch_chain_net_srv.c
@@ -797,7 +797,7 @@ static bool s_grace_period_finish(usages_in_grace_t *a_grace_item)
 
                     // create and fill limits and first receipt
                     l_grace->usage->receipt = dap_chain_datum_tx_receipt_create(
-                                l_grace->usage->service->uid, l_price->units_uid, l_price->units, l_price->value_datoshi, NULL, NULL);
+                                l_grace->usage->service->uid, l_price->units_uid, l_price->units, l_price->value_datoshi, NULL, 0);
 
                     if (l_grace->usage->service->callbacks.response_success)
                         l_grace->usage->service->callbacks.response_success(l_grace->usage->service, l_grace->usage->id,
