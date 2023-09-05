65f6692f4e488ec8a866a93652a534a27a382922
roman.padenkov <roman.padenkov@demlabs.net>
2023-09-05 11:09:15 +0700

    ...

--- modules/net/dap_chain_net_node_list.c
+++ modules/net/dap_chain_net_node_list.c
@@ -30,6 +30,12 @@ along with any CellFrame SDK based project.  If not, see <http://www.gnu.org/lic
 
 #define LOG_TAG "dap_chain_net_node_list"
 
+#ifdef DAP_OS_WINDOWS
+#define dap_cond_signal(x) SetEvent(x)
+#else
+#define dap_cond_signal(x) pthread_cond_broadcast(&x)
+#endif
+
 void dap_chain_net_node_check_http_issue_link(dap_http_simple_t *a_http_simple, void *a_arg)
 {
     log_it(L_DEBUG,"Proc enc http request");
@@ -117,6 +123,7 @@ static void s_net_node_link_prepare_success(void *a_response, size_t a_response_
     struct node_link_request * l_node_list_request = (struct node_link_request *)a_arg;
     dap_chain_node_info_t *l_node_info = l_node_list_request->link_info;
     l_node_list_request->response = *(uint8_t*)a_response;
+    dap_cond_signal(l_node_list_request->wait_cond);
     //DAP_DELETE(l_node_info);
     //DAP_DELETE(l_node_list_request);
 }
@@ -181,7 +188,7 @@ static int dap_chain_net_node_list_wait(struct node_link_request *a_node_list_re
     clock_gettime( CLOCK_MONOTONIC, &l_cond_timeout);
     l_cond_timeout.tv_sec += a_timeout_ms/1000;
     // signal waiting
-    while (a_node_list_request->response) {
+    while (!a_node_list_request->response) {
         int l_ret_wait = pthread_cond_timedwait(&a_node_list_request->wait_cond, &a_node_list_request->wait_mutex, &l_cond_timeout);
         if(l_ret_wait) {
             ret = a_node_list_request->response ? 0 : -2;
@@ -256,7 +263,7 @@ int dap_chain_net_node_list_request (dap_chain_net_t *a_net, dap_chain_node_info
                                             NULL) == NULL;
 
     size_t rc = dap_chain_net_node_list_wait(l_node_list_request, 4000);
-
+    log_it(L_DEBUG, "Stop node list HTTP request to ");
     if(ret){
         s_node_list_request_dinit(l_node_list_request);
         return 4;
