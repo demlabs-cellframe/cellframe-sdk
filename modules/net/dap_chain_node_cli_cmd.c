4b07c677737a3177203a8af3fa85c40f36b6334f
alexey.stratulat <alexey.stratulat@demlabs.net>
2023-09-22 10:51:03 +0700

    [*] Fixed functionality for creating a transaction both in the native ticker and when the transaction ticker differs from the native one. Now the transaction is formed correctly and asserts do not fire.

--- modules/net/dap_chain_node_cli_cmd.c
+++ modules/net/dap_chain_node_cli_cmd.c
@@ -5309,8 +5309,8 @@ int com_tx_create_json(int a_argc, char ** a_argv, char **a_str_reply)
                 //SUM_256_256(a_value, a_value_fee, &l_value_need);
                 uint256_t l_value_need_check = {};
                 if (!dap_strcmp(l_native_token, l_main_token)) {
-                    SUM_256_256(l_value_need_check, l_value_need, &l_value_need);
-                    SUM_256_256(l_value_need_check, l_value_need_fee, &l_value_need);
+                    SUM_256_256(l_value_need_check, l_value_need, &l_value_need_check);
+                    SUM_256_256(l_value_need_check, l_value_need_fee, &l_value_need_check);
                     l_list_used_out = dap_chain_ledger_get_list_tx_outs_with_val(l_net->pub.ledger, l_json_item_token,
                                                                                              l_addr_from, l_value_need_check, &l_value_transfer);
                     if(!l_list_used_out) {
@@ -5335,7 +5335,7 @@ int com_tx_create_json(int a_argc, char ** a_argv, char **a_str_reply)
                     }
                     //CHECK value fee
                     l_list_used_out_fee = dap_chain_ledger_get_list_tx_outs_with_val(l_net->pub.ledger, l_native_token,
-                                                                                                  l_addr_from, l_value_need_fee, &l_value_transfer);
+                                                                                                  l_addr_from, l_value_need_fee, &l_value_transfer_fee);
                     if(!l_list_used_out_fee) {
                         log_it(L_WARNING, "Not enough funds in previous tx to transfer");
                         dap_string_append_printf(l_err_str, "Can't create in transaction. Not enough funds in previous tx "
@@ -5358,6 +5358,8 @@ int com_tx_create_json(int a_argc, char ** a_argv, char **a_str_reply)
                     if (!IS_ZERO_256(l_value_back)) {
                         dap_chain_datum_tx_add_out_ext_item(&l_tx, l_addr_from, l_value_back, l_native_token);
                     }
+                } else {
+                    SUM_256_256(l_value_need, l_value_need_fee, &l_value_need);
                 }
                 dap_list_free_full(l_list_used_out, free);
                 if(!IS_ZERO_256(l_value_got)) {
