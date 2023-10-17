0b6fa0d44ecdf43ca6e12c7c7bb233424b4f069b
alexey.stratulat <alexey.stratulat@demlabs.net>
2023-10-17 21:09:36 +0700

    [*] Added error output if memory allocation for JSON failed in serialization dap_chain_datum_anchor to JSON.

--- modules/common/dap_chain_datum_anchor.c
+++ modules/common/dap_chain_datum_anchor.c
@@ -27,6 +27,7 @@
 #include "dap_common.h"
 #include "dap_enc_base58.h"
 #include "dap_chain_datum_anchor.h"
+#include "dap_json_rpc_errors.h"
 
 #define LOG_TAG "dap_chain_datum_anchor"
 
@@ -126,26 +127,86 @@ json_object *s_dap_chain_datum_anchor_sign_to_json(byte_t * a_signs, size_t a_ce
 
 json_object *dap_chain_datum_anchor_to_json(dap_chain_datum_anchor_t *a_anchor){
     json_object *l_obj_anchor = json_object_new_object();
+    if (!l_obj_anchor) {
+        dap_json_rpc_allocated_error
+        return NULL;
+    }
     json_object *l_obj_version = json_object_new_uint64(a_anchor->anchor_version);
+    if (!l_obj_version){
+        json_object_put(l_obj_anchor);
+        dap_json_rpc_allocated_error
+        return NULL;
+    }
     json_object *l_obj_ts_created = json_object_new_uint64(a_anchor->header.ts_created);
+    if (!l_obj_ts_created) {
+        json_object_put(l_obj_version);
+        json_object_put(l_obj_anchor);
+        dap_json_rpc_allocated_error
+        return NULL;
+    }
     json_object *l_obj_tsd_array = json_object_new_array();
+    if(!l_obj_tsd_array) {
+        json_object_put(l_obj_ts_created);
+        json_object_put(l_obj_version);
+        json_object_put(l_obj_anchor);
+        dap_json_rpc_allocated_error
+        return NULL;
+    }
     size_t l_tsd_offset = 0, tsd_data_size = a_anchor->header.data_size;
 
     while(l_tsd_offset < tsd_data_size){
         json_object *l_jobj_tsd = json_object_new_object();
+        if (!l_jobj_tsd) {
+            json_object_put(l_obj_tsd_array);
+            json_object_put(l_obj_ts_created);
+            json_object_put(l_obj_version);
+            json_object_put(l_obj_anchor);
+            dap_json_rpc_allocated_error
+            return NULL;
+        }
         dap_tsd_t *l_tsd = (dap_tsd_t*)a_anchor->data_n_sign + l_tsd_offset;
         size_t l_tsd_size = l_tsd->size + sizeof(dap_tsd_t);
         if(l_tsd_size > tsd_data_size){
             json_object *l_jobj_wgn = json_object_new_string("TSD size is greater than all data size. It's possible corrupt data.");
+            if (!l_jobj_wgn){
+                json_object_put(l_jobj_tsd);
+                json_object_put(l_obj_tsd_array);
+                json_object_put(l_obj_ts_created);
+                json_object_put(l_obj_ts_created);
+                json_object_put(l_obj_version);
+                json_object_put(l_obj_anchor);
+                dap_json_rpc_allocated_error
+                return NULL;
+            }
             json_object_object_add(l_jobj_tsd, "warning", l_jobj_wgn);
             json_object_array_add(l_obj_tsd_array, l_jobj_tsd);
             break;
         }
         if (l_tsd->type == DAP_CHAIN_DATUM_ANCHOR_TSD_TYPE_DECREE_HASH){
             json_object *l_obj_tsd_type = json_object_new_string("DAP_CHAIN_DATUM_ANCHOR_TSD_TYPE_DECREE_HASH");
+            if (!l_obj_tsd_type){
+                json_object_put(l_jobj_tsd);
+                json_object_put(l_obj_tsd_array);
+                json_object_put(l_obj_ts_created);
+                json_object_put(l_obj_ts_created);
+                json_object_put(l_obj_version);
+                json_object_put(l_obj_anchor);
+                dap_json_rpc_allocated_error
+                return NULL;
+            }
             json_object_object_add(l_jobj_tsd, "type", l_obj_tsd_type);
             if(l_tsd->size > sizeof(dap_hash_fast_t)){
                 json_object *l_jobj_wgn = json_object_new_string("Wrong fee tsd data size.");
+                if (!l_jobj_wgn){
+                    json_object_put(l_jobj_tsd);
+                    json_object_put(l_obj_tsd_array);
+                    json_object_put(l_obj_ts_created);
+                    json_object_put(l_obj_ts_created);
+                    json_object_put(l_obj_version);
+                    json_object_put(l_obj_anchor);
+                    dap_json_rpc_allocated_error
+                    return NULL;
+                }
                 json_object_object_add(l_jobj_tsd, "warning", l_jobj_wgn);
                 json_object_array_add(l_obj_tsd_array, l_jobj_tsd);
                 break;
@@ -153,9 +214,29 @@ json_object *dap_chain_datum_anchor_to_json(dap_chain_datum_anchor_t *a_anchor){
             dap_hash_fast_t l_out_hash = {0};
             _dap_tsd_get_scalar(l_tsd, &l_out_hash);
             char *l_hash_str = dap_hash_fast_to_str_new(&l_out_hash);
+            if (!l_hash_str) {
+                json_object_put(l_jobj_tsd);
+                json_object_put(l_obj_tsd_array);
+                json_object_put(l_obj_ts_created);
+                json_object_put(l_obj_ts_created);
+                json_object_put(l_obj_version);
+                json_object_put(l_obj_anchor);
+                dap_json_rpc_allocated_error
+                return NULL;
+            }
             json_object *l_obj_tsd_hash = json_object_new_string(l_hash_str);
-            json_object_object_add(l_jobj_tsd, "hash", l_obj_tsd_hash);
             DAP_DELETE(l_hash_str);
+            if (!l_obj_tsd_hash){
+                json_object_put(l_jobj_tsd);
+                json_object_put(l_obj_tsd_array);
+                json_object_put(l_obj_ts_created);
+                json_object_put(l_obj_ts_created);
+                json_object_put(l_obj_version);
+                json_object_put(l_obj_anchor);
+                dap_json_rpc_allocated_error
+                return NULL;
+            }
+            json_object_object_add(l_jobj_tsd, "hash", l_obj_tsd_hash);
         }
         json_object_array_add(l_obj_tsd_array, l_jobj_tsd);
         l_tsd_offset += l_tsd_size;
@@ -165,6 +246,15 @@ json_object *dap_chain_datum_anchor_to_json(dap_chain_datum_anchor_t *a_anchor){
     json_object_object_add(l_obj_anchor, "TSD", l_obj_tsd_array);
     json_object *l_jobj_signs = s_dap_chain_datum_anchor_sign_to_json(a_anchor->data_n_sign + a_anchor->header.data_size,
                                                                       a_anchor->header.signs_size);
+    if (!l_jobj_signs) {
+        json_object_put(l_obj_tsd_array);
+        json_object_put(l_obj_ts_created);
+        json_object_put(l_obj_ts_created);
+        json_object_put(l_obj_version);
+        json_object_put(l_obj_anchor);
+        dap_json_rpc_error_add(2, "I can't serialize the anchor signature in JSON.");
+        return NULL;
+    }
     json_object_object_add(l_obj_anchor, "signs", l_jobj_signs);
     return l_obj_anchor;
 }
