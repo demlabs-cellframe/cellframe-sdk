d83d939b17e5c0c65ba562b4450aff553c99d430
alexey.stratulat <alexey.stratulat@demlabs.net>
2023-10-17 18:25:13 +0700

    [*] Added error output if memory allocation for JSON failed

--- modules/common/dap_chain_datum_tx_receipt.c
+++ modules/common/dap_chain_datum_tx_receipt.c
@@ -27,6 +27,7 @@
 #include "dap_enc_key.h"
 #include "dap_sign.h"
 #include "dap_chain_datum_tx_receipt.h"
+#include "dap_json_rpc_errors.h"
 
 #define LOG_TAG "dap_chain_datum_tx_receipt"
 
@@ -164,16 +165,46 @@ uint16_t dap_chain_datum_tx_receipt_signs_count(dap_chain_datum_tx_receipt_t * a
 
 json_object* dap_chain_receipt_info_to_json(dap_chain_receipt_info_t *a_info){
     json_object *l_obj = json_object_new_object();
+    if (!l_obj) {
+        dap_json_rpc_allocated_error
+        return NULL;
+    }
     json_object *l_obj_srv_uid = json_object_new_uint64(a_info->srv_uid.uint64);
+    if (!l_obj_srv_uid) {
+        json_object_put(l_obj_srv_uid);
+        dap_json_rpc_allocated_error
+        return NULL;
+    }
     json_object_object_add(l_obj, "srvUID", l_obj_srv_uid);
 #if DAP_CHAIN_NET_SRV_UID_SIZE == 8
     json_object *l_obj_addition = json_object_new_uint64(a_info->addition);
+    if (!l_obj_addition){
+        json_object_put(l_obj);
+        dap_json_rpc_allocated_error
+        return NULL;
+    }
     json_object_object_add(l_obj, "addition", l_obj_addition);
 #endif
     json_object *l_obj_units_type = json_object_new_string(dap_chain_srv_unit_enum_to_str(a_info->units_type.enm));
+    if (!l_obj_units_type) {
+        json_object_put(l_obj);
+        dap_json_rpc_allocated_error
+        return NULL;
+    }
     json_object_object_add(l_obj, "unitsType", l_obj_units_type);
     char *l_datoshi_value = dap_chain_balance_print(a_info->value_datoshi);
+    if (!l_datoshi_value) {
+        json_object_put(l_obj);
+        dap_json_rpc_allocated_error
+        return NULL;
+    }
     json_object *l_obj_datoshi = json_object_new_string(l_datoshi_value);
+    if (!l_obj_datoshi) {
+        json_object_put(l_datoshi_value);
+        DAP_DELETE(l_datoshi_value);
+        dap_json_rpc_allocated_error
+        return NULL;
+    }
     DAP_DELETE(l_datoshi_value);
     json_object_object_add(l_obj, "value", l_obj_datoshi);
     return l_obj;
@@ -181,18 +212,64 @@ json_object* dap_chain_receipt_info_to_json(dap_chain_receipt_info_t *a_info){
 
 json_object *dap_chain_datum_tx_receipt_to_json(dap_chain_datum_tx_receipt_t *a_receipt) {
     json_object *l_obj = json_object_new_object();
+    if (!l_obj) {
+        dap_json_rpc_allocated_error
+        return NULL;
+    }
     json_object *l_obj_info = dap_chain_receipt_info_to_json(&a_receipt->receipt_info);
+    if (!l_obj_info) {
+        json_object_put(l_obj);
+        return NULL;
+    }
     json_object *l_obj_size = json_object_new_uint64(a_receipt->size);
+    if (!l_obj_size) {
+        json_object_put(l_obj);
+        json_object_put(l_obj_info);
+        dap_json_rpc_allocated_error
+        return NULL;
+    }
     //Provider
     dap_sign_t *l_first_sign  = dap_chain_datum_tx_receipt_sign_get(a_receipt, a_receipt->size, 1);
     //Client
     dap_sign_t *l_second_sign  = dap_chain_datum_tx_receipt_sign_get(a_receipt, a_receipt->size, 2);
     json_object *l_obj_signs = json_object_new_object();
+    if (!l_obj_signs) {
+        json_object_put(l_obj_size);
+        json_object_put(l_obj_info);
+        json_object_put(l_obj);
+        dap_json_rpc_allocated_error
+        return NULL;
+    }
     json_object *l_obj_provider_sign = dap_sign_to_json(l_first_sign);
+    if (!l_obj_provider_sign) {
+        json_object_put(l_obj_size);
+        json_object_put(l_obj_info);
+        json_object_put(l_obj_signs);
+        json_object_put(l_obj);
+        dap_json_rpc_error_add(2, "Error serializing signature to JSON.");
+        return NULL;
+    }
     json_object *l_obj_client_sign = dap_sign_to_json(l_second_sign);
+    if (!l_obj_client_sign) {
+        json_object_put(l_obj_provider_sign);
+        json_object_put(l_obj_size);
+        json_object_put(l_obj_info);
+        json_object_put(l_obj_signs);
+        json_object_put(l_obj);
+        dap_json_rpc_error_add(2, "Error serializing signature to JSON.");
+        return NULL;
+    }
     json_object_object_add(l_obj_signs, "provider", l_obj_provider_sign);
     json_object_object_add(l_obj_signs, "client", l_obj_client_sign);
     json_object *l_exts_data = json_object_new_string_len((char *)a_receipt->exts_n_signs, a_receipt->exts_size);
+    if (!l_exts_data) {
+        json_object_put(l_obj_size);
+        json_object_put(l_obj_info);
+        json_object_put(l_obj_signs);
+        json_object_put(l_obj);
+        dap_json_rpc_allocated_error
+        return NULL;
+    }
     json_object_object_add(l_obj, "info", l_obj_info);
     json_object_object_add(l_obj, "size", l_obj_size);
     json_object_object_add(l_obj, "sings", l_obj_signs);
