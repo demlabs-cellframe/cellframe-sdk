954d266e78af768c802c116d5b3ec5477aac8b37
alexey.stratulat <alexey.stratulat@demlabs.net>
2023-10-17 23:03:40 +0700

    [*] Added error output if memory allocation for JSON failed in serialization dap_chain_datum_token_decl to JSON.

--- modules/common/dap_chain_datum_token.c
+++ modules/common/dap_chain_datum_token.c
@@ -29,6 +29,7 @@
 #include "dap_chain_datum_token.h"
 #include "dap_uuid.h"
 #include "dap_enc_base58.h"
+#include "dap_json_rpc_errors.h"
 
 #define LOG_TAG "dap_chain_datum_token"
 
@@ -360,8 +361,17 @@ dap_sign_t ** dap_chain_datum_token_signs_parse(dap_chain_datum_token_t * a_datu
 
 json_object *dap_chain_datum_token_to_json(dap_chain_datum_token_t * a_token, size_t a_token_size){
     json_object *l_jobj_token = json_object_new_object();
+    if (!l_jobj_token){
+        dap_json_rpc_allocated_error
+        return NULL;
+    }
     json_object *l_jobj_type;
     json_object *l_jobj_version = json_object_new_uint64(a_token->version);
+    if (!l_jobj_version) {
+        json_object_put(l_jobj_token);
+        dap_json_rpc_allocated_error
+        return NULL;
+    }
     switch (a_token->type) {
         case DAP_CHAIN_DATUM_TOKEN_TYPE_DECL:
             l_jobj_type = json_object_new_string("DAP_CHAIN_DATUM_TOKEN_TYPE_DECL");
@@ -373,14 +383,31 @@ json_object *dap_chain_datum_token_to_json(dap_chain_datum_token_t * a_token, si
             l_jobj_type = json_object_new_string("DAP_CHAIN_DATUM_TOKEN_TYPE_UNKNOWN");
             break;
     }
+    if (!l_jobj_type) {
+        json_object_put(l_jobj_version);
+        json_object_put(l_jobj_token);
+        dap_json_rpc_allocated_error
+        return NULL;
+    }
     json_object_object_add(l_jobj_token, "version", l_jobj_version);
     json_object_object_add(l_jobj_token, "type", l_jobj_type);
     json_object *l_jobj_subtype = NULL;
     json_object *l_jobj_header = json_object_new_object();
+    if (!l_jobj_header){
+        json_object_put(l_jobj_token);
+        dap_json_rpc_allocated_error
+        return NULL;
+    }
     size_t l_tsd_total_size = 0;
     switch (a_token->subtype) {
         case DAP_CHAIN_DATUM_TOKEN_SUBTYPE_SIMPLE: {
             json_object *l_jobj_decimals  = json_object_new_uint64(a_token->header_simple.decimals);
+            if (!l_jobj_decimals){
+                json_object_put(l_jobj_header);
+                json_object_put(l_jobj_token);
+                dap_json_rpc_allocated_error
+                return NULL;
+            }
             json_object_object_add(l_jobj_header, "decimals", l_jobj_decimals);
             l_jobj_subtype = json_object_new_string("DAP_CHAIN_DATUM_TOKEN_SUBTYPE_SIMPLE");
         }break;
@@ -389,11 +416,37 @@ json_object *dap_chain_datum_token_to_json(dap_chain_datum_token_t * a_token, si
             json_object *l_jobj_decimals  = NULL;
             if (a_token->type == DAP_CHAIN_DATUM_TOKEN_TYPE_DECL) {
                 l_jobj_flags = json_object_new_string(s_flag_str_from_code(a_token->header_private_decl.flags));
+                if (!l_jobj_flags){
+                    json_object_put(l_jobj_header);
+                    json_object_put(l_jobj_token);
+                    dap_json_rpc_allocated_error
+                    return NULL;
+                }
                 l_jobj_decimals = json_object_new_uint64(a_token->header_private_decl.decimals);
+                if (!l_jobj_decimals) {
+                    json_object_put(l_jobj_flags);
+                    json_object_put(l_jobj_header);
+                    json_object_put(l_jobj_token);
+                    dap_json_rpc_allocated_error
+                    return NULL;
+                }
                 l_tsd_total_size = a_token->header_private_decl.tsd_total_size;
             } else {
                 l_jobj_flags = json_object_new_string(s_flag_str_from_code(a_token->header_private_update.flags));
+                if (!l_jobj_flags) {
+                    json_object_put(l_jobj_header);
+                    json_object_put(l_jobj_token);
+                    dap_json_rpc_allocated_error
+                    return NULL;
+                }
                 l_jobj_decimals = json_object_new_uint64(a_token->header_private_update.decimals);
+                if (!l_jobj_decimals) {
+                    json_object_put(l_jobj_flags);
+                    json_object_put(l_jobj_header);
+                    json_object_put(l_jobj_token);
+                    dap_json_rpc_allocated_error
+                    return NULL;
+                }
                 l_tsd_total_size = a_token->header_private_update.tsd_total_size;
             }
             json_object_object_add(l_jobj_header, "flags", l_jobj_flags);
@@ -405,11 +458,37 @@ json_object *dap_chain_datum_token_to_json(dap_chain_datum_token_t * a_token, si
             json_object *l_jobj_decimals  = NULL;
             if (a_token->type == DAP_CHAIN_DATUM_TOKEN_TYPE_DECL) {
                 l_jobj_flags = json_object_new_string(s_flag_str_from_code(a_token->header_native_decl.flags));
+                if (!l_jobj_flags) {
+                    json_object_put(l_jobj_header);
+                    json_object_put(l_jobj_token);
+                    dap_json_rpc_allocated_error
+                    return NULL;
+                }
                 l_jobj_decimals = json_object_new_uint64(a_token->header_native_decl.decimals);
+                if (!l_jobj_decimals){
+                    json_object_put(l_jobj_flags);
+                    json_object_put(l_jobj_header);
+                    json_object_put(l_jobj_token);
+                    dap_json_rpc_allocated_error
+                    return NULL;
+                }
                 l_tsd_total_size = a_token->header_native_decl.tsd_total_size;
             } else {
                 l_jobj_flags = json_object_new_string(s_flag_str_from_code(a_token->header_native_update.flags));
+                if (!l_jobj_flags) {
+                    json_object_put(l_jobj_header);
+                    json_object_put(l_jobj_token);
+                    dap_json_rpc_allocated_error
+                    return NULL;
+                }
                 l_jobj_decimals = json_object_new_uint64(a_token->header_native_update.decimals);
+                if (!l_jobj_decimals) {
+                    json_object_put(l_jobj_flags);
+                    json_object_put(l_jobj_header);
+                    json_object_put(l_jobj_token);
+                    dap_json_rpc_allocated_error
+                    return NULL;
+                }
                 l_tsd_total_size = a_token->header_native_update.tsd_total_size;
             }
             json_object_object_add(l_jobj_header, "flags", l_jobj_flags);
@@ -418,38 +497,94 @@ json_object *dap_chain_datum_token_to_json(dap_chain_datum_token_t * a_token, si
         } break;
         case DAP_CHAIN_DATUM_TOKEN_SUBTYPE_PUBLIC: {
             json_object *l_jobj_flags = json_object_new_string(s_flag_str_from_code(a_token->header_public.flags));
+            if (!l_jobj_flags){
+                json_object_put(l_jobj_header);
+                json_object_put(l_jobj_token);
+                dap_json_rpc_allocated_error
+                return NULL;
+            }
             char *l_premine_supply_str = dap_chain_balance_print(a_token->header_public.premine_supply);
+            if (!l_premine_supply_str) {
+                json_object_put(l_jobj_flags);
+                json_object_put(l_jobj_header);
+                json_object_put(l_jobj_token);
+                dap_json_rpc_allocated_error
+                return NULL;
+            }
             json_object *l_jobj_premine_supply = json_object_new_string(l_premine_supply_str);
             DAP_DELETE(l_premine_supply_str);
+            if (!l_jobj_premine_supply) {
+                json_object_put(l_jobj_flags);
+                json_object_put(l_jobj_header);
+                json_object_put(l_jobj_token);
+                dap_json_rpc_allocated_error
+                return NULL;
+            }
             json_object *l_jobj_premine_address = dap_chain_addr_to_json(&a_token->header_public.premine_address);
+            if (!l_jobj_premine_address) {
+                json_object_put(l_jobj_flags);
+                json_object_put(l_jobj_premine_supply);
+                json_object_put(l_jobj_header);
+                json_object_put(l_jobj_token);
+                dap_json_rpc_error_add(2, "Failed to convert address to JSON.");
+                return NULL;
+            }
             json_object_object_add(l_jobj_header, "flags", l_jobj_flags);
             json_object_object_add(l_jobj_header, "premine_supply", l_jobj_premine_supply);
             json_object_object_add(l_jobj_header, "premine_address", l_jobj_premine_address);
             l_jobj_subtype = json_object_new_string("DAP_CHAIN_DATUM_TOKEN_SUBTYPE_PUBLIC");
         } break;
         default: {
-            json_object *l_wgn_header = json_object_new_string("The token subtype could not be determined, so the "
-                                                               "header contents could not be determined.");
             l_jobj_subtype = json_object_new_string("DAP_CHAIN_DATUM_TOKEN_SUBTYPE_UNKNOWN");
-
-
         } break;
+    }
+    if (!l_jobj_subtype) {
+        json_object_put(l_jobj_header);
+        json_object_put(l_jobj_token);
+        dap_json_rpc_allocated_error
+        return NULL;
     }
     json_object_object_add(l_jobj_token, "subtype", l_jobj_subtype);
     json_object_object_add(l_jobj_token, "header", l_jobj_header);
     json_object *l_jobj_ticker = json_object_new_string(a_token->ticker);
-    json_object *l_jobj_signs_valid = json_object_new_uint64(a_token->signs_valid);
-    json_object *l_jobj_signs_total = json_object_new_uint64(a_token->signs_total);
+    if (!l_jobj_ticker) {
+        json_object_put(l_jobj_token);
+        dap_json_rpc_allocated_error
+        return NULL;
+    }
     json_object_object_add(l_jobj_token, "ticker", l_jobj_ticker);
+    json_object *l_jobj_signs_valid = json_object_new_uint64(a_token->signs_valid);
+    if (!l_jobj_signs_valid) {
+        json_object_put(l_jobj_token);
+        dap_json_rpc_allocated_error
+        return NULL;
+    }
     json_object_object_add(l_jobj_token, "signs_valid", l_jobj_signs_valid);
+    json_object *l_jobj_signs_total = json_object_new_uint64(a_token->signs_total);
+    if (!l_jobj_signs_total) {
+        json_object_put(l_jobj_token);
+        dap_json_rpc_allocated_error
+        return NULL;
+    }
     json_object_object_add(l_jobj_token, "signs_total", l_jobj_signs_total);
     json_object *l_obj_signs = json_object_new_array();
+    if (!l_obj_signs) {
+        json_object_put(l_jobj_token);
+        dap_json_rpc_allocated_error
+        return NULL;
+    }
     size_t l_offset = 0;
     size_t l_certs_field_size = a_token_size - sizeof(*a_token);
     while (l_offset < l_certs_field_size) {
         dap_sign_t *l_sign = (dap_sign_t *) ((byte_t*)a_token->data_n_tsd + l_tsd_total_size + l_offset);
         l_offset += dap_sign_get_size(l_sign);
         json_object *l_obj_sign = dap_sign_to_json(l_sign);
+        if (!l_obj_sign) {
+            json_object_put(l_obj_signs);
+            json_object_put(l_jobj_token);
+            dap_json_rpc_error_add(2, "Failed to convert signature to JSON.");
+            return NULL;
+        }
         json_object_array_add(l_obj_signs, l_obj_sign);
     }
     json_object_object_add(l_jobj_token, "signs", l_obj_signs);
