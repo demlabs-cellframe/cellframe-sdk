d82c00b529e4c3d898feb75bd3334d521dee431d
roman.padenkov <roman.padenkov@demlabs.net>
2023-12-07 14:06:52 +0700

    ...

---
+++ modules/net/srv/dap_chain_net_srv_request.h
@@ -0,0 +1,39 @@
+/*
+* Authors:
+* Roman Padenkov <roman.padenkov@demlabs.net>
+* Cellframe       https://cellframe.net
+* DeM Labs Inc.   https://demlabs.net
+* Copyright  (c) 2023-2024
+* All rights reserved.
+
+This file is part of CellFrame SDK the open source project
+
+CellFrame SDK is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+CellFrame SDK is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with any CellFrame SDK based project.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include "dap_chain_net.h"
+#include "dap_chain_net_srv_order.h"
+
+struct node_link_request {
+    dap_chain_net_srv_order_t *order;
+    dap_chain_net_t *net;
+    dap_worker_t *worker;
+    bool from_http;
+    int link_replace_tries;
+    int response;
+    pthread_cond_t wait_cond;
+    pthread_mutex_t wait_mutex;
+};
