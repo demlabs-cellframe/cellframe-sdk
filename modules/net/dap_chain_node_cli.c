99c5e49b293acd8d84767096dec9de86d25325ac
alexey.stratulat <alexey.stratulat@demlabs.net>
2023-12-29 21:58:08 +0700

    [*] Fixed help command mempool add_ca.

--- modules/net/dap_chain_node_cli.c
+++ modules/net/dap_chain_node_cli.c
@@ -265,7 +265,7 @@ int dap_chain_node_cli_init(dap_config_t * g_config)
                            "\tDelete datum with hash <datum hash> for selected chain network\n"
                            "mempool dump -net <net_name> -chain <chain_name> -datum <datum_hash>\n"
                            "\tOutput information about datum in mempool\n"
-                           "mempool add_ca -net <net_name> [-chain <chain_name>] -ca_name <priv_cert_name>\n"
+                           "mempool add_ca -net <net_name> [-chain <chain_name>] -ca_name <pub_cert_name>\n"
                            "\tAdd pubic certificate into the mempool to prepare its way to chains\n"
                            "mempool count -net <net_name> [-chain <chain_name>]\n"
                            "\tDisplays the number of elements in the mempool of a given network.");
