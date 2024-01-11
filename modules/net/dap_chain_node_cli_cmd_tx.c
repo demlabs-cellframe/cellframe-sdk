ff6adf2a905a1a5d8bd77f6579109242fc280770
Constantin P <papizh.konstantin@demlabs.net>
2024-01-11 13:27:35 +0700

    ...

--- modules/net/dap_chain_node_cli_cmd_tx.c
+++ modules/net/dap_chain_node_cli_cmd_tx.c
@@ -573,9 +573,6 @@ json_object* dap_db_history_addr(dap_chain_addr_t *a_addr, dap_chain_t *a_chain,
             DAP_DELETE(l_coins_str);
             l_is_need_correction = false;
         }
-        if (json_object_array_length(j_arr_data) > 0) {
-            json_object_object_add(j_obj_tx, "data", j_arr_data);
-        }
     }
     a_chain->callback_datum_iter_delete(l_datum_iter);
     // delete hashes
