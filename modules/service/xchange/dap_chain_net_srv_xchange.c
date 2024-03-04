ada81ac3ac99023b5e577b8ae4792d159657d48b
alexey.stratulat <alexey.stratulat@demlabs.net>
2024-03-04 22:04:34 +0700

    [*] Fixed SIGARBT.

--- modules/service/xchange/dap_chain_net_srv_xchange.c
+++ modules/service/xchange/dap_chain_net_srv_xchange.c
@@ -1403,7 +1403,6 @@ static int s_cli_srv_xchange_order(int a_argc, char **a_argv, int a_arg_index, c
                     dap_cli_server_cmd_set_reply_text(a_str_reply, "An error occurred with an unknown code: %d.", l_ret_code);
                     break;
             }
-            DAP_DELETE(l_sign_str);
             return l_ret_code;
         } break;
 
