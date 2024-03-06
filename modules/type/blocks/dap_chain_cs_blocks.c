8842ec4330cf4afbb75c3f57d7d0e052985cadd8
alexey.stratulat <alexey.stratulat@demlabs.net>
2024-03-06 21:45:29 +0700

    [+] Added limit and offset parameters to block list output.

--- modules/type/blocks/dap_chain_cs_blocks.c
+++ modules/type/blocks/dap_chain_cs_blocks.c
@@ -744,7 +744,7 @@ static int s_cli_blocks(int a_argc, char ** a_argv, void **a_str_reply)
 
         case SUBCMD_LIST:{
             const char *l_cert_name = NULL, *l_from_hash_str = NULL, *l_to_hash_str = NULL,
-                        *l_from_date_str = NULL, *l_to_date_str = NULL, *l_pkey_hash_str = NULL;
+                        *l_from_date_str = NULL, *l_to_date_str = NULL, *l_pkey_hash_str = NULL, *l_limit_str = NULL, *l_offset_str = NULL;
             bool l_unspent_flag = false, l_first_signed_flag = false, l_signed_flag = false, l_hash_flag = false;
             size_t l_block_count = 0;
             dap_pkey_t * l_pub_key = NULL;
@@ -760,6 +760,11 @@ static int s_cli_blocks(int a_argc, char ** a_argv, void **a_str_reply)
             dap_cli_server_cmd_find_option_val(a_argv, arg_index, a_argc, "-to_hash", &l_to_hash_str);
             dap_cli_server_cmd_find_option_val(a_argv, arg_index, a_argc, "-from_dt", &l_from_date_str);
             dap_cli_server_cmd_find_option_val(a_argv, arg_index, a_argc, "-to_dt", &l_to_date_str);
+            dap_cli_server_cmd_find_option_val(a_argv, arg_index, a_argc, "-limit", &l_limit_str);
+            dap_cli_server_cmd_find_option_val(a_argv, arg_index, a_argc, "-offset", &l_offset_str);
+            char *ptr;
+            size_t l_offset = l_offset_str ? strtoul(l_offset_str, &ptr, 10) : 0;
+            size_t l_limit = l_limit_str ? strtoul(l_limit_str, &ptr, 10) : 0;
 
             if (l_signed_flag && l_first_signed_flag) {
                 dap_cli_server_cmd_set_reply_text(a_str_reply, "Choose only one option from 'singed' and 'first_signed'");
@@ -826,7 +831,20 @@ static int s_cli_blocks(int a_argc, char ** a_argv, void **a_str_reply)
 
             pthread_rwlock_rdlock(&PVT(l_blocks)->rwlock);
             dap_string_t *l_str_tmp = dap_string_new(NULL);
+            size_t l_start_arr = 0;
+            if(l_offset) {
+                l_start_arr = l_offset * l_limit;
+            }
+            size_t l_arr_end = PVT(l_blocks)->blocks_count;
+            if (l_limit) {
+                l_arr_end = l_start_arr + l_limit;
+                if (l_arr_end > PVT(l_blocks)->blocks_count)
+                    l_arr_end = PVT(l_blocks)->blocks_count;
+            }
+            size_t i = 0;
             for (dap_chain_block_cache_t *l_block_cache = PVT(l_blocks)->blocks; l_block_cache; l_block_cache = l_block_cache->hh.next) {
+                if (i < l_start_arr || i > l_arr_end)
+                    continue;
                 dap_time_t l_ts = l_block_cache->block->hdr.ts_created;
                 if (l_from_time && l_ts < l_from_time)
                     continue;
