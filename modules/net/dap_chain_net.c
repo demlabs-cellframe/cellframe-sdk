01a9a5eeaeb2df1f3f0ae655a9b1a2fc2b9a8a1e
alexey.stratulat <alexey.stratulat@demlabs.net>
2024-03-21 12:17:02 +0700

    [*] Now information about the percent chain synchronization status is always displayed.

--- modules/net/dap_chain_net.c
+++ modules/net/dap_chain_net.c
@@ -1797,31 +1797,39 @@ json_object* s_set_reply_text_node_status_json(dap_chain_net_t *a_net) {
         json_object_object_add(l_jobj_links, "total", l_jobj_total_links);
         json_object_object_add(l_jobj_ret, "links", l_jobj_links);
     }
-    if (PVT(a_net)->state == NET_STATE_SYNC_CHAINS) {
-        json_object *l_jobj_processed_sync = json_object_new_object();
-        dap_chain_t *l_chain = NULL;
-        size_t l_count_el = 0, l_count_el_all = 0;
-        struct net_link *l_link, *l_link_tmp;
+    json_object *l_jobj_processed_sync = json_object_new_object();
+    dap_chain_t *l_chain = NULL;
+    size_t l_count_el = 0, l_count_el_all = 0;
+    struct net_link *l_link, *l_link_tmp;
+    if (a_net->pub.link_list) {
+        for (dap_list_t *i = a_net->pub.link_list; i; i = i->next) {
+            dap_chain_node_info_t *l_node_link = (dap_chain_node_info_t *) i->data;
+            if (l_count_el_all < l_node_link->hdr.blocks_events)
+                l_count_el_all = l_node_link->hdr.blocks_events;
+        }
+    } else {
+        dap_chain_net_pvt_t *l_net_pvt = PVT(a_net);
         pthread_mutex_lock(&PVT(a_net)->uplinks_mutex);
         HASH_ITER(hh,  PVT(a_net)->net_links, l_link, l_link_tmp) {
             if (l_count_el_all < l_link->link_info->hdr.blocks_events)
                 l_count_el_all = l_link->link_info->hdr.blocks_events;
         }
         pthread_mutex_unlock(&PVT(a_net)->uplinks_mutex);
-        DL_FOREACH(a_net->pub.chains, l_chain){
-            l_count_el += l_chain->callback_count_atom(l_chain);
-        }
-        double l_percent = l_count_el_all ? (double)(l_count_el * 100) / l_count_el_all : 0;
-        char *l_percent_str = dap_strdup_printf("%.3f", l_percent);
-        json_object *l_jobj_percent = json_object_new_string(l_percent_str);
-        DAP_DELETE(l_percent_str);
-        json_object *l_jobj_total = json_object_new_uint64(l_count_el_all);
-        json_object *l_jobj_current  = json_object_new_uint64(l_count_el);
-        json_object_object_add(l_jobj_processed_sync, "current", l_jobj_current);
-        json_object_object_add(l_jobj_processed_sync, "total", l_jobj_total);
-        json_object_object_add(l_jobj_processed_sync, "percent", l_jobj_percent);
-        json_object_object_add(l_jobj_ret, "processed", l_jobj_processed_sync);
     }
+
+    DL_FOREACH(a_net->pub.chains, l_chain){
+        l_count_el += l_chain->callback_count_atom(l_chain);
+    }
+    double l_percent = l_count_el_all ? (double)(l_count_el * 100) / l_count_el_all : 0;
+    char *l_percent_str = dap_strdup_printf("%.3f", l_percent);
+    json_object *l_jobj_percent = json_object_new_string(l_percent_str);
+    DAP_DELETE(l_percent_str);
+    json_object *l_jobj_total = json_object_new_uint64(l_count_el_all);
+    json_object *l_jobj_current  = json_object_new_uint64(l_count_el);
+    json_object_object_add(l_jobj_processed_sync, "current", l_jobj_current);
+    json_object_object_add(l_jobj_processed_sync, "total", l_jobj_total);
+    json_object_object_add(l_jobj_processed_sync, "percent", l_jobj_percent);
+    json_object_object_add(l_jobj_ret, "processed", l_jobj_processed_sync);
     json_object *l_jobj_states = json_object_new_object();
     json_object *l_jobj_current_states = json_object_new_string(c_net_states[PVT(a_net)->state]);
     json_object *l_jobj_target_states = json_object_new_string(c_net_states[PVT(a_net)->state_target]);
