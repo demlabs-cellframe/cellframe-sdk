0268bcf6f8e9ba7b20c381fdcd7a54fe6e69e935
alexey.stratulat <alexey.stratulat@demlabs.net>
2024-06-06 23:49:15 +0700

    [*] Fixed output information about net list in node.

--- modules/net/dap_chain_net.c
+++ modules/net/dap_chain_net.c
@@ -2895,6 +2895,9 @@ static int s_cli_net(int argc, char **argv, void **reply)
                                    "Command 'net' requires one of subcomands: sync, link, go, get, stats, ca, ledger");
             l_ret = DAP_CHAIN_NET_JSON_RPC_UNKNOWN_SUBCOMMANDS;
         }
+    } else {
+        json_object_put(l_jobj_return);
+        l_jobj_return = NULL;
     }
     if (l_jobj_return) {
         json_object_array_add(*reply, l_jobj_return);
