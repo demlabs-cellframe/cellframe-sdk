2d22470d4fcf9fc6d961ab589ee9aab8293537f8
Constantin P. <papizh.konstantin@demlabs.net>
2024-06-13 05:29:50 +0000

    Hotfix 11886

--- modules/chain/include/dap_chain_cell.h
+++ modules/chain/include/dap_chain_cell.h
@@ -74,11 +74,12 @@ typedef struct dap_chain_cell_decl{
 
 int dap_chain_cell_init(void);
 dap_chain_cell_t *dap_chain_cell_create_fill(dap_chain_t *a_chain, dap_chain_cell_id_t a_cell_id);
-dap_chain_cell_t *dap_chain_cell_create_fill2(dap_chain_t *a_chain, const char *a_filename);
 dap_chain_cell_t *dap_chain_cell_find_by_id(dap_chain_t *a_chain, dap_chain_cell_id_t a_cell_id);
 void dap_chain_cell_close(dap_chain_cell_t *a_cell);
 void dap_chain_cell_delete(dap_chain_cell_t *a_cell);
 void dap_chain_cell_delete_all(dap_chain_t *a_chain);
 int dap_chain_cell_load(dap_chain_t *a_chain, dap_chain_cell_t *a_cell);
-ssize_t dap_chain_cell_file_update(dap_chain_cell_t *a_cell);
 ssize_t dap_chain_cell_file_append(dap_chain_cell_t *a_cell,const void *a_atom, size_t a_atom_size);
+DAP_STATIC_INLINE ssize_t dap_chain_cell_file_update(dap_chain_cell_t *a_cell) {
+    return dap_chain_cell_file_append(a_cell, NULL, 0);
+}
