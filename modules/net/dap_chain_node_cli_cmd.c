e503fc2e5c910ae96436720601057bccb3efbd4a
Olzhas <oljas.jarasbaev@demlabs.net>
2024-06-24 13:54:36 +0700

    [*] client close

--- modules/net/dap_chain_node_cli_cmd.c
+++ modules/net/dap_chain_node_cli_cmd.c
@@ -1220,7 +1220,7 @@ int com_node(int a_argc, char ** a_argv, void **a_str_reply)
         if (res) {
             dap_cli_server_cmd_set_reply_text(a_str_reply, "No response from node");
             // clean client struct
-            dap_chain_node_client_close_mt(l_client);
+            dap_chain_node_client_close_unsafe(l_client);
             DAP_DELETE(node_info);
             return -8;
         }
