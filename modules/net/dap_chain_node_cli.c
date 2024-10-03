f5a9e88e5f978f0689a4f64df78cb07545367611
pavel.uhanov <pavel.uhanov@demlabs.net>
2024-10-03 05:40:27 +0000

    hotfix-12480

--- modules/net/dap_chain_node_cli.c
+++ modules/net/dap_chain_node_cli.c
@@ -218,9 +218,9 @@ int dap_chain_node_cli_init(dap_config_t * g_config)
             "\n"
             );
 
-    dap_cli_server_cmd_add("token_update_sign", com_token_decl_sign, "Token update add sign and new sign",
-                                        "token_update_sign -net <net_name> [-chain <chain_name>] -datum <datum_hash> -certs <certs list> -new_certs <certs list>\n"
-                                        "\t Sign existent <datum hash> in mempool with <certs list>\n"
+    dap_cli_server_cmd_add("token_update_sign", com_token_decl_sign, "Token update add sign to datum",
+                                        "token_update_sign -net <net_name> [-chain <chain_name>] -datum <datum_hash> -certs <cert_list>\n"
+                                        "\t Sign existent <datum hash> in mempool with <certs_list>\n"
     );
     // Token commands
 
