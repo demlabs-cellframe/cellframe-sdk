4c43a06f40950e3af43bae3af4d57ce39baf1bc9
alexey.stratulat <alexey.stratulat@demlabs.net>
2024-10-14 20:23:31 +0700

    [+] Added function of outputting order dump to JSON.

--- modules/net-srv/include/dap_chain_net_srv_order.h
+++ modules/net-srv/include/dap_chain_net_srv_order.h
@@ -142,6 +142,9 @@ char *dap_chain_net_srv_order_save(dap_chain_net_t *a_net, dap_chain_net_srv_ord
 void dap_chain_net_srv_order_dump_to_string(const dap_chain_net_srv_order_t *a_order, dap_string_t *a_str_out,
                                             const char *a_hash_out_type, const char *a_native_ticker);
 
+void dap_chain_net_srv_order_dump_to_json(const dap_chain_net_srv_order_t *a_order, json_object *a_jobj_out,
+                                            const char *a_hash_out_type, const char *a_native_ticker);
+
 void dap_chain_net_srv_order_add_notify_callback(dap_chain_net_t *a_net, dap_store_obj_callback_notify_t a_callback, void *a_cb_arg);
 /**
 * @brief dap_chain_net_srv_order_get_gdb_group_mempool
