96711af55f0e7831bfde3771ac852cee082a2b01
pavel.uhanov <pavel.uhanov@demlabs.net>
2024-10-14 10:33:15 +0300

    ...

--- modules/consensus/esbocs/dap_chain_cs_esbocs.c
+++ modules/consensus/esbocs/dap_chain_cs_esbocs.c
@@ -257,14 +257,14 @@ static int s_callback_new(dap_chain_t *a_chain, dap_config_t *a_chain_cfg)
         goto lb_err;
     }
     dap_chain_net_srv_stake_net_add(a_chain->net_id);
-    size_t l_auth_certs_count = dap_config_get_item_uint16_default(a_chain_cfg, DAP_CHAIN_ESBOCS_CS_TYPE_STR, "auth_certs_count", l_node_addrs_count);
+    uint16_t l_auth_certs_count = dap_config_get_item_uint16_default(a_chain_cfg, DAP_CHAIN_ESBOCS_CS_TYPE_STR, "auth_certs_count", l_node_addrs_count);
     log_it(L_ERROR, "l_auth_certs_count = %zu net %s  chain %s", l_auth_certs_count, a_chain->net_name, a_chain->name);
     dap_chain_net_t *l_net = dap_chain_net_by_id(a_chain->net_id);
-    for (size_t j = 0; j < l_auth_certs_count; ++j) {
-        uint16_t i = j;
-        if (i >= l_auth_certs_count) {
-            log_it(L_ERROR, "HOW??? i_count = %u l_auth_certs_count = %llu i_count < l_auth_certs_count %d net %s  chain %s ", i, l_auth_certs_count, i < l_auth_certs_count, a_chain->net_name, a_chain->name);
-        }
+    uint16_t i = -1;
+    while (true) {
+        ++i;
+        if (i >= l_auth_certs_count)
+            break;
         log_it(L_ERROR, "i_count = %llu l_auth_certs_count = %llu i_count < l_auth_certs_count %d net %s  chain %s ", i, l_auth_certs_count, i < l_auth_certs_count, a_chain->net_name, a_chain->name);
         char l_cert_name[512];
         dap_cert_t *l_cert_cur;
