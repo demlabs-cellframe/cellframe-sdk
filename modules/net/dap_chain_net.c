3eb04f2a617f4ffa71a7d61fa0c9a80531ea49dc
pavel.uhanov <pavel.uhanov@demlabs.net>
2024-10-15 12:41:31 +0300

    [*] small fix

--- modules/net/dap_chain_net.c
+++ modules/net/dap_chain_net.c
@@ -203,7 +203,7 @@ static const char *c_net_states[] = {
 };
 
 static inline const char * dap_chain_net_state_to_str(dap_chain_net_state_t a_state) {
-    return a_state < NET_STATE_OFFLINE || a_state > NET_STATE_ONLINE ? "NET_STATE_INVALID" : c_net_states[a_state];
+    return a_state < NET_STATE_LOADING || a_state > NET_STATE_ONLINE ? "NET_STATE_INVALID" : c_net_states[a_state];
 }
 
 // Node link callbacks
