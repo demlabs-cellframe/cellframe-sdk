93cef52c0cd97932d5bf997a9108a2c4b7aa878e
alexey.stratulat <alexey.stratulat@demlabs.net>
2024-10-24 22:09:26 +0700

    [*] Dumped wallet information for WalletsInfo and WalletInfo notifier message that carries wallet information.

--- modules/wallet/include/dap_chain_wallet.h
+++ modules/wallet/include/dap_chain_wallet.h
@@ -88,3 +88,4 @@ int dap_chain_wallet_activate   (const char *a_name, ssize_t a_name_len, const c
 int dap_chain_wallet_deactivate   (const char *a_name, ssize_t a_name_len);
 
 const char* dap_chain_wallet_check_sign(dap_chain_wallet_t *a_wallet);
+json_object *dap_chain_wallet_info_to_json(const char *a_name, const char *a_path);
