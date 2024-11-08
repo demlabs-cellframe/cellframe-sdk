90582edf2095fe7a75dac783f0e59a8a8e088ea6
alexey.stratulat <alexey.stratulat@demlabs.net>
2024-11-08 14:40:14 +0700

    [+] Added a check to the wallet signature verification function to make sure that the wallet certificate is loaded at all, otherwise an error message is displayed.

--- modules/wallet/dap_chain_wallet.c
+++ modules/wallet/dap_chain_wallet.c
@@ -1027,6 +1027,7 @@ const char* dap_chain_wallet_check_sign(dap_chain_wallet_t *a_wallet) {
     dap_chain_wallet_internal_t *l_wallet_internal = DAP_CHAIN_WALLET_INTERNAL(a_wallet);
     dap_return_val_if_pass(!l_wallet_internal->certs || !l_wallet_internal->certs, "" );
     for (size_t i = 0; i < l_wallet_internal->certs_count; ++i) {
+        dap_return_val_if_pass(!l_wallet_internal->certs[i], "The wallet contains an undefined certificate.\n");
         dap_sign_type_t l_sign_type = dap_sign_type_from_key_type(l_wallet_internal->certs[i]->enc_key->type);
         if (SIG_TYPE_BLISS == l_sign_type.type || SIG_TYPE_PICNIC == l_sign_type.type || SIG_TYPE_TESLA == l_sign_type.type) {
             return "The Bliss, Picnic and Tesla signatures is deprecated. We recommend you to create a new wallet with another available signature and transfer funds there.\n";
