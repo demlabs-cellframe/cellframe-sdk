03b73c0e5c8b3f51fc098603675d95a049498687
pavel.uhanov <pavel.uhanov@demlabs.net>
2024-11-26 16:46:06 +0300

    [*] change tsd code to 0x0013

--- modules/net/dap_chain_ledger.c
+++ modules/net/dap_chain_ledger.c
@@ -4262,8 +4262,10 @@ static int s_balance_cache_update(dap_ledger_t *a_ledger, dap_ledger_wallet_bala
     /* Notify the world*/
     if ( !dap_chain_net_get_load_mode(a_ledger->net) ) {
         struct json_object *l_json = wallet_info_json_collect(a_ledger, a_balance);
-        dap_notify_server_send_mt(json_object_get_string(l_json));
-        json_object_put(l_json);
+        if (l_json) {
+            dap_notify_server_send_mt(json_object_get_string(l_json));
+            json_object_put(l_json);
+        }
     }
     return 0;
 }
