a64ce31b9dbe6476029aa77998149500be3e03ab
Constantin P <papizh.konstantin@demlabs.net>
2024-11-29 14:27:57 +0700

    random access

--- modules/chain/dap_chain_cell.c
+++ modules/chain/dap_chain_cell.c
@@ -185,7 +185,7 @@ DAP_STATIC_INLINE int s_cell_map_new_volume(dap_chain_cell_t *a_cell, size_t a_f
     a_cell->map_range_bounds = dap_list_append(a_cell->map_range_bounds, a_cell->map_end = a_cell->map + l_map_size);
 #ifndef DAP_OS_WINDOWS    
     if (a_load)
-        madvise(a_cell->map, l_map_size, MADV_SEQUENTIAL);
+        madvise(a_cell->map, l_map_size, MADV_RANDOM);
 #endif
     return 0;
 }
