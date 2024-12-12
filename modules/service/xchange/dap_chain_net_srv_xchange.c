9bf993d349f5c60314eb0a5fc9ca71dea03e6938
daniil.frolov <daniil.frolov@demlabs.net>
2024-12-12 14:50:04 +0300

    add datum deleting in fork handling

--- modules/service/xchange/dap_chain_net_srv_xchange.c
+++ modules/service/xchange/dap_chain_net_srv_xchange.c
@@ -3355,28 +3355,32 @@ static void s_ledger_tx_add_notify(void *a_arg, dap_ledger_t *a_ledger, dap_chai
             HASH_FIND(hh, l_cache->cache, a_tx_hash, sizeof(dap_hash_fast_t), l_cache_found);
             if (l_cache_found){
                 xchange_tx_type_t l_tx_type = l_cache_found->tx_type;
-
                 if (l_tx_type == TX_TYPE_EXCHANGE){
                     xchange_tx_cache_t* l_cache_prev_tx = NULL;
                     HASH_FIND(hh, l_cache->cache, &l_cache_found->tx_info.exchange_info.prev_hash, sizeof(dap_hash_fast_t), l_cache_prev_tx);
                     if(l_cache_prev_tx){
                         if (l_cache_prev_tx->tx_type == TX_TYPE_EXCHANGE){
-                                l_cache_prev_tx->tx_info.exchange_info.next_hash = (dap_hash_fast_t){0};
-                                //update order ammount
+                            xchange_tx_cache_t* l_cache_order = NULL;
+                            HASH_FIND(hh, l_cache->cache, &l_cache_found->tx_info.exchange_info.order_hash, sizeof(dap_hash_fast_t), l_cache_order);
+                            l_cache_prev_tx->tx_info.exchange_info.next_hash = (dap_hash_fast_t){0};
+                            SUM_256_256(l_cache_order->tx_info.order_info.value_ammount, l_cache_found->tx_info.exchange_info.buy_value, &l_cache_order->tx_info.order_info.value_ammount);
                         } else if (l_cache_prev_tx->tx_type == TX_TYPE_ORDER){
-                                l_cache_prev_tx->tx_info.exchange_info.next_hash = (dap_hash_fast_t){0};
+                            l_cache_prev_tx->tx_info.order_info.next_hash = (dap_hash_fast_t){0};
+                            l_cache_prev_tx->tx_info.order_info.value_ammount = l_cache_prev_tx->tx_info.order_info.value;
                         }
                     }
-
                 } else if (l_tx_type == TX_TYPE_INVALIDATE){
-                    
-                    // find prev tx and clear next_hash
-
                     xchange_tx_cache_t* l_cache_prev_tx = NULL;
-                    HASH_FIND(hh, l_cache_net->cache, &l_cache->tx_info.invalidate_info.prev_hash, sizeof(dap_hash_fast_t), l_cache_prev_tx);
+                    HASH_FIND(hh, l_cache->cache, &l_cache_found->tx_info.exchange_info.prev_hash, sizeof(dap_hash_fast_t), l_cache_prev_tx);
                     if(l_cache_prev_tx){
                         if (l_cache_prev_tx->tx_type == TX_TYPE_EXCHANGE){
-                                l_cache_prev_tx->tx_info.exchange_info.next_hash = *a_tx_hash;
+                            xchange_tx_cache_t* l_cache_order = NULL;
+                            HASH_FIND(hh, l_cache->cache, &l_cache_found->tx_info.exchange_info.order_hash, sizeof(dap_hash_fast_t), l_cache_order);
+                            l_cache_prev_tx->tx_info.exchange_info.next_hash = (dap_hash_fast_t){0};
+                            l_cache_order->tx_info.order_info.value_ammount = l_cache_found->tx_info.invalidate_info.returned_value;
+                        } else if (l_cache_prev_tx->tx_type == TX_TYPE_ORDER){
+                            l_cache_prev_tx->tx_info.order_info.next_hash = (dap_hash_fast_t){0};
+                            l_cache_prev_tx->tx_info.order_info.value_ammount = l_cache_prev_tx->tx_info.order_info.value;
                         }
                     }
                 }
@@ -3384,9 +3388,6 @@ static void s_ledger_tx_add_notify(void *a_arg, dap_ledger_t *a_ledger, dap_chai
                 HASH_DEL(l_cache->cache, l_cache_found);
                 DAP_DELETE(l_cache_found);
             }
-
-            // Delete all connection of this tx with other in cache
-            
         }
     } 
 }
