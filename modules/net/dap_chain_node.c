8ae6f71cf67f6871b5d433ffedc398c7d0d441ee
Olzhas <oljas.jarasbaev@demlabs.net>
2025-01-30 20:49:48 +0700

    [*] fix~

--- modules/net/dap_chain_node.c
+++ modules/net/dap_chain_node.c
@@ -232,7 +232,7 @@ void dap_chain_node_list_cluster_del_callback(dap_store_obj_t *a_obj, void *a_ar
         dap_global_db_set_raw_sync(a_obj, 1);
     } else {
         log_it(L_DEBUG, "Can't do handshake with %s [ %s : %u ] delete from node list", a_obj->key, l_node_info->ext_host, l_node_info->ext_port);
-        dap_global_db_driver_delete(a_obj, 1);
+        dap_del_global_db_obj_by_ttl(a_obj);
     }
     dap_strfreev(l_group_strings);
 }
