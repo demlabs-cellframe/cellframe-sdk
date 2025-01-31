4dc8108c79b95d5547bc160737cc9d9e47c9fbec
Olzhas <oljas.jarasbaev@demlabs.net>
2025-01-31 12:05:10 +0700

    [*] fix

--- modules/net/dap_chain_node.c
+++ modules/net/dap_chain_node.c
@@ -228,8 +228,8 @@ void dap_chain_node_list_cluster_del_callback(dap_store_obj_t *a_obj, void *a_ar
         // dap_chain_node_client_close_unsafe(l_client);  del in s_go_stage_on_client_worker_unsafe
     }
     if (l_ret == 0) {
-        a_obj->timestamp = dap_nanotime_now();
-        dap_global_db_set_raw_sync(a_obj, 1);
+        dap_global_db_set_sync(a_obj->group, a_obj->key, a_obj->value, a_obj->value_len, a_obj->flags & DAP_GLOBAL_DB_RECORD_PINNED);
+        // dap_global_db_set_raw_sync(a_obj, 1);
     } else {
         log_it(L_DEBUG, "Can't do handshake with %s [ %s : %u ] delete from node list", a_obj->key, l_node_info->ext_host, l_node_info->ext_port);
         dap_del_global_db_obj_by_ttl(a_obj);
