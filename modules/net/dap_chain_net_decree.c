f9b621d5a7b3e65e4001336c720c2b6cb9394dfa
pavel.uhanov <pavel.uhanov@demlabs.net>
2025-02-11 11:03:05 +0000

    [*] port

--- modules/net/dap_chain_net_decree.c
+++ modules/net/dap_chain_net_decree.c
@@ -33,6 +33,7 @@
 #include "dap_chain_net_tx.h"
 #include "dap_chain_net_srv_stake_pos_delegate.h"
 #include "dap_http_ban_list_client.h"
+#include "dap_chain_policy.h"
 
 
 
@@ -632,6 +633,19 @@ static int s_common_decree_handler(dap_chain_datum_decree_t *a_decree, dap_chain
                 break;
             dap_chain_esbocs_set_emergency_validator(l_chain, l_action, l_sign_type, &l_hash);
         } break;
+        case DAP_CHAIN_DATUM_DECREE_COMMON_SUBTYPE_POLICY: {
+            if (!a_apply)
+                break;
+            dap_chain_policy_t *l_policy = NULL;
+            if ( !(l_policy = dap_chain_datum_decree_get_policy(a_decree)) ){
+                log_it(L_WARNING,"Can't get policy from decree.");
+                return -105;
+            }
+            l_policy = DAP_DUP_SIZE_RET_VAL_IF_FAIL(l_policy, dap_chain_policy_get_size(l_policy), -106);
+            if (DAP_FLAG_CHECK(l_policy->activate.flags, DAP_CHAIN_POLICY_FLAG_ACTIVATE_BY_BLOCK_NUM))
+                l_policy->activate.chain_union.chain = dap_chain_find_by_id(a_net->pub.id, l_policy->activate.chain_union.chain_id);
+            return dap_chain_policy_add(l_policy, a_net->pub.id.uint64);
+        }
         default:
             return -1;
     }
--- modules/net/dap_chain_node_cli.c
+++ modules/net/dap_chain_node_cli.c
@@ -353,6 +353,22 @@ int dap_chain_node_cli_init(dap_config_t * g_config)
                            "\tTypes decree: fee, owners, owners_min, stake_approve, stake_invalidate, min_value, "
                            "min_validators_count, ban, unban, reward, validator_max_weight, emergency_validators, check_signs_structure\n");
 
+
+    dap_cli_server_cmd_add ("policy", com_policy, "Policy commands", "policy [execute] - prepare or create policy execute decree\n"
+                "\t-net <net_name> - net name to policy apply\n"
+                "\t-num <policy_num> - policy num\n"
+                "\t[-ts_start <dd/mm/YY-H:M:S>] - date to start policy\n"
+                "\t[-ts_stop <dd/mm/YY-H:M:S>] - date to stop policy\n"
+                "\t{\n\t\t[-block_start <block_num>] - block num to start policy\n"
+                "\t\t[-block_stop <block_num>] - block num to stop policy\n"
+                "\t\t-chain <chain_name> - chain name to check blocks num\n\t}\n"
+                "\t[-deactivate <deactivate_nums>] - list deactivated policy, example: 1,4,6\n"
+                "\t[-certs <certs_name>] - list signing certs, example: foobar.root.pvt.0,foobar.root.pvt.1\n"
+                "policy find - find info about policy in net\n"
+                "\t-net <net_name> - net name to policy search\n"
+                "\t-num <policy_num> - policy num\n"
+                "policy list - show all policies from table in net\n"
+                "\t-net <net_name> - net name to policy search\n");
     // Exit - always last!
     dap_cli_server_cmd_add ("exit", com_exit, "Stop application and exit",
                 "exit\n" );
