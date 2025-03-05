bfd2731ec8335dfe256f16f30419cf115f30af2c
pavel.uhanov <pavel.uhanov@demlabs.net>
2025-03-05 04:26:31 +0000

    [*] fix flag check

--- modules/net/dap_chain_net_decree.c
+++ modules/net/dap_chain_net_decree.c
@@ -644,7 +644,7 @@ static int s_common_decree_handler(dap_chain_datum_decree_t *a_decree, dap_chain
             l_policy = DAP_DUP_SIZE_RET_VAL_IF_FAIL(l_policy, dap_chain_policy_get_size(l_policy), -106);
             if (l_policy->type == DAP_CHAIN_POLICY_ACTIVATE) {
                 dap_chain_policy_activate_t *l_policy_activate = (dap_chain_policy_activate_t *)l_policy->data;
-                if(DAP_FLAG_CHECK(l_policy_activate->flags, DAP_CHAIN_POLICY_FLAG_ACTIVATE_BY_BLOCK_NUM));
+                if(DAP_FLAG_CHECK(l_policy_activate->flags, DAP_CHAIN_POLICY_FLAG_ACTIVATE_BY_BLOCK_NUM))
                     l_policy_activate->chain_union.chain = dap_chain_find_by_id(a_net->pub.id, l_policy_activate->chain_union.chain_id);
             }
             return dap_chain_policy_add(l_policy, a_net->pub.id.uint64);
