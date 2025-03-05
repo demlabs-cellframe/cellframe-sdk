ecbd1040144dd44a3895c334f44ba1246dd292fb
Olzhas <oljas.jarasbaev@demlabs.net>
2025-03-05 11:17:35 +0700

    [*] fix timers

--- modules/net/dap_chain_net.c
+++ modules/net/dap_chain_net.c
@@ -2193,7 +2193,7 @@ static void *s_net_load(void *a_arg)
     snprintf(l_net->pub.gdb_nodes, sizeof(l_net->pub.gdb_nodes), "%s.%s", l_net->pub.gdb_groups_prefix, s_gdb_nodes_postfix);
     l_net_pvt->nodes_cluster = dap_global_db_cluster_add(dap_global_db_instance_get_default(),
                                                          l_net->pub.name, dap_guuid_compose(l_net->pub.id.uint64, 0),
-                                                         l_net->pub.gdb_nodes, 100, true, //7200
+                                                         l_net->pub.gdb_nodes, 7200, true,
                                                          DAP_GDB_MEMBER_ROLE_GUEST,
                                                          DAP_CLUSTER_TYPE_EMBEDDED);
     if (!l_net_pvt->nodes_cluster) {
