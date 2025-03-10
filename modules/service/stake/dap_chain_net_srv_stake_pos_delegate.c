6a32cbdb591da4f9c624d64b4873948f0886a338
pavel.uhanov <pavel.uhanov@demlabs.net>
2025-03-10 07:27:17 +0000

    [*] fix verificator error

--- modules/service/stake/dap_chain_net_srv_stake_pos_delegate.c
+++ modules/service/stake/dap_chain_net_srv_stake_pos_delegate.c
@@ -274,7 +274,7 @@ static int s_stake_verificator_callback(dap_ledger_t *a_ledger, dap_chain_datum_
             log_it(L_WARNING, "Conditional out and conditional in have different headers");         \
             return -3;                                                                              \
         }                                                                                           \
-        if (l_tx_new_cond->tsd_size != a_cond->tsd_size ||                                          \
+        if (l_tx_new_cond->tsd_size < a_cond->tsd_size ||                                          \
                 memcmp(l_tx_new_cond->tsd, a_cond->tsd, a_cond->tsd_size)) {                        \
             log_it(L_WARNING, "Conditional out and conditional in have different TSD sections");    \
             return -4;                                                                              \
