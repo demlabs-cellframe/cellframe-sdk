0d6e557e422e01c53e7e6e7fba6b2f93062b5b88
pavel.uhanov <pavel.uhanov@demlabs.net>
2025-03-17 07:25:13 +0000

    hotfix-15809_m

--- modules/service/stake/include/dap_chain_net_srv_stake_pos_delegate.h
+++ modules/service/stake/include/dap_chain_net_srv_stake_pos_delegate.h
@@ -104,3 +104,4 @@ int dap_chain_net_srv_stake_mark_validator_active(dap_chain_addr_t *a_signing_ad
 dap_chain_net_srv_stake_item_t *dap_chain_net_srv_stake_check_pkey_hash(dap_chain_net_id_t a_net_id, dap_hash_fast_t *a_pkey_hash);
 uint256_t dap_chain_net_srv_stake_get_total_weight(dap_chain_net_id_t a_net_id, uint256_t *a_locked_weight);
 size_t dap_chain_net_srv_stake_get_total_keys(dap_chain_net_id_t a_net_id, size_t *a_in_active_count);
+dap_pkey_t *dap_chain_net_srv_stake_get_pkey_by_hash(dap_chain_net_id_t a_net_id, dap_hash_fast_t *a_hash);
