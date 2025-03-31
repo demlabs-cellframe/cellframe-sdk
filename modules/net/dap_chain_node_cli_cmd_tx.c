5e77f4cdc6607f88e279f9a6b77c0df9d0628fef
Olzhas <oljas.jarasbaev@demlabs.net>
2025-03-31 12:13:39 +0700

    [+] need sign

--- modules/net/dap_chain_node_cli_cmd_tx.c
+++ modules/net/dap_chain_node_cli_cmd_tx.c
@@ -1104,6 +1104,8 @@ int com_ledger(int a_argc, char ** a_argv, void **reply)
         dap_cli_server_cmd_find_option_val(a_argv, arg_index, a_argc, "-hash", &l_tx_hash_str);
         //get net
         dap_cli_server_cmd_find_option_val(a_argv, arg_index, a_argc, "-net", &l_net_str);
+        const char * l_need_sign;
+        dap_cli_server_cmd_find_option_val(a_argv, arg_index, a_argc, "-need_sign", &l_need_sign);
         //get search type
         bool l_unspent_flag = dap_cli_server_cmd_find_option_val(a_argv, arg_index, a_argc, "-unspent", NULL);
         //check input
@@ -1139,6 +1141,21 @@ int com_ledger(int a_argc, char ** a_argv, void **reply)
             json_object_put(json_datum);
             DAP_DEL_Z(l_tx_hash);
             return DAP_CHAIN_NODE_CLI_COM_LEDGER_TX_HASH_ERR;
+        }
+        if (l_need_sign) {
+            byte_t *item; size_t l_size;
+            TX_ITEM_ITER_TX(item, l_size, l_datum_tx) {
+                if (*item == TX_ITEM_TYPE_SIG) {
+                    json_object *json_obj_item = json_object_new_object();
+                    dap_sign_t *l_sign = dap_chain_datum_tx_item_sign_get_sig((dap_chain_tx_sig_t*)item);
+                    char *l_sign_b64 = DAP_NEW_Z_SIZE(char, DAP_ENC_BASE64_ENCODE_SIZE(dap_sign_get_size(l_sign)) + 1);
+                    size_t l_sign_size = dap_sign_get_size(l_sign);
+                    dap_enc_base64_encode(l_sign, l_sign_size, l_sign_b64, DAP_ENC_DATA_TYPE_B64_URLSAFE);
+                    json_object_object_add(json_obj_item, "sig_b64_size",   json_object_new_uint64(l_sign_size));
+                    json_object_object_add(json_obj_item, "sig_b64",    json_object_new_string(l_sign_b64));
+                    json_object_array_add(json_datum, json_obj_item);
+                }
+            }
         }
         DAP_DEL_Z(l_tx_hash);
         if (json_datum){
