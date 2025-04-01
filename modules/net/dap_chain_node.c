bf0619c1b6fca06090a357a16803b5f3a0cadcdb
oljas.jarasbaev <oljas.jarasbaev@demlabs.net>
2025-04-01 10:05:09 +0000

    13903

--- modules/net/dap_chain_node.c
+++ modules/net/dap_chain_node.c
@@ -214,12 +214,16 @@ dap_string_t *dap_chain_node_states_info_read(dap_chain_net_t *a_net, dap_stream
 
 void dap_chain_node_list_cluster_del_callback(dap_store_obj_t *a_obj, void *a_arg) {
     UNUSED(a_arg);
-    dap_return_if_fail(a_obj);
+    if (dap_store_obj_get_type(a_obj) == DAP_GLOBAL_DB_OPTYPE_DEL) {
+        log_it(L_DEBUG, "Delete node list hole %s key %s", a_obj->group, a_obj->key);
+        dap_global_db_driver_delete(a_obj, 1);
+        return;
+    }
     log_it(L_DEBUG, "Start check node list %s group %s key", a_obj->group, a_obj->key);
 
-    if (a_obj->value_len == 0) {
-        dap_global_db_del_sync(a_obj->group, a_obj->key);
+    if (!a_obj->value) {
         log_it(L_DEBUG, "Can't find value in %s group %s key delete from node list", a_obj->group, a_obj->key);
+        dap_global_db_driver_delete(a_obj, 1);
         return;
     }
     dap_chain_node_info_t *l_node_info = (dap_chain_node_info_t*)a_obj->value;
@@ -236,7 +240,6 @@ void dap_chain_node_list_cluster_del_callback(dap_store_obj_t *a_obj, void *a_ar
         dap_chain_node_client_t *l_client = dap_chain_node_client_connect_default_channels(l_net, l_node_info);
         if (l_client)
             l_ret = dap_chain_node_client_wait(l_client, NODE_CLIENT_STATE_ESTABLISHED, 30000);
-        // dap_chain_node_client_close_unsafe(l_client); TODO unexpected del in s_go_stage_on_client_worker_unsafe
     }
     if (l_ret == 0) {
         dap_global_db_set_sync(a_obj->group, a_obj->key, a_obj->value, a_obj->value_len, a_obj->flags & DAP_GLOBAL_DB_RECORD_PINNED);
@@ -257,6 +260,7 @@ int dap_chain_node_list_clean_init() {
             log_it(L_DEBUG, "Node list clean inited for net %s", l_net->pub.name);
         }
     }
+    dap_proc_thread_timer_add_pri(NULL, dap_chain_net_announce_addr_all, NULL, 300000, true, DAP_QUEUE_MSG_PRIORITY_NORMAL);
     return 0;
 }
 
