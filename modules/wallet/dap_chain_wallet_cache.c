4c533ae7eb22a0e0eeef7d5bcc4b6e5bbbacaa1c
Constantin P. <papizh.konstantin@demlabs.net>
2025-04-17 13:02:01 +0700

    ...

--- modules/wallet/dap_chain_wallet_cache.c
+++ modules/wallet/dap_chain_wallet_cache.c
@@ -744,7 +744,7 @@ static int s_save_tx_cache_for_addr(dap_chain_t *a_chain, dap_chain_addr_t *a_ad
         dap_chain_addr_t l_addr;
         uint256_t l_value;
         uint8_t *l_prev_item = NULL;
-        int l_prev_idx;
+        int l_prev_idx = 0;
 
         switch(*l_tx_item) {
         case TX_ITEM_TYPE_IN: {
