42aa8a58e83279932d3f195c335d861600f81135
roman.khlopkov <roman.khlopkov@demlabs.net>
2025-07-09 11:57:49 +0300

    [-] Debug code

--- modules/ledger/dap_chain_ledger_tx.c
+++ modules/ledger/dap_chain_ledger_tx.c
@@ -919,8 +919,7 @@ static int s_tx_cache_check(dap_ledger_t *a_ledger,
         } break;
         case TX_ITEM_TYPE_OUT_STD: {
             dap_chain_tx_out_std_t *l_tx_out = (dap_chain_tx_out_std_t *)it;
-            if (l_tx_out->ts_unlock &&
-                    dap_chain_policy_is_activated( a_ledger->net->pub.id, DAP_CHAIN_POLICY_OUT_STD_TIMELOCK_USE)) { // UNDO it after debug
+            if (l_tx_out->ts_unlock) {
                 l_err_num = DAP_LEDGER_TX_CHECK_TIMELOCK_ILLEGAL;
                 break;
             }
