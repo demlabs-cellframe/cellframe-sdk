510eed4efd6bca33ceda3c521542247c2f543d39
roman.khlopkov <roman.khlopkov@demlabs.net>
2025-07-28 19:29:38 +0300

    [*] Auction finish types correction

--- modules/common/include/dap_chain_datum_tx_event.h
+++ modules/common/include/dap_chain_datum_tx_event.h
@@ -49,17 +49,15 @@ typedef struct dap_chain_tx_event {
 
 #define DAP_CHAIN_TX_EVENT_TYPE_AUCTION_STARTED             0x0001
 #define DAP_CHAIN_TX_EVENT_TYPE_AUCTION_BID_PLACED          0x0002
-#define DAP_CHAIN_TX_EVENT_TYPE_AUCTION_WON                 0x0003
-#define DAP_CHAIN_TX_EVENT_TYPE_AUCTION_LOST                0x0004
-#define DAP_CHAIN_TX_EVENT_TYPE_AUCTION_CANCELLED           0x0005
+#define DAP_CHAIN_TX_EVENT_TYPE_AUCTION_FINISH              0x0003
+#define DAP_CHAIN_TX_EVENT_TYPE_AUCTION_CANCELLED           0x0004
 
 DAP_STATIC_INLINE const char *dap_chain_tx_item_event_type_to_str(uint16_t a_event_type)
 {
     switch (a_event_type) {
         case DAP_CHAIN_TX_EVENT_TYPE_AUCTION_STARTED: return "auction_started";
         case DAP_CHAIN_TX_EVENT_TYPE_AUCTION_BID_PLACED: return "auction_bid_placed";
-        case DAP_CHAIN_TX_EVENT_TYPE_AUCTION_WON: return "auction_won";
-        case DAP_CHAIN_TX_EVENT_TYPE_AUCTION_LOST: return "auction_lost";  
+        case DAP_CHAIN_TX_EVENT_TYPE_AUCTION_FINISH: return "auction_finish";  
         case DAP_CHAIN_TX_EVENT_TYPE_AUCTION_CANCELLED: return "auction_cancel";
         default: return "unknown";
     }
