20fb660dcf03d5da502cac2abb7be38e215c1cf7
roman.padenkov <roman.padenkov@demlabs.net>
2025-08-06 12:12:21 +0700

    ...

--- modules/net/dap_chain_node_cli.c
+++ modules/net/dap_chain_node_cli.c
@@ -428,6 +428,7 @@ int dap_chain_node_cli_parser_init(void) {
     dap_cli_server_cmd_add("srv_stake", NULL, json_print_for_srv_stake_list_keys, "---","---");
     dap_cli_server_cmd_add("dag", NULL, json_print_for_dag_list, "---","---");
     dap_cli_server_cmd_add("srv_xchange", NULL, json_print_for_srv_xchange_list, "---","---");
+    dap_cli_server_cmd_add("tx_history", NULL, json_print_for_tx_history_all, "---","---");
     return 0;
 }
 
