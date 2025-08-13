116c5c81f9919dbcb606136e7d576ba72fa51c2d
pavel.uhanov <pavel.uhanov@demlabs.net>
2025-08-13 14:22:02 +0300

    [*] subs update

--- modules/chain/tests/main.c
+++ modules/chain/tests/main.c
@@ -1,6 +1,6 @@
 #include "dap_chain_ledger_tests.h"
 int main(void){
-    dap_log_level_set(L_DEBUG);
+    dap_log_level_set(L_WARNING);
     dap_log_set_external_output(LOGGER_OUTPUT_STDOUT, NULL);
     dap_ledger_test_run();
     return 0;
