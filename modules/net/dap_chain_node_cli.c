fa5291d8398f7000b88c59416fe837e046e4d9b6
Olzhas <oljas.jarasbaev@demlabs.net>
2025-10-09 12:07:49 +0700

    [*] fix

--- modules/net/dap_chain_node_cli.c
+++ modules/net/dap_chain_node_cli.c
@@ -439,10 +439,10 @@ int dap_chain_node_cli_init(dap_config_t * g_config)
                 "\t-net <net_name>\n");
 
     dap_cli_server_cmd_add ("file", com_file, NULL, "Work with logs and files",
-                "file print {-num_line <number_of_lines> | -ts_after <10/06/25-22:59:40> } {-log | -path <path_to_file>}\n"
+                "file print {-num_line <number_of_lines> | -ts_after <M/D/Y-H:M:S>> } {-log | -path <path_to_file>}\n"
                 "\t print the last <num_line> lines from the log file or all logs after the specified date and time\n"
                 "\t -path <path_to_file> allows printing from a text file, but -ts_after option might not work\n"
-                "file export {-num_line <number_of_lines> | -ts_after <m/d/Y-H:M:S>} {-log | -path <path_to_file>} -dest <destination_path>\n"
+                "file export {-num_line <number_of_lines> | -ts_after <M/D/Y-H:M:S>} {-log | -path <path_to_file>} -dest <destination_path>\n"
                 "\t export last <num_line> lines from the log file or all logs after the specified date and time\n"
                 "\t -path <path_to_file> allows exporting from a text file, but -ts_after option might not work\n"
                 "file clear_log\n"
