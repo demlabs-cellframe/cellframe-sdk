a2e2f2186d5b92341edcc9cb6237714a233081c5
pavel.uhanov <pavel.uhanov@demlabs.net>
2025-10-28 13:01:38 +0300

    [*] port 19952

--- modules/net/dap_chain_net_tx.c
+++ modules/net/dap_chain_net_tx.c
@@ -2023,7 +2023,40 @@ int dap_chain_net_tx_to_json(dap_chain_datum_tx_t *a_tx, dap_json_t *a_out_json)
 }
 
 int dap_chain_net_tx_create_by_json(dap_json_t *a_tx_json, dap_chain_net_t *a_net, dap_json_t *a_json_obj_error, 
-    dap_chain_datum_tx_t** a_out_tx, size_t* a_items_count, size_t *a_items_ready)
+                                        dap_chain_datum_tx_t** a_out_tx, size_t* a_items_count, size_t *a_items_ready)
 {
-    dap_chain_tx_datum_from_json(a_tx_json, a_net, a_json_obj_error, a_out_tx, a_items_count, a_items_ready);
+    dap_return_val_if_pass(!a_tx_json || !a_out_tx, DAP_CHAIN_NET_TX_CREATE_JSON_WRONG_ARGUMENTS);
+
+    // Read items from json file
+    dap_json_t*l_json_items = dap_json_object_get_object(a_tx_json, "items");
+    size_t l_items_count;
+    if(!l_json_items || !dap_json_is_array(l_json_items) || !(l_items_count = dap_json_array_length(l_json_items))) {
+        return DAP_CHAIN_NET_TX_CREATE_JSON_NOT_FOUNT_ARRAY_ITEMS;
+    }
+    // Creating and adding items to the transaction
+    for(size_t i = 0; i < l_items_count; ++i) {
+        dap_json_t*l_json_item_obj = dap_json_array_get_idx(l_json_items, i);
+        if(!l_json_item_obj || !dap_json_is_object(l_json_item_obj)) {
+            continue;
+        }
+        const char *l_item_type_str = dap_json_object_get_string(l_json_item_obj, "type");
+        dap_chain_tx_item_type_t l_item_type = dap_chain_datum_tx_item_type_from_str_short(l_item_type_str);
+        if(l_item_type != TX_ITEM_TYPE_TSD) {
+            log_it(L_WARNING, "Item %zu has invalid type '%s'", i, l_item_type_str);
+            continue;
+        }
+
+        int64_t l_tsd_type = 0;
+        if(dap_json_object_get_int64_ext(l_json_item_obj, "type_tsd", &l_tsd_type)) {
+            dap_json_object_add_object(l_json_item_obj,"data_type", dap_json_object_new_int(l_tsd_type));
+            const char *l_tsd_data = dap_json_object_get_string(l_json_item_obj, "data");
+            if (l_tsd_data) {
+                dap_json_object_add_object(l_json_item_obj,"data_size", dap_json_object_new_uint64(strlen(l_tsd_data)));
+                char *l_tsd_str = dap_enc_base58_encode_to_str(l_tsd_data, strlen(l_tsd_data));
+                dap_json_object_add_object(l_json_item_obj,"data", dap_json_object_new_string(l_tsd_str));
+                DAP_DELETE(l_tsd_str);
+            }
+        }
+    }
+    return dap_chain_tx_datum_from_json(a_tx_json, a_net, a_json_obj_error, a_out_tx, a_items_count, a_items_ready);
 }
