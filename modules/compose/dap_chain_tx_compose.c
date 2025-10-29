76736e82023d94192c149474ee5389225bcbf11a
pavel.uhanov <pavel.uhanov@demlabs.net>
2025-10-29 11:08:12 +0300

    [*] build fix

--- modules/compose/dap_chain_tx_compose.c
+++ modules/compose/dap_chain_tx_compose.c
@@ -90,7 +90,7 @@ dap_chain_datum_tx_t* dap_chain_tx_compose_for_service(dap_chain_tx_compose_conf
 dap_chain_tx_compose_config_t* dap_chain_tx_compose_config_init(dap_chain_net_id_t a_net_id, const char *a_net_name, const char *a_native_ticker, const char *a_url_str,
                                  uint16_t a_port, const char *a_enc_cert_path)
 {
-    dap_return_val_if_pass(!a_net_id.raw || !a_net_name || !a_native_ticker || !a_url_str || !a_port, NULL);
+    dap_return_val_if_pass(!a_net_id.uint64 || !a_net_name || !a_native_ticker || !a_url_str || !a_port, NULL);
     dap_chain_tx_compose_config_t *l_config = DAP_NEW_Z_RET_VAL_IF_FAIL(dap_chain_tx_compose_config_t, NULL);
     l_config->net_id.uint64 = a_net_id.uint64;
     l_config->net_name = a_net_name;
@@ -720,7 +720,7 @@ bool dap_chain_tx_compose_get_remote_net_fee_and_address(uint256_t *a_net_fee, d
 
     if(IS_ZERO_256(*a_net_fee)) {
         log_it(L_INFO, "%s network feee is zero", a_config->net_name);
-        json_object_put(l_json_get_fee);
+        dap_json_object_free(l_json_get_fee);
         return false;
     }
 
