1c27316fdaa3b57c870bb6e879b2ee8476d8be8b
naeper <ceo@demlabs.net>
2025-11-26 00:51:36 +0700

    [*] Removed useless shit [*] Updated DAP SDK

--- modules/api/btc_rpc/dap_chain_btc_rpc.c
+++
@@ -1,9 +0,0 @@
-#include "dap_chain_btc_rpc.h"
-
-int dap_chain_btc_rpc_init(){
-    dap_chain_btc_rpc_registration_handlers();
-    return 0;
-}
-void dap_chain_btc_rpc_deinit(){
-    dap_chain_btc_rpc_unregistration_handlers();
-}
