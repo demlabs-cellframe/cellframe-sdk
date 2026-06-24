5d98fffeaf0bacf0a0b6bd05db5e7d1bee3dbfb5
Dmitriy A. Gerasimov <ceo@demlabs.net>
2026-06-24 19:57:43 +0700

    fix: remove duplicate l_addr definition in dap_chain_token_cli.c

--- modules/net/tx/dap_chain_token_cli.c
+++ modules/net/tx/dap_chain_token_cli.c
@@ -1285,7 +1285,6 @@ int com_token_emit(int a_argc, char **a_argv, dap_json_t *a_json_arr_reply, UNUS
     const char * l_chain_emission_str = NULL;
     dap_chain_t * l_chain_emission = NULL;
     dap_chain_net_t * l_net = NULL;
-    dap_chain_addr_t *l_addr = NULL;
     
     dap_json_t *json_obj_out = dap_json_object_new();
     int l_ret = 0;
